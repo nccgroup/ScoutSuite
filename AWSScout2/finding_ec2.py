58e6616ec3f68c2029aed44349ef9bf491b9f137
l01c d3v <l01cd3v@gmail.com>
2014-02-06 14:19:00 -0800

    Set macro_items fro all EC2 findings

--- AWSScout2/finding_ec2.py
+++ AWSScout2/finding_ec2.py
@@ -11,3 +11,36 @@ class Ec2Finding(Finding):
     def __init__(self, description, name, entity, callback, callback_args, idprefix, level):
         self.keyword_prefix = 'ec2'
         Finding.__init__(self, description, name, entity, callback, callback_args, idprefix, level)
+
+    def checkInternetAccessiblePort(self, obj):
+        protocol = self.callback_args[0][0].lower()
+        port = self.callback_args[0][1]
+        if protocol in obj['protocols']:
+            for rule in obj['protocols'][protocol]['rules']:
+                for grant in rule['grants']:
+                    if grant == '0.0.0.0/0' and self.portInRange(port, rule['ports']):
+                        self.items.append(obj['id'] + '-' + protocol.upper() + '-' + port + '-' + grant)
+                        self.macro_items.append(obj['id'])
+
+    def checkOpenPort(self, obj):
+        protocol = self.callback_args[0][0].lower()
+        port = self.callback_args[0][1]
+        if protocol in obj['protocols']:
+            for rule in obj['protocols'][protocol]['rules']:
+                for grant in rule['grants']:
+                    if self.portInRange(port, rule['ports']):
+                        self.items.append(obj['id'] + '-' + protocol.upper() + '-' + port)
+                        self.macro_items.append(obj['id'])
+
+    def portInRange(self, port, ports):
+        result = self.re_port_range.match(ports)
+        if result:
+            p1 = int(result.group(1))
+            p2 = int(result.group(2))
+            if int(port) in range(int(result.group(1)), int(result.group(2))):
+                return True
+        else:
+            result = self.re_single_port.match(ports)
+            if result and port == result.group(1):
+                return True
+        return False
