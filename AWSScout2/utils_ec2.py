c5f242f95b7a7b642ec2c46ebfb714bda57cc97f
l01c d3v <l01cd3v@gmail.com>
2014-05-02 16:14:12 -0700

    Fetch ELB data

--- AWSScout2/utils_ec2.py
+++ AWSScout2/utils_ec2.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python2
 
+# Import the Amazon SDK
+from boto.ec2 import elb
+
 # Import AWS Scout2 tools
 from AWSScout2.utils import *
 from AWSScout2.findings import *
@@ -18,27 +21,69 @@ def analyze_ec2_config(ec2_info, force_write):
     print 'Analyzing EC2 data...'
     analyze_config(ec2_finding_dictionary, ec2_info, 'EC2', force_write)
 
+def build_region_list(regions, ec2_info, fetch_ec2_gov):
+    region_list = []
+    for region in regions:
+        # h4ck -- skip china north region as it hangs when requesting instances (https://github.com/boto/boto/issues/2083)
+        if (region.name != 'us-gov-west-1' or fetch_ec2_gov) and (region.name != 'cn-north-1'):
+            region_list.append(region.name)
+            manage_dictionary(ec2_info['regions'], region.name, {})
+            # h4ck : data redundancy because I can't call ../@key in Handlebars
+            ec2_info['regions'][region.name]['name'] = region.name
+    return region_list
+
 def get_ec2_info(key_id, secret, session_token, fetch_ec2_gov):
     ec2_info = {}
     ec2_info['regions'] = {}
-    for region in boto.ec2.regions():
+    # Build region list for each EC2 service
+    ec2_regions = build_region_list(boto.ec2.regions(), ec2_info, fetch_ec2_gov)
+    elb_regions = build_region_list(boto.ec2.elb.regions(), ec2_info, fetch_ec2_gov)
+    vpc_regions = build_region_list(boto.vpc.regions(), ec2_info, fetch_ec2_gov)
+    for region in ec2_info['regions']:
         try:
-            # h4ck -- skip china north region as it hangs when requesting instances (https://github.com/boto/boto/issues/2083)
-            if (region.name != 'us-gov-west-1' or fetch_ec2_gov) and (region.name != 'cn-north-1'):
-                manage_dictionary(ec2_info['regions'], region.name, {})
-                # h4ck : data redundancy because I can't call ../@key in Handlebars
-                ec2_info['regions'][region.name]['name'] = region.name
-                ec2_connection = boto.ec2.connect_to_region(region.name, aws_access_key_id = key_id, aws_secret_access_key = secret, security_token = session_token)
-                vpc_connection = boto.vpc.connect_to_region(region.name, aws_access_key_id = key_id, aws_secret_access_key = secret, security_token = session_token)
-                print 'Fetching EC2 data for region %s...' % region.name
-                manage_dictionary(ec2_info['regions'][region.name], 'vpcs', {})
-                get_vpc_info(vpc_connection, ec2_info['regions'][region.name]['vpcs'])
-                get_security_groups_info(ec2_connection, ec2_info['regions'][region.name]['vpcs'])
-                get_instances_info(ec2_connection, ec2_info['regions'][region.name]['vpcs'])
+            print 'Fetching EC2 data for region %s...' % region
+            # VPC
+            if region in vpc_regions:
+                vpc_connection = boto.vpc.connect_to_region(region, aws_access_key_id = key_id, aws_secret_access_key = secret, security_token = session_token)
+                manage_dictionary(ec2_info['regions'][region], 'vpcs', {})
+                get_vpc_info(vpc_connection, ec2_info['regions'][region]['vpcs'])
+            # Security groups and instances
+            if region in ec2_regions:
+                ec2_connection = boto.ec2.connect_to_region(region, aws_access_key_id = key_id, aws_secret_access_key = secret, security_token = session_token)
+                manage_dictionary(ec2_info['regions'][region], 'vpcs', {})
+                get_security_groups_info(ec2_connection, ec2_info['regions'][region]['vpcs'])
+                get_instances_info(ec2_connection, ec2_info['regions'][region]['vpcs'])
+            # ELB
+            if region in elb_regions:
+                elb_connection = boto.ec2.elb.connect_to_region(region, aws_access_key_id = key_id, aws_secret_access_key = secret, security_token = session_token)
+                get_elb_info(elb_connection, ec2_info['regions'][region])
         except Exception, e:
             print 'Exception:\n %s' % traceback.format_exc()
             pass
     return ec2_info
+
+def get_elb_info(elb_connection, region_info):
+    load_balancers = elb_connection.get_all_load_balancers()
+    count, total = init_status(None, 'ELB')
+    for lb in load_balancers:
+        manage_dictionary(region_info, 'elbs', {})
+        manage_dictionary(region_info['elbs'], lb.name, {})
+        for key in ['dns_name', 'created_time', 'availability_zones', 'canonical_hosted_zone_name', 'canonical_hosted_zone_name_id', 'name', 'security_groups', 'subnets', 'vpc_id']:
+            region_info['elbs'][lb.name][key] = lb.__dict__[key]
+        manage_dictionary(region_info['elbs'][lb.name], 'listeners', {})
+        for l in lb.listeners:
+            port = str(l.load_balancer_port)
+            manage_dictionary(region_info['elbs'][lb.name]['listeners'], port, {})
+            for key in ['load_balancer_port', 'instance_port', 'protocol', 'instance_protocol', 'ssl_certificate_id', 'policy_names']:
+                region_info['elbs'][lb.name]['listeners'][port][key] = l.__dict__[key]
+        manage_dictionary(region_info['elbs'][lb.name], 'instances', [])
+        for i in lb.instances:
+            region_info['elbs'][lb.name]['instances'].append(i.id)
+        manage_dictionary(region_info['elbs'][lb.name], 'source_security_group', {})
+        region_info['elbs'][lb.name]['source_security_group']['name'] = lb.source_security_group.name
+        region_info['elbs'][lb.name]['source_security_group']['owner_alias'] = lb.source_security_group.owner_alias
+        count = update_status(count, total, 'ELB')
+    close_status(count, total, 'ELB')
 
 def get_instances_info(ec2, vpc_info):
     reservations = ec2.get_all_reservations()
@@ -108,41 +153,44 @@ def get_security_groups_info(ec2_connection, vpc_info):
         vpc_info[vpc_id]['id'] = vpc_id
         manage_dictionary(vpc_info[vpc_id], 'security_groups', {})
         manage_dictionary(vpc_info[vpc_id]['security_groups'], group.name, {})
-        security_group = {}
-        security_group['name'] = group.name
-        security_group['id'] = group.id
-        security_group['description'] = group.description
-        security_group = manage_dictionary(security_group, 'running-instances', [])
-        security_group = manage_dictionary(security_group, 'stopped-instances', [])
-        protocols = {}
-        for rule in group.rules:
-            protocols = manage_dictionary(protocols, rule.ip_protocol, {})
-            protocols[rule.ip_protocol] = manage_dictionary(protocols[rule.ip_protocol], 'rules', [])
-            protocols[rule.ip_protocol]['name'] = rule.ip_protocol.upper()
-            acl = {}
-            acl['grants'] = []
-            # Save grants, values are either a CIDR or an EC2 security group
-            for grant in rule.grants:
-                if grant.cidr_ip:
-                    acl['grants'].append(grant.cidr_ip)
-                else:
-                    acl['grants'].append('%s (%s)' % (grant.name, grant.groupId))
-            # Save the port (single port or range)
-            if rule.from_port == rule.to_port:
-                acl['ports'] = rule.from_port
-            else:
-                acl['ports'] = '%s-%s' % (rule.from_port, rule.to_port)
-            # Save the new rule
-            protocols[rule.ip_protocol]['rules'].append(acl)
-        # Save all the rules, sorted by protocol
-        security_group['protocols'] = protocols
-        # Save all instances associated with this group
-        for i in group.instances():
-            if i.state == 'running':
-                security_group['running-instances'].append(i.id)
-            else:
-                security_group['stopped-instances'].append(i.id)
         # Append the new security group to the return list
-        vpc_info[vpc_id]['security_groups'][group.name] = security_group
+        vpc_info[vpc_id]['security_groups'][group.name] = parse_security_group(group)
         count = update_status(count, total, 'Security groups')
     close_status(count, total, 'Security groups')
+
+def parse_security_group(group):
+    security_group = {}
+    security_group['name'] = group.name
+    security_group['id'] = group.id
+    security_group['description'] = group.description
+    security_group = manage_dictionary(security_group, 'running-instances', [])
+    security_group = manage_dictionary(security_group, 'stopped-instances', [])
+    protocols = {}
+    for rule in group.rules:
+        protocols = manage_dictionary(protocols, rule.ip_protocol, {})
+        protocols[rule.ip_protocol] = manage_dictionary(protocols[rule.ip_protocol], 'rules', [])
+        protocols[rule.ip_protocol]['name'] = rule.ip_protocol.upper()
+        acl = {}
+        acl['grants'] = []
+        # Save grants, values are either a CIDR or an EC2 security group
+        for grant in rule.grants:
+            if grant.cidr_ip:
+                acl['grants'].append(grant.cidr_ip)
+            else:
+                acl['grants'].append('%s (%s)' % (grant.name, grant.groupId))
+        # Save the port (single port or range)
+        if rule.from_port == rule.to_port:
+            acl['ports'] = rule.from_port
+        else:
+            acl['ports'] = '%s-%s' % (rule.from_port, rule.to_port)
+        # Save the new rule
+        protocols[rule.ip_protocol]['rules'].append(acl)
+    # Save all the rules, sorted by protocol
+    security_group['protocols'] = protocols
+    # Save all instances associated with this group
+    for i in group.instances():
+        if i.state == 'running':
+            security_group['running-instances'].append(i.id)
+        else:
+            security_group['stopped-instances'].append(i.id)
+    return security_group
