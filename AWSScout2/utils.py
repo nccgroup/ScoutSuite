bcf1aa8213c9344fc9a1bbffb8a06697b6365ddb
l01c d3v <l01cd3v@gmail.com>
2015-08-07 08:44:55 -0700

    Fix build, vpc is not "really" a supported service

--- AWSScout2/utils.py
+++ AWSScout2/utils.py
@@ -34,7 +34,7 @@ re_service_utils = re.compile(r'^utils_(.*?).py$')
 scout2_utils_dir, foo = os.path.split(__file__)
 for filename in os.listdir(scout2_utils_dir):
     service = re_service_utils.match(filename)
-    if service:
+    if service and service.groups()[0] != 'vpc':
         supported_services.append(service.groups()[0])
 
 ec2_classic = 'EC2-Classic'
