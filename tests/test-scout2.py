165945528c0736faa404cdfc246b35f11dfda77e
l01c d3v <l01cd3v@gmail.com>
2016-02-02 18:41:52 -0800

    Add --debug flag in test command...

--- tests/test-scout2.py
+++ tests/test-scout2.py
@@ -18,7 +18,7 @@ class TestScout2Class:
     # Make sure that Scout2's default run does not crash
     #
     def test_scout2_default_run(self):
-        command = './Scout2.py --force'
+        command = './Scout2.py --force --debug'
         process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
         process.wait()
         assert process.returncode == 0
