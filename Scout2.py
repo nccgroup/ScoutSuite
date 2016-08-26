86c29d10a3555a6447f2b60073d5023b3b084866
l01c d3v <l01cd3v@gmail.com>
2016-08-25 18:43:07 -0700

    Fix issue #99

--- Scout2.py
+++ Scout2.py
@@ -135,6 +135,8 @@ def main(args):
     analyze_vpc_config(aws_config, args.ip_ranges, args.ip_ranges_key_name)
     if 'ec2' in services:
         analyze_ec2_config(aws_config['services']['ec2'], aws_config['account_id'], args.force_write)
+    if 'iam' in services:
+        analyze_iam_config(aws_config)
 
     ##### Multiple service analyzis
     if 'ec2' in services and 'iam' in services:
