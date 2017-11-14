fd35059598c21b4d2d4589233c7fb9ef455add8c
l01c d3v <l01cd3v@gmail.com>
2017-11-13 18:25:12 -0800

    Add some test code

--- tests/test-utils.py
+++ tests/test-utils.py
@@ -7,10 +7,24 @@ from AWSScout2.utils import *
 #
 class TestAWSScout2UtilsClass:
 
-    #
-    # Unit tests for get_scout2_paths
-    #
-    def test_get_scout2_paths(self):
-        assert type(get_scout2_paths('')) == tuple
-        assert get_scout2_paths('default') == ('report.html', AWSCONFIG_DIR + '/' + AWSCONFIG_FILE + '.js')
-        assert get_scout2_paths('test') == ('report-test.html', AWSCONFIG_DIR + '/' + AWSCONFIG_FILE + '-test.js')
+    def test_format_service_name(self):
+        assert (format_service_name('iAm') == 'IAM')
+        assert (format_service_name('cloudformation') == 'CloudFormation')
+
+
+    def test_get_keys(self):
+        test1 = {'a': 'b', 'c': 'd'}
+        test2 = {'a': '',  'e': 'f'}
+        get_keys(test1, test2, 'a')
+        assert (test2['a'] == 'b')
+        assert ('c' not in test2)
+        get_keys(test1, test2, 'c')
+        assert (test2['c'] == 'd')
+
+
+    def test_no_camel(self):
+        assert (no_camel('TestTest') == 'test_test')
+
+
+    def test_is_throttled(self):
+        pass
