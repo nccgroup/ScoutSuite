7e7fd623cd40852e7c87a4662c692708ee435b34
l01c d3v <l01cd3v@gmail.com>
2017-12-07 12:53:46 -0800

    Enable run of test-scout2 locally

--- tests/test-scout2.py
+++ tests/test-scout2.py
@@ -1,15 +1,33 @@
 import subprocess
 
+from opinel.utils.console import configPrintException
+from opinel.utils.credentials import read_creds_from_environment_variables
+
 #
 # Test for Scout2.py
 #
 class TestScout2Class:
 
     #
+    # Setup
+    #
+    def setup(self):
+        configPrintException(True)
+        creds = read_creds_from_environment_variables()
+        self.profile_name = 'travislike' if creds['AccessKeyId'] == None else None
+
+    def get_command(self, args):
+        command = './Scout2.py'
+        if self.profile_name:
+            command += ' --profile %s' % self.profile_name
+        command += ' %s' % args
+        return command
+
+    #
     # Make sure that Scout2 does not crash with --help
     #
     def test_scout2_help(self):
-        command = './Scout2.py --help'
+        command = self.get_command('--help')
         process = subprocess.Popen(command, shell=True, stdout=None) #subprocess.PIPE)
         process.wait()
         assert process.returncode == 0
@@ -18,7 +36,7 @@ class TestScout2Class:
     # Make sure that Scout2's default run does not crash
     #
     def test_scout2_default_run(self):
-        command = './Scout2.py --force --debug'
+        command = self.get_command('--force --debug')
         process = subprocess.Popen(command, shell=True, stdout=None) #subprocess.PIPE)
         process.wait()
         assert process.returncode == 0
