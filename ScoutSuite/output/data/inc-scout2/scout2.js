d031aa599fce921d1126d9e98dac8d705745933c
Xavier Garceau-Aranda <xavier.garceau-aranda@owasp.org>
2018-09-04 13:49:51 +0200

    Improve UI

--- ScoutSuite/output/data/inc-scout2/scout2.js
+++ ScoutSuite/output/data/inc-scout2/scout2.js
@@ -493,6 +493,28 @@ function show_main_dashboard() {
     showRowWithItems('aws_account_id');
     showRowWithItems('last_run');
     $('#section_title-h2').text('');
+    removeHash()
+}
+
+
+//
+// Remove everything from "#" in URL
+///
+function removeHash () {
+    var scrollV, scrollH, loc = window.location;
+    if ("pushState" in history)
+        history.pushState("", document.title, loc.pathname + loc.search);
+    else {
+        // Prevent scrolling by storing the page's current scroll offset
+        scrollV = document.body.scrollTop;
+        scrollH = document.body.scrollLeft;
+
+        loc.hash = "";
+
+        // Restore the scroll offset, should be flicker free
+        document.body.scrollTop = scrollV;
+        document.body.scrollLeft = scrollH;
+    }
 }
 
 
