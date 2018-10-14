f93be3e934990822219db7b723ab979a97bc9106
Xavier Garceau-Aranda <xavier.garceau-aranda@owasp.org>
2018-10-14 09:10:19 -0700

    Remove support for python 3.3, add support for python 3.5 and 3.7

--- setup.py
+++ setup.py
@@ -69,9 +69,12 @@ setup(
         'Natural Language :: English',
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4'
+        'Programming Language :: Python :: 3.5'
+        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.7'
     ],
 )
