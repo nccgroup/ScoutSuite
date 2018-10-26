a8a456d088abaa3ddac70254f4483c590fe43dd8
Xavier Garceau-Aranda <xavier.garceau-aranda@owasp.org>
2018-10-26 09:27:52 +0200

    Fix CloudSQL bugs

--- ScoutSuite/providers/gcp/services/cloudsql.py
+++ ScoutSuite/providers/gcp/services/cloudsql.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from ScoutSuite.providers.gcp.configs.base import GCPBaseConfig
+from opinel.utils.console import printError
 
-from opinel.utils.console import printError, printException, printInfo
+from ScoutSuite.providers.gcp.configs.base import GCPBaseConfig
 
 
 class CloudSQLConfig(GCPBaseConfig):
@@ -31,13 +31,15 @@ class CloudSQLConfig(GCPBaseConfig):
         instance_dict['id'] = self.get_non_provider_id(instance['name'])
         instance_dict['name'] = instance['name']
         instance_dict['project_id'] = instance['project']
-
-        instance_dict['automatic_backup_status'] = 'Enabled' if instance['settings']['backupConfiguration']['enabled'] else 'Disabled'
-        instance_dict['log_status'] = 'Enabled' if instance['settings']['backupConfiguration']['binaryLogEnabled'] else 'Disabled'
-        # TODO test this
+        instance_dict['automatic_backup_status'] = 'Enabled' if instance['settings']['backupConfiguration'][
+            'enabled'] else 'Disabled'
+        instance_dict['log_status'] = 'Enabled' \
+            if 'binaryLogEnagled' in instance['settings']['backupConfiguration'] \
+               and instance['settings']['backupConfiguration']['binaryLogEnabled'] \
+            else 'Disabled'
         instance_dict['ssl_required'] = 'Enabled' if ('requireSsl' in \
-                                                     instance['settings']['ipConfiguration'] and \
-                                                     instance['settings']['ipConfiguration']['requireSsl']) else 'Disabled'
+                                                      instance['settings']['ipConfiguration'] and \
+                                                      instance['settings']['ipConfiguration']['requireSsl']) else 'Disabled'
 
         instance_dict['backups'] = self._get_instance_backups(instance, params)
 
@@ -45,17 +47,19 @@ class CloudSQLConfig(GCPBaseConfig):
 
 
     def _get_instance_backups(self, instance, params):
-        backups = params['api_client'].backupRuns().list(project=instance['project'],
-                                                         instance=instance['name']).execute()
         try:
+            backups = params['api_client'].backupRuns().list(project=instance['project'],
+                                                             instance=instance['name']).execute()
+
             backups_dict = {}
-            for backup in backups['items']:
-                backups_dict[backup['id']] = {
-                    'backup_url': backup['selfLink'],
-                    'creation_timestamp': backup['endTime'],
-                    'status': backup['status'],
-                    'type': backup['type']
-                }
+            if 'items' in backups:
+                for backup in backups['items']:
+                    backups_dict[backup['id']] = {
+                        'backup_url': backup['selfLink'],
+                        'creation_timestamp': backup['endTime'],
+                        'status': backup['status'],
+                        'type': backup['type']
+                    }
             return backups_dict
         except Exception as e:
             printError('Failed to get backups for SQL instance %s: %s' % (instance['name'], e))
