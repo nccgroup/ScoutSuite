3a7929e25a8b291b248d9f0b9f3e57e208910092
Xavier Garceau-Aranda <xavier.garceau-aranda@nccgroup.com>
2019-01-03 09:47:56 +0100

    remove whitespace

--- ScoutSuite/__init__.py
+++ ScoutSuite/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__author__  = 'NCC Group'
+__author__ = 'NCC Group'
 __version__ = '4.0.6'
 
 AWSCONFIG  = 42
@@ -9,10 +9,10 @@ HTMLREPORT = 424242
 AWSRULESET = 42424242
 
 DEFAULT_REPORT_DIR = 'scoutsuite-report'
-AWSCONFIG_FILE  = 'scoutsuite-results/scoutsuite_results.js'
+AWSCONFIG_FILE = 'scoutsuite-results/scoutsuite_results.js'
 AWSRULESET_FILE = 'scoutsuite-results/scoutsuite_ruleset.js'
 EXCEPTIONS_FILE = 'scoutsuite-results/scoutsuite_exceptions.js'
 HTMLREPORT_FILE = 'report.html'
-GENERATOR_FILE  = 'ruleset-generator.html'
+GENERATOR_FILE = 'ruleset-generator.html'
 
-REPORT_TITLE  = 'Scout Suite Report'
+REPORT_TITLE = 'Scout Suite Report'
