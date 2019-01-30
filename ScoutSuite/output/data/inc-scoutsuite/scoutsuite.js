cccb8a28d8c17eb20b5e0794419b396a9199de9a
Antoine Boisier-Michaud <aboisiermichaud@gmail.com>
2019-01-30 08:46:34 -0500

    Removed unused method

--- ScoutSuite/output/data/inc-scoutsuite/scoutsuite.js
+++ ScoutSuite/output/data/inc-scoutsuite/scoutsuite.js
@@ -799,25 +799,6 @@ function show_main_dashboard() {
 };
 
 /**
- * Remove everything from "#" in URL
- */
-function removeHash() {
-    var scrollV, scrollH, loc = window.location;
-    if ("pushState" in history)
-        history.pushState("", document.title, loc.pathname + loc.search);
-    else {
-        // Prevent scrolling by storing the page's current scroll offset
-        scrollV = document.body.scrollTop;
-        scrollH = document.body.scrollLeft;
-        // Remove hash value
-        loc.hash = "";
-        // Restore the scroll offset, should be flicker free
-        document.body.scrollTop = scrollV;
-        document.body.scrollLeft = scrollH;
-    };
-};
-
-/**
  * Show All Resources
  * @param script_id
  */
