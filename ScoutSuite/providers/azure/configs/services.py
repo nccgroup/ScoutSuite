7808f0a1b25ba9327ee99c644cbe8a11c043f8ed
Michaël Sghaier <michael.sghaier@polymtl.ca>
2019-02-03 10:59:42 -0500

    Add support for monitor service.

--- ScoutSuite/providers/azure/configs/services.py
+++ ScoutSuite/providers/azure/configs/services.py
@@ -2,6 +2,10 @@
 
 from ScoutSuite.providers.base.configs.services import BaseServicesConfig
 from ScoutSuite.providers.azure.services.storageaccounts import StorageAccountsConfig
+try:
+    from ScoutSuite.providers.azure.services.monitor_private import MonitorConfig
+except ImportError:
+    pass
 
 
 class AzureServicesConfig(BaseServicesConfig):
@@ -9,6 +13,10 @@ class AzureServicesConfig(BaseServicesConfig):
     def __init__(self, metadata=None, thread_config=4, **kwargs):
 
         self.storageaccounts = StorageAccountsConfig(thread_config=thread_config)
+        try:
+            self.monitor = MonitorConfig(thread_config=thread_config)
+        except NameError:
+            pass
 
     def _is_provider(self, provider_name):
         return provider_name == 'azure'
