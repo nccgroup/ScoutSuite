03852414b2c2d156d3c3cf67610f8ae38a732b32
Antoine Boisier-Michaud <aboisiermichaud@gmail.com>
2019-02-04 08:22:50 -0500

    Renamed variable

--- ScoutSuite/core/processingengine.py
+++ ScoutSuite/core/processingengine.py
@@ -66,6 +66,6 @@ class ProcessingEngine(object):
                     cloud_provider.services[service][self.ruleset.rule_type][rule.key]['flagged_items'] = 0
 
     def _filter_rules(self, rules, services):
-        return { rule: rules[rule] for rule in rules if rule.split('.')[0] in services }
+        return { rule_name: rules[rule_name] for rule_name in rules if rule_name.split('.')[0] in services }
 
 
