29351df14caac527132d55fe0993603b91997e17
Michaël Sghaier <michael.sghaier@polymtl.ca>
2019-02-04 20:11:52 -0500

    Manage sql service and define a get_resource_group_name function.

--- ScoutSuite/providers/azure/utils.py
+++ ScoutSuite/providers/azure/utils.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 
+import re
+
 from opinel.utils.console import printException, printInfo
 
 from azure.mgmt.storage import StorageManagementClient
 from azure.mgmt.monitor import MonitorManagementClient
+from azure.mgmt.sql import SqlManagementClient
+
 
 def azure_connect_service(service, credentials, region_name=None):
 
@@ -12,7 +16,8 @@ def azure_connect_service(service, credentials, region_name=None):
             return StorageManagementClient(credentials.credentials, credentials.subscription_id)
         elif service == 'monitor':
             return MonitorManagementClient(credentials.credentials, credentials.subscription_id)
-
+        elif service == 'sql':
+            return SqlManagementClient(credentials.credentials, credentials.subscription_id)
         else:
             printException('Service %s not supported' % service)
             return None
@@ -20,3 +25,7 @@ def azure_connect_service(service, credentials, region_name=None):
     except Exception as e:
         printException(e)
         return None
+
+
+def get_resource_group_name(id):
+    return re.findall("/resourceGroups/(.*?)/", id)[0]
