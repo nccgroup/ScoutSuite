7efc3aeb1aaa1437a06988d0a49565ae3f78fbd2
Philippe Dugre <philippe.dugre@polymtl.ca>
2019-02-10 15:08:55 -0500

    Azure CIS2.19: Security contact email not set

--- ScoutSuite/providers/azure/services/securitycenter.py
+++ ScoutSuite/providers/azure/services/securitycenter.py
@@ -6,11 +6,15 @@ from ScoutSuite.providers.azure.configs.base import AzureBaseConfig
 class SecurityCenterConfig(AzureBaseConfig):
     targets = (
         ('pricings', 'Pricings', 'list', {}, False),
+        ('security_contacts', 'Security Contacts', 'list', {}, False)
     )
 
     def __init__(self, thread_config):
         self.pricings = {}
         self.pricings_count = 0
+
+        self.security_contacts = {}
+        self.security_contacts_count = 0
 
         super(SecurityCenterConfig, self).__init__(thread_config)
 
@@ -20,3 +24,16 @@ class SecurityCenterConfig(AzureBaseConfig):
                         'pricing_tier': pricing.pricing_tier}
 
         self.pricings[pricing_dict['id']] = pricing_dict
+
+    def parse_security_contacts(self, security_contact, params):
+        security_contact_dict = {
+            'id': security_contact.id,
+            'name': security_contact.name,
+            'email': security_contact.email,
+            'phone': security_contact.phone,
+            'alert_notifications': security_contact.alert_notifications,
+            'alerts_to_admins': security_contact.alerts_to_admins,
+            #'additional_properties': security_contact.additional_properties,
+        }
+
+        self.security_contacts[security_contact_dict['id']] = security_contact_dict
