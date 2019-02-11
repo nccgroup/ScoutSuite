6616aa0c212c26014beaa8a2c0e864545e6420b4
Philippe Dugre <philippe.dugre@polymtl.ca>
2019-02-11 14:02:17 -0500

    [Azure/SecurityCenter] Reverted dictionnary assignments

--- ScoutSuite/providers/azure/services/securitycenter.py
+++ ScoutSuite/providers/azure/services/securitycenter.py
@@ -23,30 +23,29 @@ class SecurityCenterConfig(AzureBaseConfig):
         super(SecurityCenterConfig, self).__init__(thread_config)
 
     def parse_pricings(self, pricing, params):
-        pricing_dict = {'id': pricing.id,
-                        'name': pricing.name,
-                        'pricing_tier': pricing.pricing_tier}
+        pricing_dict = {}
+        pricing_dict['id'] = pricing.id
+        pricing_dict['name'] = pricing.name
+        pricing_dict['pricing_tier'] = pricing.pricing_tier
 
         self.pricings[pricing_dict['id']] = pricing_dict
 
     def parse_security_contacts(self, security_contact, params):
-        security_contact_dict = {
-            'id': security_contact.id,
-            'name': security_contact.name,
-            'email': security_contact.email,
-            'phone': security_contact.phone,
-            'alert_notifications': security_contact.alert_notifications,
-            'alerts_to_admins': security_contact.alerts_to_admins,
-            'additional_properties': security_contact.additional_properties,
-        }
+        security_contact_dict = {}
+        security_contact_dict['id'] = security_contact.id
+        security_contact_dict['name'] = security_contact.name
+        security_contact_dict['email'] = security_contact.email
+        security_contact_dict['phone'] = security_contact.phone
+        security_contact_dict['alert_notifications'] = security_contact.alert_notifications
+        security_contact_dict['alerts_to_admins'] = security_contact.alerts_to_admins
+        security_contact_dict['additional_properties'] = security_contact.additional_properties
 
         self.security_contacts[security_contact_dict['id']] = security_contact_dict
 
     def parse_auto_provisioning_settings(self, auto_provisioning_setting, params):
-        auto_provisioning_setting_dict = {
-            'id': auto_provisioning_setting.id,
-            'name': auto_provisioning_setting.name,
-            'auto_provision': auto_provisioning_setting.auto_provision
-        }
+        auto_provisioning_setting_dict = {}
+        auto_provisioning_setting_dict['id'] = auto_provisioning_setting.id
+        auto_provisioning_setting_dict['name'] = auto_provisioning_setting.name
+        auto_provisioning_setting_dict['auto_provision'] = auto_provisioning_setting.auto_provision
 
         self.auto_provisioning_settings[auto_provisioning_setting_dict['id']] = auto_provisioning_setting_dict
