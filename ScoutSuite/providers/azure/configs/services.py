4d68189b288461cf285b4e0b79937b1cfac31d67
Michaël Sghaier <michael.sghaier@polymtl.ca>
2019-02-12 12:38:52 -0500

    Make Application Gateway support private.

--- ScoutSuite/providers/azure/configs/services.py
+++ ScoutSuite/providers/azure/configs/services.py
@@ -4,7 +4,10 @@ from ScoutSuite.providers.base.configs.services import BaseServicesConfig
 from ScoutSuite.providers.azure.services.storageaccounts import StorageAccountsConfig
 from ScoutSuite.providers.azure.services.monitor import MonitorConfig
 from ScoutSuite.providers.azure.services.sqldatabase import SQLDatabaseConfig
-from ScoutSuite.providers.azure.services.appgateway import AppGatewayConfig
+try:
+    from ScoutSuite.providers.azure.services.appgateway_private import AppGatewayConfig
+except ImportError:
+    pass
 
 
 class AzureServicesConfig(BaseServicesConfig):
@@ -14,7 +17,11 @@ class AzureServicesConfig(BaseServicesConfig):
         self.storageaccounts = StorageAccountsConfig(thread_config=thread_config)
         self.monitor = MonitorConfig(thread_config=thread_config)
         self.sqldatabase = SQLDatabaseConfig(thread_config=thread_config)
-        self.appgateway = AppGatewayConfig(thread_config=thread_config)
+
+        try:
+            self.appgateway = AppGatewayConfig(thread_config=thread_config)
+        except NameError:
+            pass
 
     def _is_provider(self, provider_name):
         return provider_name == 'azure'
