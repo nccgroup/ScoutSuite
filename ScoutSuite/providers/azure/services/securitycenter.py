dc76bd0c086a0edea78337d0448221cc10fc2d07
Philippe Dugre <philippe.dugre@polymtl.ca>
2019-02-13 10:39:11 -0500

    [Azure/SecurityCenter] Now using True/False instead of On/Off

--- ScoutSuite/providers/azure/services/securitycenter.py
+++ ScoutSuite/providers/azure/services/securitycenter.py
@@ -36,8 +36,8 @@ class SecurityCenterConfig(AzureBaseConfig):
         security_contact_dict['name'] = security_contact.name
         security_contact_dict['email'] = security_contact.email
         security_contact_dict['phone'] = security_contact.phone
-        security_contact_dict['alert_notifications'] = security_contact.alert_notifications
-        security_contact_dict['alerts_to_admins'] = security_contact.alerts_to_admins
+        security_contact_dict['alert_notifications'] = security_contact.alert_notifications == "On"
+        security_contact_dict['alerts_to_admins'] = security_contact.alerts_to_admins == "On"
         security_contact_dict['additional_properties'] = security_contact.additional_properties
 
         self.security_contacts[security_contact_dict['id']] = security_contact_dict
