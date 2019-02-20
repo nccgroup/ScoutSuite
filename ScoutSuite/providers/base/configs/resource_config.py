d38e3b7c64cfd72b34c76fa331c0d33fd4033d50
Antoine Boisier-Michaud <aboisiermichaud@gmail.com>
2019-02-20 11:16:48 -0500

    Renamed thread_config to _thread_config, since it's private

--- ScoutSuite/providers/base/configs/resource_config.py
+++ ScoutSuite/providers/base/configs/resource_config.py
@@ -2,7 +2,7 @@
 class ResourceConfig(object):
 
     def __init__(self, thread_config=4):
-        self.thread_config = thread_config
+        self._thread_config = thread_config
 
     async def fetch_all(self, credentials, regions=None, partition_name='aws', targets=None):
         raise NotImplementedError()
