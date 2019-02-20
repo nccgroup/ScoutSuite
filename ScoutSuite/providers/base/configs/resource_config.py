76372ea333f4eabc7491e9e51ecafd0dbc23a78f
Antoine Boisier-Michaud <aboisiermichaud@gmail.com>
2019-02-20 15:07:51 -0500

    Removed thread config

--- ScoutSuite/providers/base/configs/resource_config.py
+++ ScoutSuite/providers/base/configs/resource_config.py
@@ -1,9 +1,6 @@
 
 class ResourceConfig(dict):
 
-    def __init__(self, thread_config=4):
-        self._thread_config = thread_config
-
     async def fetch_all(self, credentials, regions=None, partition_name='aws', targets=None):
         raise NotImplementedError()
 
