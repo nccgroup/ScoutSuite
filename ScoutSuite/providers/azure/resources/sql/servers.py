7ea7068ae8e34b9995ebb40faa125514f8ae9b0f
Michaël Sghaier <michael.sghaier@polymtl.ca>
2019-02-20 23:47:07 -0500

    Modify base class and remove parsing processes of children.

--- ScoutSuite/providers/azure/resources/sql/servers.py
+++ ScoutSuite/providers/azure/resources/sql/servers.py
@@ -2,57 +2,37 @@
 
 from azure.mgmt.sql import SqlManagementClient
 
-from ScoutSuite.providers.base.configs.resource_config import ResourceConfig
+from ScoutSuite.providers.base.configs.resources import Resources
 from ScoutSuite.providers.azure.utils import get_resource_group_name
 
-from .databases import DatabasesConfig
-from .server_azure_ad_administrators import ServerAzureAdAdministratorsConfig
+from .databases import Databases
+from .server_azure_ad_administrators import ServerAzureAdAdministrators
 from ..utils import get_non_provider_id
 
 
-class ServersConfig(ResourceConfig):
-    # the following static methods will be used to parse the config of each resource nested in a server config
-    # (defined in 'children' attribute below):
-    @staticmethod
-    def _parse_databases_config(config):
-        return {
-            'databases': config.databases
-        }
-
-    @staticmethod
-    def _parse_azure_ad_administrators_config(config):
-        return {
-            'ad_admin_configured': config.value is not None
-        }
-
-    # register children resources:
+class Servers(Resources):
     children = [
-        (DatabasesConfig, _parse_databases_config),
-        (ServerAzureAdAdministratorsConfig, _parse_azure_ad_administrators_config)
+        Databases,
+        ServerAzureAdAdministrators,
     ]
 
-    def __init__(self):
-        self.servers = {}
-
-    async def fetch_all(self, credentials, regions=None, partition_name='aws', targets=None):
+    async def fetch_all(self, credentials):
         # sdk container:
         api = SqlManagementClient(credentials.credentials, credentials.subscription_id)
 
-        # for server in await api.servers.list():
+        self['servers'] = {}
+        # TODO: for server in await api.servers.list():
         for server in api.servers.list():
-            # parsing:
-
             id = get_non_provider_id(server.id)
             resource_group_name = get_resource_group_name(server.id)
 
-            self.servers[id] = {
+            self['servers'][id] = {
                 'id': id,
                 'name': server.name
             }
 
             # put the following code in a fetch_children() parent method (typical method for a composite node)?
-            for (resource_config, resource_parser) in self.children:
-                resource = resource_config(resource_group_name, server.name)
-                await resource.fetch_all(credentials)
-                for k, v in resource_parser.__func__(resource).items():
-                    self.servers[id][k] = v
+            for resources_class in self.children:
+                resources = resources_class(resource_group_name, server.name)
+                await resources.fetch_all(credentials)
+                self['servers'][id].update(resources)
