a1f5ace55f27b08ce99aeb5d44a2a2296806e068
Michaël Sghaier <michael.sghaier@polymtl.ca>
2019-02-20 23:46:04 -0500

    Modify base class and add parsing process.

--- ScoutSuite/providers/azure/resources/sql/replication_links.py
+++ ScoutSuite/providers/azure/resources/sql/replication_links.py
@@ -2,22 +2,26 @@
 
 from azure.mgmt.sql import SqlManagementClient
 
-from ScoutSuite.providers.base.configs.resource_config import ResourceConfig
+from ScoutSuite.providers.base.configs.resources import Resources
 
 
-class ReplicationLinksConfig(ResourceConfig):
+class ReplicationLinks(Resources):
 
     def __init__(self, resource_group_name, server_name, database_name):
         self.resource_group_name = resource_group_name
         self.server_name = server_name
         self.database_name = database_name
 
-        self.value = None
-
     async def fetch_all(self, credentials, regions=None, partition_name='aws', targets=None):
         # sdk container:
         api = SqlManagementClient(credentials.credentials, credentials.subscription_id)
 
-        self.value =\
+        links =\
             list(api.replication_links.list_by_database(self.resource_group_name, self.server_name, self.database_name))
             # TODO: await api.replication_links.list_by_databases(self.resource_group_name, self.server_name, self.database_name)
+
+        self['replication_configured'] = self._is_replication_configured(links)
+
+    @staticmethod
+    def _is_replication_configured(links):
+        return len(links) > 0
