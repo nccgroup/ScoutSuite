efaebfd11613e83caf721edbd8c2b61d949e7eae
Michaël Sghaier <michael.sghaier@polymtl.ca>
2019-02-20 22:13:17 -0500

    Rename ResourceConfig class to Resources, make it abstract and make fetch_all method more general.

--- ScoutSuite/providers/base/configs/resource_config.py
+++
@@ -1,8 +0,0 @@
-
-class ResourceConfig(dict):
-
-    async def fetch_all(self, credentials, regions=None, partition_name='aws', targets=None):
-        raise NotImplementedError()
-
-    async def finalize(self):
-        pass
