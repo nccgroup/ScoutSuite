9eaee5e57c98239a73d6088d36b2abbe2e1d6256
Michaël Sghaier <michael.sghaier@polymtl.ca>
2019-02-20 23:46:16 -0500

    Modify base class and add parsing process.

--- ScoutSuite/providers/azure/resources/sql/server_azure_ad_administrators.py
+++ ScoutSuite/providers/azure/resources/sql/server_azure_ad_administrators.py
@@ -3,24 +3,23 @@
 from azure.mgmt.sql import SqlManagementClient
 from msrestazure.azure_exceptions import CloudError
 
-from ScoutSuite.providers.base.configs.resource_config import ResourceConfig
+from ScoutSuite.providers.base.configs.resources import Resources
 
 
-class ServerAzureAdAdministratorsConfig(ResourceConfig):
+class ServerAzureAdAdministrators(Resources):
 
     def __init__(self, resource_group_name, server_name):
         self.resource_group_name = resource_group_name
         self.server_name = server_name
 
-        self.value = None
-
-    async def fetch_all(self, credentials, regions=None, partition_name='aws', targets=None):
+    async def fetch_all(self, credentials):
         # sdk container:
         api = SqlManagementClient(credentials.credentials, credentials.subscription_id)
 
         try:
-            self.value = \
+            admins = \
                 api.server_azure_ad_administrators.get(self.resource_group_name, self.server_name)
                 # TODO: await api.server_azure_ad_administrators.get(self.resource_group_name, self.server_name)
+            self['ad_admin_configured'] = True
         except CloudError:  # no ad admin configured returns a 404 error
-            pass
+            self['ad_admin_configured'] = False
