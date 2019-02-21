40b4ff2206e9dce78100922818fce862bb329edc
Michaël Sghaier <michael.sghaier@polymtl.ca>
2019-02-20 23:45:53 -0500

    Modify base class and add parsing process.

--- ScoutSuite/providers/azure/resources/sql/database_threat_detection_policies.py
+++ ScoutSuite/providers/azure/resources/sql/database_threat_detection_policies.py
@@ -2,22 +2,26 @@
 
 from azure.mgmt.sql import SqlManagementClient
 
-from ScoutSuite.providers.base.configs.resource_config import ResourceConfig
+from ScoutSuite.providers.base.configs.resources import Resources
 
 
-class DatabaseThreatDetectionPoliciesConfig(ResourceConfig):
+class DatabaseThreatDetectionPolicies(Resources):
 
     def __init__(self, resource_group_name, server_name, database_name):
         self.resource_group_name = resource_group_name
         self.server_name = server_name
         self.database_name = database_name
 
-        self.value = None
-
-    async def fetch_all(self, credentials, regions=None, partition_name='aws', targets=None):
+    async def fetch_all(self, credentials):
         # sdk container:
         api = SqlManagementClient(credentials.credentials, credentials.subscription_id)
 
-        self.value =\
+        policies =\
             api.database_threat_detection_policies.get(self.resource_group_name, self.server_name, self.database_name)
             # TODO: await api.transparent_data_encryptions.get(self.resource_group_name, self.server_name, self.database_name)
+
+        self['threat_detection_enabled'] = self._is_threat_detection_enabled(policies)
+
+    @staticmethod
+    def _is_threat_detection_enabled(policies):
+        return policies.state == "Enabled"
