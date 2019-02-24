360245eecd8c7b12c5e54e30260f68e25f2b8de3
Michaël Sghaier <michael.sghaier@polymtl.ca>
2019-02-24 16:14:11 -0500

    Make ReplicationLinks AzureSimpleResources.

--- ScoutSuite/providers/azure/resources/sql/replication_links.py
+++ ScoutSuite/providers/azure/resources/sql/replication_links.py
@@ -2,26 +2,21 @@
 
 from azure.mgmt.sql import SqlManagementClient
 
-from ScoutSuite.providers.base.configs.resources import Resources
+from ..resources import AzureSimpleResources
 
 
-class ReplicationLinks(Resources):
+class ReplicationLinks(AzureSimpleResources):
 
     def __init__(self, resource_group_name, server_name, database_name):
         self.resource_group_name = resource_group_name
         self.server_name = server_name
         self.database_name = database_name
 
-    async def fetch_all(self, credentials, regions=None, partition_name='aws', targets=None):
-        # sdk container:
+    # TODO: make it really async.
+    async def get_resources_from_api(self, credentials):
         api = SqlManagementClient(credentials.credentials, credentials.subscription_id)
+        return list(api.replication_links.list_by_database(
+            self.resource_group_name, self.server_name, self.database_name))
 
-        links =\
-            list(api.replication_links.list_by_database(self.resource_group_name, self.server_name, self.database_name))
-            # TODO: await api.replication_links.list_by_databases(self.resource_group_name, self.server_name, self.database_name)
-
-        self['replication_configured'] = self._is_replication_configured(links)
-
-    @staticmethod
-    def _is_replication_configured(links):
-        return len(links) > 0
+    def parse_resource(self, links):
+        return 'replication_configured', len(links) > 0
