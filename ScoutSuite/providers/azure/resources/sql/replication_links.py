61460b4eb6e03bc1904fb8c36500af60654be496
Michaël Sghaier <michael.sghaier@polymtl.ca>
2019-02-24 16:36:43 -0500

    Use Azure API through a facade.

--- ScoutSuite/providers/azure/resources/sql/replication_links.py
+++ ScoutSuite/providers/azure/resources/sql/replication_links.py
@@ -7,15 +7,15 @@ from ..resources import AzureSimpleResources
 
 class ReplicationLinks(AzureSimpleResources):
 
-    def __init__(self, resource_group_name, server_name, database_name):
+    def __init__(self, resource_group_name, server_name, database_name, facade):
         self.resource_group_name = resource_group_name
         self.server_name = server_name
         self.database_name = database_name
+        self.facade = facade
 
     # TODO: make it really async.
-    async def get_resources_from_api(self, credentials):
-        api = SqlManagementClient(credentials.credentials, credentials.subscription_id)
-        return list(api.replication_links.list_by_database(
+    async def get_resources_from_api(self):
+        return list(self.facade.replication_links.list_by_database(
             self.resource_group_name, self.server_name, self.database_name))
 
     def parse_resource(self, links):
