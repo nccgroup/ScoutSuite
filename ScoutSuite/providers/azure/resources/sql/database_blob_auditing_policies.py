891e5be663bf5d649c4b30e90a29ec1e188204b3
Michaël Sghaier <michael.sghaier@polymtl.ca>
2019-02-24 16:36:03 -0500

    Use Azure API through a facade.

--- ScoutSuite/providers/azure/resources/sql/database_blob_auditing_policies.py
+++ ScoutSuite/providers/azure/resources/sql/database_blob_auditing_policies.py
@@ -7,15 +7,15 @@ from ..resources import AzureSimpleResources
 
 class DatabaseBlobAuditingPolicies(AzureSimpleResources):
 
-    def __init__(self, resource_group_name, server_name, database_name):
+    def __init__(self, resource_group_name, server_name, database_name, facade):
         self.resource_group_name = resource_group_name
         self.server_name = server_name
         self.database_name = database_name
+        self.facade = facade
 
     # TODO: make it really async.
-    async def get_resources_from_api(self, credentials):
-        api = SqlManagementClient(credentials.credentials, credentials.subscription_id)
-        return api.database_blob_auditing_policies.get(
+    async def get_resources_from_api(self):
+        return self.facade.database_blob_auditing_policies.get(
             self.resource_group_name, self.server_name, self.database_name)
 
     def parse_resource(self, policies):
