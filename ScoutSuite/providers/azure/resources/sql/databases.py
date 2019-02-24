cdfd6e5daf23e893280a430dab60d390a5abd138
Michaël Sghaier <michael.sghaier@polymtl.ca>
2019-02-24 16:16:08 -0500

    Make Databases AzureCompositeResources.

--- ScoutSuite/providers/azure/resources/sql/databases.py
+++ ScoutSuite/providers/azure/resources/sql/databases.py
@@ -2,7 +2,7 @@
 
 from azure.mgmt.sql import SqlManagementClient
 
-from ScoutSuite.providers.base.configs.resources import Resources
+from ..resources import AzureCompositeResources
 
 from .database_blob_auditing_policies import DatabaseBlobAuditingPolicies
 from .database_threat_detection_policies import DatabaseThreatDetectionPolicies
@@ -10,7 +10,7 @@ from .replication_links import ReplicationLinks
 from .transparent_data_encryptions import TransparentDataEncryptions
 
 
-class Databases(Resources):
+class Databases(AzureCompositeResources):
     children = [
         DatabaseBlobAuditingPolicies,
         DatabaseThreatDetectionPolicies,
@@ -22,19 +22,19 @@ class Databases(Resources):
         self.resource_group_name = resource_group_name
         self.server_name = server_name
 
+    # TODO: make it really async.
     async def fetch_all(self, credentials):
-        # sdk container:
         api = SqlManagementClient(credentials.credentials, credentials.subscription_id)
 
         self['databases'] = {}
-        # TODO: for db in await api.databases.list_by_server(self.resource_group_name, self.server_name):
         for db in api.databases.list_by_server(self.resource_group_name, self.server_name):
             self['databases'][db.name] = {
                 'id': db.name,
             }
-
-            # put the following code in a fetch_children() parent method (typical method for a composite node)?
-            for resources_class in self.simple_children:
-                resources = resources_class(self.resource_group_name, self.server_name, db.name)
-                await resources.fetch_all(credentials)
-                self['databases'][db.name].update(resources)
+            await self.fetch_children(
+                parent=self['databases'][db.name],
+                resource_group_name=self.resource_group_name,
+                server_name=self.server_name,
+                database_name=db.name,
+                credentials=credentials
+            )
