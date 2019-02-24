2a3558ee2d29919bc96bc507dba66dbe7327c5a5
Michaël Sghaier <michael.sghaier@polymtl.ca>
2019-02-24 16:13:50 -0500

    Make DatabaseThreatDetectionPolicies AzureSimpleResources.

--- ScoutSuite/providers/azure/resources/sql/database_threat_detection_policies.py
+++ ScoutSuite/providers/azure/resources/sql/database_threat_detection_policies.py
@@ -2,26 +2,21 @@
 
 from azure.mgmt.sql import SqlManagementClient
 
-from ScoutSuite.providers.base.configs.resources import Resources
+from ..resources import AzureSimpleResources
 
 
-class DatabaseThreatDetectionPolicies(Resources):
+class DatabaseThreatDetectionPolicies(AzureSimpleResources):
 
     def __init__(self, resource_group_name, server_name, database_name):
         self.resource_group_name = resource_group_name
         self.server_name = server_name
         self.database_name = database_name
 
-    async def fetch_all(self, credentials):
-        # sdk container:
+    # TODO: make it really async.
+    async def get_resources_from_api(self, credentials):
         api = SqlManagementClient(credentials.credentials, credentials.subscription_id)
+        return api.database_threat_detection_policies.get(
+            self.resource_group_name, self.server_name, self.database_name)
 
-        policies =\
-            api.database_threat_detection_policies.get(self.resource_group_name, self.server_name, self.database_name)
-            # TODO: await api.transparent_data_encryptions.get(self.resource_group_name, self.server_name, self.database_name)
-
-        self['threat_detection_enabled'] = self._is_threat_detection_enabled(policies)
-
-    @staticmethod
-    def _is_threat_detection_enabled(policies):
-        return policies.state == "Enabled"
+    def parse_resource(self, policies):
+        return 'threat_detection_enabled', policies.state == "Enabled"
