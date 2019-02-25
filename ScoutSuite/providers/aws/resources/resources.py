2d644e6ebb7fa17581d6694b2c93f13b018359f8
Antoine Boisier-Michaud <aboisiermichaud@gmail.com>
2019-02-25 10:17:52 -0500

    Implemented AWSSimpleResource and AWSCompositeResource

---
+++ ScoutSuite/providers/aws/resources/resources.py
@@ -0,0 +1,28 @@
+from ScoutSuite.providers.base.configs.resources import SimpleResources
+from ScoutSuite.providers.base.configs.resources import CompositeResources
+from ScoutSuite.providers.aws.facade.facade import AWSFacade
+from opinel.utils.aws import get_aws_account_id
+import abc
+
+class AWSSimpleResources(SimpleResources, metaclass=abc.ABCMeta):
+    def __init__(self, scope):
+        self.scope = scope
+        self.facade = AWSFacade()
+
+    async def fetch_all(self):
+        raw_resources = await self.get_resources_from_api()
+        for raw_resource in raw_resources:
+            name, resource = self.parse_resource(raw_resource)
+            self[name] = resource
+
+class AWSCompositeResources(CompositeResources, metaclass=abc.ABCMeta):
+
+    # TODO: get rid of the credentials.
+    async def fetch_children(self, parent, **kwargs):
+        for child_class, key in self.children:
+            child = child_class(**kwargs)
+            await child.fetch_all()
+            if parent.get(key) is None: 
+                parent[key] = {}
+            parent[key].update(child)
+            parent[key + '_count'] = len(child)
