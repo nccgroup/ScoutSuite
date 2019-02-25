f1c35dc3e9b3f4f0df3e519f29fbd4a8f1033c8f
Michaël Sghaier <michael.sghaier@polymtl.ca>
2019-02-24 22:42:19 -0500

    Add docstrings.

--- ScoutSuite/providers/base/configs/resources.py
+++ ScoutSuite/providers/base/configs/resources.py
@@ -1,34 +1,83 @@
 # -*- coding: utf-8 -*-
+"""
+This module provides some abstract classes for representing a hierarchical structure.
+Especially since all cloud providers (AWS, Azure and GCP for now) organize their resources (virtual machines,
+databases, load balancers, user accounts and so on...) with some kind of hierarchy, these classes may be
+used to reflect that.
+"""
 
 import abc
 
 
 class Resources(dict, metaclass=abc.ABCMeta):
 
+    """
+    This is the base class of a hierarchical structure. Everything is basically `Resources`.
+    It stores in its internal dictionary instances of a given type of resources, with instance ids as keys and
+    instance configurations (which store other nested resources) as values.
+    """
+
     @abc.abstractmethod
     async def fetch_all(self, **kwargs):
+        """Fetches, parses and stores instances of a given type of resources from a cloud provider API.
+
+        :param kwargs:
+        :return:
+        """
         raise NotImplementedError()
-
-
-class SimpleResources(Resources, metaclass=abc.ABCMeta):
-
-    @abc.abstractmethod
-    def parse_resource(self, resource):
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    async def get_resources_from_api(self, **kwargs):
-        raise NotImplementedError
 
 
 class CompositeResources(Resources, metaclass=abc.ABCMeta):
 
-    # The following enforces that classes which inherits from CompositeResources define a `children` attribute:
+    """
+    This class represents a node in the hierarchical structure.
+    As inherited from `Resources`, it still stores instances of a given type of resources internally but
+    also store some kind of nested resources referred as its 'children'.
+    """
+
     @property
     @abc.abstractmethod
     def children(self):
+        """A class that inherits from 'CompositeResources' should define a 'children' attribute, typically a list of
+         `Resources` classes. That is enforced by this abstract property.
+
+        :return:
+        """
         raise NotImplementedError
 
     @abc.abstractmethod
     async def fetch_children(self, **kwargs):
+        """Fetches, parses and stores instances of nested resources included in a `CompositeResources` and defined
+        in the 'children' attribute.
+
+        :param kwargs:
+        :return:
+        """
         raise NotImplementedError
+
+
+class SimpleResources(Resources, metaclass=abc.ABCMeta):
+
+    """
+    This class represents a leaf in the hierarchical structure.
+    As inherited from `Resources`, it still stores instances of a given type of resources internally.
+    """
+
+    @abc.abstractmethod
+    async def get_resources_from_api(self, **kwargs):
+        """ Fetches resources from a cloud provider API.
+
+        :param kwargs:
+        :return:
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def parse_resource(self, resource):
+        """ Parses the retrieved resources.
+
+        :param resource:
+        :return:
+        """
+        raise NotImplementedError
+
