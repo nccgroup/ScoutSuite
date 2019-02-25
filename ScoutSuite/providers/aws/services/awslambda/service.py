46fa3fbb2f41dd0b96268f42adc3d5f86951f397
Antoine Boisier-Michaud <aboisiermichaud@gmail.com>
2019-02-25 10:18:48 -0500

    Integrated AWSSimpleResources in AWSLambda service

--- ScoutSuite/providers/aws/services/awslambda/service.py
+++ ScoutSuite/providers/aws/services/awslambda/service.py
@@ -1,40 +1,30 @@
 # -*- coding: utf-8 -*-
-from ScoutSuite.providers.base.configs.resources import Resources
-from ScoutSuite.providers.aws.configs.regions_config import Regions
+from ScoutSuite.providers.aws.resources.resources import Regions, AWSSimpleResources
 from ScoutSuite.providers.aws.facade.facade import AWSFacade
 from opinel.utils.aws import build_region_list
 
-
-class Lambdas(Regions):
-    def __init__(self):
-        super(Lambdas, self).__init__('lambda')
-
-    # TODO: Remove the credentials parameter. We had to keep it for compatibility
-    async def fetch_all(self, credentials=None, regions=None, partition_name='aws'):
-        await super(Lambdas, self).fetch_all(
-            chosen_regions=regions,
-            partition_name=partition_name
-        )
-
-        for region in self['regions']:
-            self['regions'][region] = await RegionalLambdas().fetch_all(region=region)
-
-
-class RegionalLambdas(Resources):
-    async def fetch_all(self, region):
-        # TODO: Should be injected
-        facade = AWSFacade()
-
-        functions = {}
-        for raw_function in facade.awslambda.get_functions(region):
-            name, function = self.parse_function(raw_function)
-            functions[name] = function
-
-        self['functions_count'] = len(functions)
-        self['functions'] = functions
-        return self
+class RegionalLambdas(AWSSimpleResources):
+    async def get_resources_from_api(self):
+        return self.facade.awslambda.get_functions(self.scope['region'])
+        
+    def parse_resource(self, raw_function):
+        raw_function['name'] = raw_function.pop('FunctionName')
+        return (raw_function['name'], raw_function)
 
     @staticmethod
     def parse_function(function):
         function['name'] = function.pop('FunctionName')
         return (function['name'], function)
+
+
+class Lambdas(Regions):
+    children = [
+        (RegionalLambdas, 'functions')
+    ]
+
+    def __init__(self):
+        super(Lambdas, self).__init__('lambda')
+
+    # TODO: Remove the credentials parameter. We had to keep it for compatibility
+    async def fetch_all(self, credentials=None, regions=None, partition_name='aws'):
+        await super(Lambdas, self).fetch_all(credentials, regions, partition_name)
