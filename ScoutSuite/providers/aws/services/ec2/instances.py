4ee7ad73b41cbfd73466d75db71da6b0319a2a11
Antoine Boisier-Michaud <aboisiermichaud@gmail.com>
2019-02-25 10:20:34 -0500

    Integrated AWSSimpleResource to EC2Instances

--- ScoutSuite/providers/aws/services/ec2/instances.py
+++ ScoutSuite/providers/aws/services/ec2/instances.py
@@ -1,20 +1,19 @@
-from ScoutSuite.providers.aws.configs.regions_config import ScopedResources
+from ScoutSuite.providers.aws.resources.resources import AWSSimpleResources
 from ScoutSuite.providers.aws.facade.facade import AWSFacade
 from opinel.utils.aws import get_name
 from ScoutSuite.utils import get_keys
 
 
-class EC2Instances(ScopedResources):
-    def __init__(self, region):
-        self.region = region
-        self.facade = AWSFacade()
-
+class EC2Instances(AWSSimpleResources):
+    async def get_resources_from_api(self):
+        return self.facade.ec2.get_instances(self.scope['region'], self.scope['vpc'])
+        
     def parse_resource(self, raw_instance):
         instance = {}
         id = raw_instance['InstanceId']
         instance['id'] = id
         instance['monitoring_enabled'] = raw_instance['Monitoring']['State'] == 'enabled'
-        instance['user_data'] = self.facade.ec2.get_instance_user_data(self.region, id)
+        instance['user_data'] = self.facade.ec2.get_instance_user_data(self.scope['region'], id)
 
         # TODO: Those methods are slightly sketchy in my opinion (get methods which set stuff in a dictionary, say what)
         get_name(raw_instance, instance, 'InstanceId')
@@ -27,6 +26,3 @@ class EC2Instances(ScopedResources):
             instance['network_interfaces'][eni['NetworkInterfaceId']] = nic
 
         return id, instance
-
-    async def get_resources_in_scope(self, vpcs): 
-        return self.facade.ec2.get_instances(self.region, vpcs)
