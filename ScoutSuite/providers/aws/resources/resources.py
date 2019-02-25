9d1c05c8b0c633d5d7ec4e59c647b13629b48919
Antoine Boisier-Michaud <aboisiermichaud@gmail.com>
2019-02-25 10:18:12 -0500

    Implemented Regions using AWSCompositeResource

--- ScoutSuite/providers/aws/resources/resources.py
+++ ScoutSuite/providers/aws/resources/resources.py
@@ -26,3 +26,29 @@ class AWSCompositeResources(CompositeResources, metaclass=abc.ABCMeta):
                 parent[key] = {}
             parent[key].update(child)
             parent[key + '_count'] = len(child)
+
+class Regions(AWSCompositeResources, metaclass=abc.ABCMeta):
+    def __init__(self, service):
+        self.service = service
+        # TODO: Should be injected
+        self.facade = AWSFacade()
+
+    async def fetch_all(self, credentials, chosen_regions=None, partition_name='aws'):
+        self['regions'] = {}
+        for region in await self.facade.build_region_list(self.service, chosen_regions, partition_name):
+            # TODO: Do we really need id, region AND name? 
+            self['regions'][region] = {
+                'id': region,
+                'region': region,
+                'name': region
+            }
+
+
+            await self.fetch_children(self['regions'][region], scope={'region': region, 'owner_id': get_aws_account_id(credentials)})
+
+        self._set_counts()
+
+    def _set_counts(self):
+        self['regions_count'] = len(self['regions'])
+        for _, key in self.children:
+            self[key + '_count'] = sum([region[key + '_count'] for region in self['regions'].values()])
