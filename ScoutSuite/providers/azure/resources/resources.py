57a53fac654359ea74e9644fa00d634a33fe90bd
Michaël Sghaier <michael.sghaier@polymtl.ca>
2019-03-03 12:36:31 -0500

    Get rid of AzureSimpleResources and update AzureCompositeResources.

--- ScoutSuite/providers/azure/resources/resources.py
+++ ScoutSuite/providers/azure/resources/resources.py
@@ -2,22 +2,13 @@
 
 import abc
 
-from ScoutSuite.providers.base.configs.resources import SimpleResources
 from ScoutSuite.providers.base.configs.resources import CompositeResources
-
-
-class AzureSimpleResources(SimpleResources, metaclass=abc.ABCMeta):
-
-    async def fetch_all(self):
-        raw_resource = await self.get_resources_from_api()
-        self.parse_resource(raw_resource)
 
 
 class AzureCompositeResources(CompositeResources, metaclass=abc.ABCMeta):
 
-    # TODO: get rid of the credentials.
-    async def fetch_children(self, parent, **kwargs):
-        for child_class in self.children:
+    async def _fetch_children(self, parent, **kwargs):
+        for child_class in self._children:
             child = child_class(**kwargs)
             await child.fetch_all()
             parent.update(child)
