9a4fd1383a50bf9fd001d058e015930106435450
Michaël Sghaier <michael.sghaier@polymtl.ca>
2019-03-03 12:38:04 -0500

    Update replication_links.py

--- ScoutSuite/providers/azure/resources/sql/replication_links.py
+++ ScoutSuite/providers/azure/resources/sql/replication_links.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from ..resources import AzureSimpleResources
+from ScoutSuite.providers.base.configs.resources import Resources
 
 
-class ReplicationLinks(AzureSimpleResources):
+class ReplicationLinks(Resources):
 
     def __init__(self, resource_group_name, server_name, database_name, facade):
         self.resource_group_name = resource_group_name
@@ -12,11 +12,12 @@ class ReplicationLinks(AzureSimpleResources):
         self.facade = facade
 
     # TODO: make it really async.
-    async def get_resources_from_api(self):
-        return list(self.facade.replication_links.list_by_database(
+    async def fetch_all(self):
+        links = list(self.facade.replication_links.list_by_database(
             self.resource_group_name, self.server_name, self.database_name))
+        self._parse_links(links)
 
-    def parse_resource(self, links):
+    def _parse_links(self, links):
         self.update({
             'replication_configured': len(links) > 0
         })
