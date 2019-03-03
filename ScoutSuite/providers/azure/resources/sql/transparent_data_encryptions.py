ac7e437e6b0808396cd023825f285e14029d0a13
Michaël Sghaier <michael.sghaier@polymtl.ca>
2019-03-03 12:38:39 -0500

    Update transparent_data_encryptions.py

--- ScoutSuite/providers/azure/resources/sql/transparent_data_encryptions.py
+++ ScoutSuite/providers/azure/resources/sql/transparent_data_encryptions.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from ..resources import AzureSimpleResources
+from ScoutSuite.providers.base.configs.resources import Resources
 
 
-class TransparentDataEncryptions(AzureSimpleResources):
+class TransparentDataEncryptions(Resources):
 
     def __init__(self, resource_group_name, server_name, database_name, facade):
         self.resource_group_name = resource_group_name
@@ -12,11 +12,12 @@ class TransparentDataEncryptions(AzureSimpleResources):
         self.facade = facade
 
     # TODO: make it really async.
-    async def get_resources_from_api(self):
-        return self.facade.transparent_data_encryptions.get(
+    async def fetch_all(self):
+        encryptions = self.facade.transparent_data_encryptions.get(
             self.resource_group_name, self.server_name, self.database_name)
+        self._parse_encryptions(encryptions)
 
-    def parse_resource(self, encryptions):
-        return self.update({
+    def _parse_encryptions(self, encryptions):
+        self.update({
             'transparent_data_encryption_enabled': encryptions.status == "Enabled"
         })
