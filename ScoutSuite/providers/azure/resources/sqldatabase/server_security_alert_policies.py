a3fc953f4ec4eaf9a5695c4999c14c11c8520bbd
misg <michael.sghaier@gmail.com>
2019-03-04 14:14:35 -0500

    Parse security config for sql servers.

---
+++ ScoutSuite/providers/azure/resources/sqldatabase/server_security_alert_policies.py
@@ -0,0 +1,23 @@
+from ScoutSuite.providers.base.configs.resources import Resources
+
+
+class ServerSecurityAlertPolicies(Resources):
+
+    def __init__(self, resource_group_name, server_name, facade):
+        self.resource_group_name = resource_group_name
+        self.server_name = server_name
+        self.facade = facade
+
+    # TODO: make it really async.
+    async def fetch_all(self):
+        policies = self.facade.server_security_alert_policies.get(
+            self.resource_group_name, self.server_name)
+        self._parse_policies(policies)
+
+    def _parse_policies(self, policies):
+        self.update({
+            'threat_detection_enabled': policies.state == "Enabled",
+            'alerts_enabled': policies.disabled_alerts == [""],
+            'send_alerts_enabled': policies.email_addresses != [""] and policies.email_account_admins,
+            'retention_days': policies.retention_days
+        })
