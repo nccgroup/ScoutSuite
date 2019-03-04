55b7fffc941c5676578a7ec92aa9500ba2dd9d62
Antoine Boisier-Michaud <aboisiermichaud@gmail.com>
2019-03-04 17:54:29 -0500

    Migrated CloudWatch to the new architecture

--- ScoutSuite/providers/aws/resources/cloudwatch/service.py
+++ ScoutSuite/providers/aws/resources/cloudwatch/service.py
@@ -1,49 +1,42 @@
-# # -*- coding: utf-8 -*-
-# """
-# CloudWatch-related classes and functions
-# """
+from ScoutSuite.providers.aws.facade.facade import AWSFacade
+from ScoutSuite.providers.aws.resources.regions import Regions
+from ScoutSuite.providers.aws.resources.resources import AWSResources
 
-# from ScoutSuite.providers.aws.configs.regions import RegionalServiceConfig, RegionConfig
-
-
-# ########################################
-# # CloudWatchRegionConfig
-# ########################################
-
-# class CloudWatchRegionConfig(RegionConfig):
-#     """
-#     CloudWatch configuration for a single AWS region
-#     """
-#     alarms = {}
-
-#     def parse_alarm(self, global_params, region, alarm):
-#         """
-#         Parse a single CloudWatch trail
-
-#         :param global_params:           Parameters shared for all regions
-#         :param region:                  Name of the AWS region
-#         :param alarm:                   Alarm
-#         """
-#         alarm['arn'] = alarm.pop('AlarmArn')
-#         alarm['name'] = alarm.pop('AlarmName')
-#         # Drop some data
-#         for k in ['AlarmConfigurationUpdatedTimestamp', 'StateReason', 'StateReasonData', 'StateUpdatedTimestamp']:
-#             if k in alarm:
-#                 alarm.pop(k)
-#         alarm_id = self.get_non_provider_id(alarm['arn'])
-#         self.alarms[alarm_id] = alarm
+from ScoutSuite.providers.utils import get_non_provider_id
 
 
-# ########################################
-# # CloudWatchConfig
-# ########################################
+class Alarms(AWSResources):
+    async def fetch_all(self, **kwargs):
+        raw_alarms = self.facade.cloudwatch.get_alarms(self.scope['region'])
+        for raw_alarm in raw_alarms:
+            name, resource = self._parse(raw_alarm)
+            self[name] = resource
 
-# class CloudWatchConfig(RegionalServiceConfig):
-#     """
-#     CloudWatch configuration for all AWS regions
-#     """
+    def _parse(self, raw_alarm):
+        """
+        Parse a single CloudWatch trail
 
-#     region_config_class = CloudWatchRegionConfig
+        :param global_params:           Parameters shared for all regions
+        :param region:                  Name of the AWS region
+        :param alarm:                   Alarm
+        """
 
-#     def __init__(self, service_metadata, thread_config=4):
-#         super(CloudWatchConfig, self).__init__(service_metadata, thread_config)
+        raw_alarm['arn'] = raw_alarm.pop('AlarmArn')
+        raw_alarm['name'] = raw_alarm.pop('AlarmName')
+        
+        # Drop some data
+        for k in ['AlarmConfigurationUpdatedTimestamp', 'StateReason', 'StateReasonData', 'StateUpdatedTimestamp']:
+            if k in raw_alarm:
+                raw_alarm.pop(k)
+
+        alarm_id = get_non_provider_id(raw_alarm['arn'])
+        return alarm_id, raw_alarm
+
+
+class CloudWatch(Regions):
+    _children = [
+        (Alarms, 'alarms')
+    ]
+
+    def __init__(self):
+        super(CloudWatch, self).__init__('cloudwatch')
