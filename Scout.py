c00aa87d8c3ddb562cb9d3488effaa35e0d31a24
misg <michael.sghaier@gmail.com>
2019-03-05 16:16:55 -0500

    Add a thread pool executor.

--- Scout.py
+++ Scout.py
@@ -4,10 +4,13 @@
 import sys
 import asyncio
 
+from concurrent.futures import ThreadPoolExecutor
+
 from ScoutSuite.__main__ import main
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
+    loop.set_default_executor(ThreadPoolExecutor(max_workers=10))
     loop.run_until_complete(main())
     loop.close()
     sys.exit()
