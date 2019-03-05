25967e57b29d9c66af0daf339162aa5e0492e3ea
misg <michael.sghaier@gmail.com>
2019-03-05 16:11:14 -0500

    Make AWS facade async.

--- ScoutSuite/providers/aws/facade/ec2.py
+++ ScoutSuite/providers/aws/facade/ec2.py
@@ -1,23 +1,25 @@
 import boto3
 import base64
-import itertools
 
 from ScoutSuite.providers.aws.facade.utils import AWSFacadeUtils
+from ScoutSuite.providers.utils import run_concurrently
 
 
 class EC2Facade:
-    def get_instance_user_data(self, region: str, instance_id: str):
-        ec2_client = AWSFacadeUtils.get_client('ec2', region)
-        user_data_response = ec2_client.describe_instance_attribute(Attribute='userData', InstanceId=instance_id)
+    async def get_instance_user_data(self, region: str, instance_id: str):
+        ec2_client = await AWSFacadeUtils.get_client('ec2', region)
+        user_data_response = await run_concurrently(
+            lambda: ec2_client.describe_instance_attribute(Attribute='userData', InstanceId=instance_id))
 
         if 'Value' not in user_data_response['UserData'].keys():
             return None
 
         return base64.b64decode(user_data_response['UserData']['Value']).decode('utf-8')
 
-    def get_instances(self, region, vpc):
+    async def get_instances(self, region, vpc):
         filters = [{'Name': 'vpc-id', 'Values': [vpc]}]
-        reservations = AWSFacadeUtils.get_all_pages('ec2', region, 'describe_instances', 'Reservations', Filters=filters)
+        reservations =\
+            await AWSFacadeUtils.get_all_pages('ec2', region, 'describe_instances', 'Reservations', Filters=filters)
 
         instances = []
         for reservation in reservations:
@@ -27,36 +29,39 @@ class EC2Facade:
                 
         return instances
 
-    def get_security_groups(self, region, vpc):
+    async def get_security_groups(self, region, vpc):
         filters = [{'Name': 'vpc-id', 'Values': [vpc]}]
-        return AWSFacadeUtils.get_all_pages('ec2', region, 'describe_security_groups', 'SecurityGroups', Filters=filters)
+        return await AWSFacadeUtils.get_all_pages(
+            'ec2', region, 'describe_security_groups', 'SecurityGroups', Filters=filters)
 
-    def get_vpcs(self, region):
-        ec2_client = boto3.client('ec2', region_name=region)
-        return ec2_client.describe_vpcs()['Vpcs']
+    async def get_vpcs(self, region):
+        ec2_client = await run_concurrently(lambda: boto3.client('ec2', region_name=region))
+        return await run_concurrently(lambda: ec2_client.describe_vpcs()['Vpcs'])
 
-    def get_images(self, region, owner_id):
+    async def get_images(self, region, owner_id):
         filters = [{'Name': 'owner-id', 'Values': [owner_id]}]
-        response = AWSFacadeUtils.get_client('ec2', region) \
-                                 .describe_images(Filters=filters)
+        client = await AWSFacadeUtils.get_client('ec2', region)
+        response = await run_concurrently(lambda: client.describe_images(Filters=filters))
 
         return response['Images']
 
-    def get_network_interfaces(self, region, vpc):
+    async def get_network_interfaces(self, region, vpc):
         filters = [{'Name': 'vpc-id', 'Values': [vpc]}]
-        return AWSFacadeUtils.get_all_pages('ec2', region, 'describe_network_interfaces', 'NetworkInterfaces', Filters=filters)
+        return await AWSFacadeUtils.get_all_pages(
+            'ec2', region, 'describe_network_interfaces', 'NetworkInterfaces', Filters=filters)
 
-    def get_volumes(self, region):
-        return AWSFacadeUtils.get_all_pages('ec2', region, 'describe_volumes', 'Volumes')
+    async def get_volumes(self, region):
+        return await AWSFacadeUtils.get_all_pages('ec2', region, 'describe_volumes', 'Volumes')
 
-    def get_snapshots(self, region, owner_id):
+    async def get_snapshots(self, region, owner_id):
         filters = [{'Name': 'owner-id', 'Values': [owner_id]}]
-        snapshots = AWSFacadeUtils.get_all_pages('ec2', region, 'describe_snapshots', 'Snapshots', Filters=filters)
+        snapshots = await AWSFacadeUtils.get_all_pages(
+            'ec2', region, 'describe_snapshots', 'Snapshots', Filters=filters)
 
-        ec2_client = AWSFacadeUtils.get_client('ec2', region)
+        ec2_client = await AWSFacadeUtils.get_client('ec2', region)
         for snapshot in snapshots:
-            snapshot['CreateVolumePermissions'] = ec2_client.describe_snapshot_attribute(
+            snapshot['CreateVolumePermissions'] = await run_concurrently(lambda: ec2_client.describe_snapshot_attribute(
                 Attribute='createVolumePermission',
-                SnapshotId=snapshot['SnapshotId'])['CreateVolumePermissions']
+                SnapshotId=snapshot['SnapshotId'])['CreateVolumePermissions'])
 
         return snapshots
--- ScoutSuite/providers/aws/facade/utils.py
+++ ScoutSuite/providers/aws/facade/utils.py
@@ -1,20 +1,21 @@
-from typing import Callable
 import boto3
+
+from ScoutSuite.providers.utils import run_concurrently
+
 
 # TODO: Add docstrings
 class AWSFacadeUtils:
     _clients = {}
-    
+
     @staticmethod
-    def get_all_pages(service: str, region: str, paginator_name: str, response_key: str, **paginator_args):
-        pages = AWSFacadeUtils.get_client(service, region) \
-                              .get_paginator(paginator_name) \
-                              .paginate(**paginator_args)
-                                   
+    async def get_all_pages(service: str, region: str, paginator_name: str, response_key: str, **paginator_args):
+        client = await AWSFacadeUtils.get_client(service, region)
+        pages = await run_concurrently(lambda: client.get_paginator(paginator_name).paginate(**paginator_args))
+
         return AWSFacadeUtils._get_from_all_pages(pages, response_key)
 
     @staticmethod
-    def _get_from_all_pages(pages: [], key:str):
+    def _get_from_all_pages(pages: [], key: str):
         resources = []
         for page in pages:
             resources.extend(page[key])
@@ -22,5 +23,6 @@ class AWSFacadeUtils:
         return resources
 
     @staticmethod
-    def get_client(service: str, region: str):
-        return AWSFacadeUtils._clients.setdefault((service, region), boto3.client(service, region_name=region))
+    async def get_client(service: str, region: str):
+        client = await run_concurrently(lambda: boto3.client(service, region_name=region))
+        return AWSFacadeUtils._clients.setdefault((service, region), client)
