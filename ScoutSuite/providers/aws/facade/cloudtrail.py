6857f409a5b5b7b0e0f5af0880c1462e73660022
misg <michael.sghaier@gmail.com>
2019-03-05 16:31:36 -0500

    Make cloudtrail facade async.

--- ScoutSuite/providers/aws/facade/cloudtrail.py
+++ ScoutSuite/providers/aws/facade/cloudtrail.py
@@ -1,13 +1,20 @@
 from ScoutSuite.providers.aws.facade.utils import AWSFacadeUtils
+from ScoutSuite.providers.utils import run_concurrently
 
 
 class CloudTrailFacade:
-    def get_trails(self, region):
-        client = AWSFacadeUtils.get_client('cloudtrail', region)
-        trails = client.describe_trails()['trailList']
+    async def get_trails(self, region):
+        client = await AWSFacadeUtils.get_client('cloudtrail', region)
+        trails = await run_concurrently(
+            lambda: client.describe_trails()['trailList']
+        )
 
         for trail in trails:
-            trail.update(client.get_trail_status(Name=trail['TrailARN']))
-            trail['EventSelectors'] = client.get_event_selectors(TrailName=trail['TrailARN'])['EventSelectors']
+            trail.update(await run_concurrently(
+                lambda: client.get_trail_status(Name=trail['TrailARN'])
+            ))
+            trail['EventSelectors'] = await run_concurrently(
+                lambda: client.get_event_selectors(TrailName=trail['TrailARN'])['EventSelectors']
+            )
 
         return trails
