c3def8230197fcae051eb05a127e254c689bc844
misg <michael.sghaier@gmail.com>
2019-03-05 16:51:10 -0500

    Add TODO.

--- Scout.py
+++ Scout.py
@@ -10,6 +10,7 @@ from ScoutSuite.__main__ import main
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
+    # TODO: make max_workers parameterizable (through the thread_config cli argument)
     loop.set_default_executor(ThreadPoolExecutor(max_workers=10))
     loop.run_until_complete(main())
     loop.close()
