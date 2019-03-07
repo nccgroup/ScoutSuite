e2fd838a319dab95268502ee1c6280499c079750
Rémi Pelletier <remi_pelletier_6@hotmail.com>
2019-03-07 14:41:35 -0500

    Merged GCP facade and IAM facade in keys.py

--- ScoutSuite/providers/gcp/resources/iam/keys.py
+++ ScoutSuite/providers/gcp/resources/iam/keys.py
@@ -1,13 +1,13 @@
 from ScoutSuite.providers.base.configs.resources import Resources
 
 class Keys(Resources):
-    def __init__(self, iam_facade, project_id, service_account_email):
-        self.iam_facade = iam_facade
+    def __init__(self, gcp_facade, project_id, service_account_email):
+        self.gcp_facade = gcp_facade
         self.project_id = project_id
         self.service_account_email = service_account_email 
 
     async def fetch_all(self):
-        raw_keys = await self.iam_facade.get_keys(self.project_id, self.service_account_email)
+        raw_keys = await self.gcp_facade.iam.get_keys(self.project_id, self.service_account_email)
         for raw_key in raw_keys:
             key_id, key = self._parse_key(raw_key)
             self[key_id] = key
