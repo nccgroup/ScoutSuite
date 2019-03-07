38cfc0ff34d4b964743e126895118130c10702b5
Michaël Sghaïer <michael.sghaier@gmail.com>
2019-03-07 17:20:29 -0500

    Update ScoutSuite/providers/gcp/facade/stackdriverlogging.py
    Co-Authored-By: Remi05 <remi_pelletier_6@hotmail.com>

--- ScoutSuite/providers/gcp/facade/stackdriverlogging.py
+++ ScoutSuite/providers/gcp/facade/stackdriverlogging.py
@@ -4,4 +4,4 @@ class StackdriverLoggingFacade:
     # TODO: Make truly async
     async def get_sinks(self, project_id):
         client = stackdriver_logging.Client(project=project_id)
-        return [sink for sink in client.list_sinks()]
+        return await run_concurrently(lambda: [sink for sink in client.list_sinks()])
