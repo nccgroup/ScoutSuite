cbe6ee2bb88f90909c30d2e9ab71e4f3812485d1
Rémi Pelletier <remi_pelletier_6@hotmail.com>
2019-03-06 22:09:37 -0500

    Added IAM class

---
+++ ScoutSuite/providers/gcp/resources/iam/service.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from ScoutSuite.providers.gcp.resources.projects import Projects
+from ScoutSuite.providers.gcp.resources.iam.service_accounts import ServiceAccounts
+
+class IAM(Projects):
+    _children = [ 
+        ('service_accounts', ServiceAccounts) 
+    ]
+
+    def __init__(self, gcp_facade, iam_facade):
+        super(IAM, self).__init__(gcp_facade, iam_facade)
