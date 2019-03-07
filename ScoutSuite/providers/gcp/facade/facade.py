6263c9158fc072702eb853ac8a41bbba9e7cb5f5
Rémi Pelletier <remi_pelletier_6@hotmail.com>
2019-03-07 14:00:04 -0500

    Moved client initialization outside of constructor

--- ScoutSuite/providers/gcp/facade/facade.py
+++ ScoutSuite/providers/gcp/facade/facade.py
@@ -5,12 +5,17 @@ from ScoutSuite.providers.gcp.facade.utils import GCPFacadeUtils
 
 class GCPFacade:
     def __init__(self):
-        self._resourcemanager_client = discovery.build('cloudresourcemanager', 'v1', cache_discovery=False, cache=MemoryCache())
         self.stackdriverlogging = StackdriverLoggingFacade()
+
+    def _get_resourcemanager_client(self):
+        if self._resourcemanager_client is None:
+            self._resourcemanager_client = discovery.build('cloudresourcemanager', 'v1', cache_discovery=False, cache=MemoryCache())
+        return self._resourcemanager_client
 
     # TODO: Make truly async    
     async def get_projects(self):
-        request = self._resourcemanager_client.projects().list() 
-        projects_group = self._resourcemanager_client.projects()
+        resourcemanager_client = self._get_resourcemanager_client()
+        request = resourcemanager_client.projects().list() 
+        projects_group = resourcemanager_client.projects()
         return await GCPFacadeUtils.get_all('projects', request, projects_group)
 
