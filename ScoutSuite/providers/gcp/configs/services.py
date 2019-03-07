cb30905a647acad16b8e8d0b2d209d9c8c1d6cfb
Rémi Pelletier <remi_pelletier_6@hotmail.com>
2019-03-06 22:48:02 -0500

    Migrated GCP IAM to new architecture

--- ScoutSuite/providers/gcp/configs/services.py
+++ ScoutSuite/providers/gcp/configs/services.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from ScoutSuite.providers.base.configs.services import BaseServicesConfig
+from ScoutSuite.providers.gcp.facade.facade import GCPFacade
+from ScoutSuite.providers.gcp.facade.iam import IAMFacade
+from ScoutSuite.providers.gcp.resources.iam.service import IAM
 from ScoutSuite.providers.gcp.services.cloudstorage import CloudStorageConfig
 from ScoutSuite.providers.gcp.services.cloudsql import CloudSQLConfig
 from ScoutSuite.providers.gcp.services.iam import IAMConfig
@@ -20,11 +23,14 @@ class GCPServicesConfig(BaseServicesConfig):
 
         projects = [] if projects is None else projects
 
+        gcp_facade = GCPFacade()
+        iam_facade = IAMFacade()
+
         self.cloudresourcemanager = CloudResourceManager(thread_config=thread_config)
         self.cloudstorage = CloudStorageConfig(thread_config=thread_config)
         self.cloudsql = CloudSQLConfig(thread_config=thread_config)
         self.computeengine = ComputeEngineConfig(thread_config=thread_config)
-        self.iam = IAMConfig(thread_config=thread_config)
+        self.iam = IAM(gcp_facade, iam_facade)
 
         try:
             self.kubernetesengine = KubernetesEngineConfig(thread_config=thread_config)
