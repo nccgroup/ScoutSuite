125ae28252c7820b601198cd06fbfa591d596687
Rémi Pelletier <remi_pelletier_6@hotmail.com>
2019-03-07 14:28:34 -0500

    Extracted get all resources loop

---
+++ ScoutSuite/providers/gcp/facade/utils.py
@@ -0,0 +1,10 @@
+
+class GCPFacadeUtils:
+    @staticmethod
+    async def get_all(resource_key, request, resources_group):
+        resources = []
+        while request is not None:
+            response = request.execute()
+            resources.extend(response.get(resource_key, []))
+            request = resources_group.list_next(previous_request=request, previous_response=response)
+        return resources
