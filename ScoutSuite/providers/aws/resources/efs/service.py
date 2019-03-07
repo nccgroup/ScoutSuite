939ae605fa2b64744888af7a3adadd85742c80cd
Antoine Boisier-Michaud <aboisiermichaud@gmail.com>
2019-03-07 16:09:58 -0500

    Renamed method, removed comment

--- ScoutSuite/providers/aws/resources/efs/service.py
+++ ScoutSuite/providers/aws/resources/efs/service.py
@@ -7,14 +7,12 @@ class FileSystems(AWSResources):
     async def fetch_all(self, **kwargs):
         raw_file_systems  = self.facade.efs.get_file_systems(self.scope['region'])
         for raw_file_system in raw_file_systems:
-            name, resource = self._parse(raw_file_system)
+            name, resource = self._parse_file_system(raw_file_system)
             self[name] = resource
 
-    def _parse(self, raw_file_system):
+    def _parse_file_system(self, raw_file_system):
         fs_id = raw_file_system.pop('FileSystemId')
         raw_file_system['name'] = raw_file_system.pop('Name') if 'Name' in raw_file_system else None
-
-        # Get tags
         raw_file_system['tags'] = raw_file_system.pop('Tags')
         
         return fs_id, raw_file_system
