e5d16af8387007db046588e6d1a0017ba226d461
Rémi Pelletier <remi_pelletier_6@hotmail.com>
2019-03-06 22:47:12 -0500

    Fixed IAM Keys parsing

--- ScoutSuite/providers/gcp/resources/iam/keys.py
+++ ScoutSuite/providers/gcp/resources/iam/keys.py
@@ -10,14 +10,14 @@ class Keys(Resources):
 
     async def fetch_all(self):
         raw_keys = await self.iam_facade.get_keys(self.project_id, self.service_account_email)
-        for raw_key in raw_keys:
+        for raw_key in raw_keys.get('keys', []):
             key_id, key = self._parse_key(raw_key)
             self[key_id] = key
 
     def _parse_key(self, raw_key):
         key_dict = {}
         key_dict['id'] = raw_key['name'].split('/')[-1]
-        key_dict['valid_after'] = raw_key['validAfterTime'],
-        key_dict['valid_before'] = raw_key['validBeforeTime'],
+        key_dict['valid_after'] = raw_key['validAfterTime']
+        key_dict['valid_before'] = raw_key['validBeforeTime']
         key_dict['key_algorithm'] = raw_key['keyAlgorithm']
         return key_dict['id'], key_dict
