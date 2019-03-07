6606ce3e41626108b7282f96f14841f1ff95e08c
Rémi Pelletier <remi_pelletier_6@hotmail.com>
2019-03-07 02:08:26 -0500

    Added CloudSQL class

---
+++ ScoutSuite/providers/gcp/resources/cloudsql/service.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from ScoutSuite.providers.gcp.resources.projects import Projects
+from ScoutSuite.providers.gcp.resources.cloudsql.database_instances import DatabaseInstances
+
+class CloudSQL(Projects):
+    _children = [ 
+        ('instances', DatabaseInstances)
+     ]
+
+    def __init__(self, gcp_facade, cloudsql_facade):
+        super(CloudSQL, self).__init__(gcp_facade, cloudsql_facade)
