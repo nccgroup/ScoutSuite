ae5cef69143148d0380d225ce8988c4e2e69ec2e
Rémi Pelletier <remi_pelletier_6@hotmail.com>
2019-03-06 20:47:28 -0500

    Added Sinks class

---
+++ ScoutSuite/providers/gcp/resources/stackdriverlogging/sinks.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from ScoutSuite.providers.base.configs.resources import Resources
+
+class Sinks(Resources):
+    def __init__(self, sdl_facade, project_id):
+        self.sdl_facade = sdl_facade
+        self.project_id = project_id
+
+    async def fetch_all(self):
+        raw_sinks = self.sdl_facade.get_sinks(self.project_id)
+        for raw_sink in raw_sinks:
+            sink_name, sink = self._parse_sink(raw_sink)
+            self[sink_name] = sink
+
+    def _parse_sink(self, raw_sink):
+        sink_dict = {}
+        sink_dict['name'] = raw_sink.name
+        sink_dict['filter'] = raw_sink.filter_
+        sink_dict['destination'] = raw_sink.destination
+        return sink_dict['name'], sink_dict
+    
