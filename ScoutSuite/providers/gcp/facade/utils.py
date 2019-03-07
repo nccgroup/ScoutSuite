f0d4bdd81ac0d2f839c5ae9fcb37f53af2fc4672
Rémi Pelletier <remi_pelletier_6@hotmail.com>
2019-03-07 17:31:16 -0500

    Made GCPFacadeUtils.get_all() truly async

--- ScoutSuite/providers/gcp/facade/utils.py
+++ ScoutSuite/providers/gcp/facade/utils.py
@@ -1,10 +1,13 @@
+from ScoutSuite.providers.utils import run_concurrently
 
 class GCPFacadeUtils:
     @staticmethod
     async def get_all(resource_key, request, resources_group):
         resources = []
         while request is not None:
-            response = request.execute()
+            response = await run_concurrently(request.execute)
             resources.extend(response.get(resource_key, []))
-            request = resources_group.list_next(previous_request=request, previous_response=response)
+            request = run_concurrently(
+                        lambda: resources_group.list_next(previous_request=request, previous_response=response)
+            )
         return resources
