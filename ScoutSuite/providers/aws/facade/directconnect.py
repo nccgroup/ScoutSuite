d561d5e5a8d94ade513a791a1a2f0515cbfec45a
Antoine Boisier-Michaud <aboisiermichaud@gmail.com>
2019-03-08 08:41:52 -0500

    Lint

--- ScoutSuite/providers/aws/facade/directconnect.py
+++ ScoutSuite/providers/aws/facade/directconnect.py
@@ -5,6 +5,4 @@ from ScoutSuite.providers.utils import run_concurrently
 class DirectConnectFacade:
     async def get_connections(self, region):
         client = AWSFacadeUtils.get_client('directconnect', region)
-        return await run_concurrently(
-                lambda: client.describe_connections()['connections']
-        )
+        return await run_concurrently(lambda: client.describe_connections()['connections'])
