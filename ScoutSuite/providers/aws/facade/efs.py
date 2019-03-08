90cb0c4b18562dd3e7039133a39ab4c3bcfa9f0b
Antoine Boisier-Michaud <aboisiermichaud@gmail.com>
2019-03-08 08:37:00 -0500

    Renamed variable, ctrl+k+d

--- ScoutSuite/providers/aws/facade/efs.py
+++ ScoutSuite/providers/aws/facade/efs.py
@@ -10,20 +10,19 @@ class EFSFacade:
         client = AWSFacadeUtils.get_client('efs', region)
         for file_system in file_systems:
             file_system_id = file_system['FileSystemId']
-            file_system['Tags'] = await run_concurrently(
-                        lambda: client.describe_tags(FileSystemId=file_system_id)['Tags']
-            )
+            file_system['Tags'] = await run_concurrently(lambda: client.describe_tags(FileSystemId=file_system_id)['Tags'])
 
             # Get mount targets
             mount_targets = await AWSFacadeUtils.get_all_pages('efs', region, 'describe_mount_targets', 'MountTargets', FileSystemId=file_system_id)
             file_system['MountTargets'] = {}
-            for mt in mount_targets:
-                mount_target_id = mt['MountTargetId']
-                file_system['MountTargets'][mount_target_id] = mt
+            for mount_target in mount_targets:
+                mount_target_id = mount_target['MountTargetId']
+                file_system['MountTargets'][mount_target_id] = mount_target
 
                 # Get security groups
                 security_groups = run_concurrently(
-                            lambda: client.describe_mount_target_security_groups(MountTargetId=mount_target_id)['SecurityGroups']
+                    lambda: client.describe_mount_target_security_groups(
+                        MountTargetId=mount_target_id)['SecurityGroups']
                 )
                 file_system['MountTargets'][mount_target_id]['SecurityGroups'] = security_groups
 
