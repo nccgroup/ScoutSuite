a6069be7d02c418b088a197407d0189d8c1d9e43
Antoine Boisier-Michaud <aboisiermichaud@gmail.com>
2019-03-10 17:09:00 -0400

    Merged resource-configs

--- ScoutSuite/providers/aws/facade/facade.py
+++ ScoutSuite/providers/aws/facade/facade.py
@@ -1,11 +1,15 @@
 from collections import Counter
 from botocore.session import Session
+
 from ScoutSuite.providers.aws.facade.awslambda import LambdaFacade
+from ScoutSuite.providers.aws.facade.cloudformation import CloudFormation
 from ScoutSuite.providers.aws.facade.cloudtrail import CloudTrailFacade
+from ScoutSuite.providers.aws.facade.cloudwatch import CloudWatch
 from ScoutSuite.providers.aws.facade.ec2 import EC2Facade
 from ScoutSuite.providers.aws.facade.efs import EFSFacade
 from ScoutSuite.providers.aws.facade.directconnect import DirectConnectFacade
 from ScoutSuite.providers.utils import run_concurrently
+
 
 
 class AWSFacade(object):
@@ -13,7 +17,9 @@ class AWSFacade(object):
     def __init__(self):
         self.ec2 = EC2Facade()
         self.awslambda = LambdaFacade()
+        self.cloudformation = CloudFormation()
         self.cloudtrail = CloudTrailFacade()
+        self.cloudwatch = CloudWatch()
         self.directconnect = DirectConnectFacade()
         self.efs = EFSFacade()
 
