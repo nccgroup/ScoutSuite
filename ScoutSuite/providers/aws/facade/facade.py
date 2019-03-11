79710d97bab6225866a3c689841535e4ef463126
Antoine Boisier-Michaud <aboisiermichaud@gmail.com>
2019-03-11 17:06:05 -0400

    Sorted imports a bit

--- ScoutSuite/providers/aws/facade/facade.py
+++ ScoutSuite/providers/aws/facade/facade.py
@@ -6,9 +6,10 @@ from ScoutSuite.providers.aws.facade.awslambda import LambdaFacade
 from ScoutSuite.providers.aws.facade.cloudformation import CloudFormation
 from ScoutSuite.providers.aws.facade.cloudtrail import CloudTrailFacade
 from ScoutSuite.providers.aws.facade.cloudwatch import CloudWatch
+from ScoutSuite.providers.aws.facade.directconnect import DirectConnectFacade
 from ScoutSuite.providers.aws.facade.ec2 import EC2Facade
 from ScoutSuite.providers.aws.facade.efs import EFSFacade
-from ScoutSuite.providers.aws.facade.directconnect import DirectConnectFacade
+from ScoutSuite.providers.aws.facade.elasticache import ElastiCacheFacade
 from ScoutSuite.providers.aws.facade.basefacade import AWSBaseFacade
 from ScoutSuite.providers.utils import run_concurrently
 from ScoutSuite.core.console import print_error, print_debug
