76dd49d95ea04f9aa75243c24f365795a23bf85f
Antoine Boisier-Michaud <aboisiermichaud@gmail.com>
2019-03-12 21:07:25 -0400

    Implemented EMR facade

---
+++ ScoutSuite/providers/aws/facade/emr.py
@@ -0,0 +1,15 @@
+from ScoutSuite.providers.aws.facade.utils import AWSFacadeUtils
+from ScoutSuite.providers.aws.facade.basefacade import AWSBaseFacade
+from ScoutSuite.providers.utils import run_concurrently
+
+
+class EMRFacade(AWSBaseFacade):
+    async def get_clusters(self, region):
+        clusters_list = await AWSFacadeUtils.get_all_pages('emr', region, self.session, 'list_clusters', 'Clusters')
+        client = AWSFacadeUtils.get_client('emr', region, self.session)
+        clusters_descriptions = []
+        for cluster_id in [cluster['Id'] for cluster in clusters_list]:
+            cluster = run_concurrently(lambda: client.describe_cluster(ClusterId=cluster_id)['Cluster'])
+            clusters_descriptions.append(cluster)
+
+        return cluster
