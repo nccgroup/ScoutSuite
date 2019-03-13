f6709ebe1fe07b0110ce23dd7cdf038a4ebaa424
misg <michael.sghaier@gmail.com>
2019-03-13 13:28:53 -0400

    Rename _parse method.

--- ScoutSuite/providers/azure/resources/keyvault/key_vaults.py
+++ ScoutSuite/providers/azure/resources/keyvault/key_vaults.py
@@ -4,19 +4,18 @@ from ScoutSuite.providers.utils import get_non_provider_id
 
 
 class KeyVaults(Resources):
-
     async def fetch_all(self, credentials, **kwargs):
         # TODO: build that facade somewhere else:
         facade = KeyVaultFacade(credentials.credentials, credentials.subscription_id)
 
         self['vaults'] = {}
         for raw_vault in await facade.get_key_vaults():
-            id, vault = self._parse(raw_vault)
+            id, vault = self._parse_key_vault(raw_vault)
             self['vaults'][id] = vault
 
         self['vaults_count'] = len(self['vaults'])
 
-    def _parse(self, raw_vault):
+    def _parse_key_vault(self, raw_vault):
         vault = {}
         vault['id'] = get_non_provider_id(raw_vault.id)
         vault['name'] = raw_vault.name
