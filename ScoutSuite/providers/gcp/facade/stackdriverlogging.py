643825f97e7758829caf5bae3ad7b57bf6faf488
Rémi Pelletier <remi_pelletier_6@hotmail.com>
2019-03-14 14:45:36 -0400

    Removed TODO comment

--- ScoutSuite/providers/gcp/facade/stackdriverlogging.py
+++ ScoutSuite/providers/gcp/facade/stackdriverlogging.py
@@ -2,7 +2,6 @@ from google.cloud import logging as stackdriver_logging
 from ScoutSuite.providers.utils import run_concurrently
 
 class StackdriverLoggingFacade:
-    # TODO: Make truly async
     async def get_sinks(self, project_id):
         client = stackdriver_logging.Client(project=project_id)
         return await run_concurrently(lambda: [sink for sink in client.list_sinks()])
