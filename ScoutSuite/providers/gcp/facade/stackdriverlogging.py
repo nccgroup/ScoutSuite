05c1401684b7609329724513b056fbecdf62ba90
Rémi Pelletier <remi_pelletier_6@hotmail.com>
2019-03-14 15:04:09 -0400

    Renamed import

--- ScoutSuite/providers/gcp/facade/stackdriverlogging.py
+++ ScoutSuite/providers/gcp/facade/stackdriverlogging.py
@@ -1,7 +1,7 @@
-from google.cloud import logging as stackdriver_logging
+from google.cloud import logging as stackdriverlogging
 from ScoutSuite.providers.utils import run_concurrently
 
 class StackdriverLoggingFacade:
     async def get_sinks(self, project_id):
-        client = stackdriver_logging.Client(project=project_id)
+        client = stackdriverlogging.Client(project=project_id)
         return await run_concurrently(lambda: [sink for sink in client.list_sinks()])
