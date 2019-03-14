9eda4beb608de9064ab43e2efa2feaf3010fabf6
Antoine Boisier-Michaud <aboisiermichaud@gmail.com>
2019-03-13 22:31:18 -0400

    Implemented role node

---
+++ ScoutSuite/providers/aws/resources/iam/roles.py
@@ -0,0 +1,15 @@
+from ScoutSuite.providers.aws.resources.resources import AWSResources
+
+
+class Roles(AWSResources):
+    async def fetch_all(self, **kwargs):
+        raw_roles = await self.facade.iam.get_roles()
+        for raw_role in raw_roles:
+            name, resource = self._parse_role(raw_role)
+            self[name] = resource
+
+    def _parse_role(self, raw_role):
+        raw_role['id'] = raw_role.pop('RoleId')
+        raw_role['name'] = raw_role.pop('RoleName')
+        raw_role['arn'] = raw_role.pop('Arn')
+        return raw_role['id'], raw_role
