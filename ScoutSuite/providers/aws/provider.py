9b42c0cf6718e67e871e147479897f4f4827997d
Antoine Boisier-Michaud <aboisiermichaud@gmail.com>
2019-03-20 13:38:45 -0400

    Added check before executing emr-related preprocessing

--- ScoutSuite/providers/aws/provider.py
+++ ScoutSuite/providers/aws/provider.py
@@ -59,7 +59,9 @@ class AWSProvider(BaseProvider):
         """
         ip_ranges = [] if ip_ranges is None else ip_ranges
         self._map_all_subnets()
-        self._set_emr_vpc_ids()
+
+        if 'emr' in self.service_list:
+            self._set_emr_vpc_ids()
 
         # TODO: Handle that when refactoring is done?
         # self.parse_elb_policies()
