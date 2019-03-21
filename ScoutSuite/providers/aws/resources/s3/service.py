7755bacfb7c325256ec9753a40b6baf0288b75fc
Michaël Sghaïer <michael.sghaier@gmail.com>
2019-03-21 19:21:47 -0400

    Renamed Bucket to Buckets
    Co-Authored-By: Aboisier <aboisiermichaud@gmail.com>

--- ScoutSuite/providers/aws/resources/s3/service.py
+++ ScoutSuite/providers/aws/resources/s3/service.py
@@ -5,7 +5,7 @@ from ScoutSuite.providers.aws.resources.resources import AWSResources, AWSCompos
 from ScoutSuite.providers.utils import get_non_provider_id
 
 
-class Bucket(AWSResources):
+class Buckets(AWSResources):
     async def fetch_all(self, **kwargs):
         raw_buckets = await self.facade.s3.get_buckets()
         for raw_bucket in raw_buckets:
@@ -38,7 +38,7 @@ class Bucket(AWSResources):
 
 class S3(AWSCompositeResources):
     _children = [
-        (Bucket, 'buckets')
+        (Buckets, 'buckets')
     ]
 
     def __init__(self):
