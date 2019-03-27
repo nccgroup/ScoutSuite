d8d69c7754b6678f478124dcf098fbec317e6d2e
misg <michael.sghaier@gmail.com>
2019-03-27 15:30:16 -0400

    Define _fetch_children_of_all_resources method and add docstrings.

--- ScoutSuite/providers/azure/resources/resources.py
+++ ScoutSuite/providers/azure/resources/resources.py
@@ -1,19 +1,54 @@
+"""This module provides implementations for Resources and CompositeResources for Azure."""
+
 import abc
 import asyncio
 
 from ScoutSuite.providers.base.configs.resources import CompositeResources
 
 
-# TODO: add docstrings.
 class AzureCompositeResources(CompositeResources, metaclass=abc.ABCMeta):
 
-    async def _fetch_children(self, parent, **kwargs):
+    """This class represents a collection of composite Resources (resources that include nested resources referred as
+    their children). Classes extending AzureCompositeResources have to define a '_children' attribute which consists of
+    a list of tuples describing the children. The tuples are expected to respect the following format:
+    (<child_class>, <child_name>). 'child_name' is used to indicate the name under which the child resources will be
+    stored in the parent object.
+    """
+
+    async def _fetch_children_of_all_resources(self, resources: dict, kwargs: dict):
+        """This method iterates through a collection of resources and fetches all children of each resource, in a
+        concurrent way.
+
+        :param resources: list of (composite) resources
+        :param kwargs: dict that maps resource parent keys to each kwargs (dict) used to retrieve child resources.
+        """
+        if len(resources) == 0:
+            return
+
+        tasks = {
+            asyncio.ensure_future(
+                self._fetch_children(resource_parent=resource_parent, **kwargs[resource_parent_key])
+            ) for (resource_parent_key, resource_parent) in resources.items()
+        }
+        await asyncio.wait(tasks)
+
+    async def _fetch_children(self, resource_parent, **kwargs):
+        """This method fetches all children of a given resource (the so called 'resource_parent') by calling fetch_all
+        method on each child defined in '_children' and then stores the fetched resources in `resource_parent` under
+        the key associated with the child.
+
+        :param resource_parent: The resource in which the children will be stored.
+        :param kwargs: parameters that depend on the type of child resources, used to fetch them.
+        """
+
         children = [(child_class(**kwargs), child_name) for (child_class, child_name) in self._children]
-        # fetch all children concurrently:
-        await asyncio.wait({asyncio.ensure_future(child.fetch_all()) for (child, _) in children})
-        # update parent content:
+        # Fetch all children concurrently:
+        await asyncio.wait(
+            {asyncio.ensure_future(child.fetch_all()) for (child, _) in children}
+        )
+        # Update parent content:
         for child, name in children:
             if name:
-                parent[name] = child
+                resource_parent[name] = child
             else:
-                parent.update(child)
+                resource_parent.update(child)
