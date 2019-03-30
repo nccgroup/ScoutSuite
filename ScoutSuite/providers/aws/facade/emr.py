ecbed0ee14c5d905640540e4bf6bcf6e1d85954a
misg <michael.sghaier@gmail.com>
2019-03-30 15:17:00 -0400

    Use map_concurrently function instead of run_tasks_concurrently.

--- ScoutSuite/providers/aws/facade/emr.py
+++ ScoutSuite/providers/aws/facade/emr.py
@@ -1,15 +1,15 @@
 from ScoutSuite.providers.aws.facade.utils import AWSFacadeUtils
 from ScoutSuite.providers.aws.facade.basefacade import AWSBaseFacade
-from ScoutSuite.providers.utils import run_concurrently, run_tasks_concurrently
-import asyncio
+from ScoutSuite.providers.utils import map_concurrently, run_concurrently
 
 
 class EMRFacade(AWSBaseFacade):
     async def get_clusters(self, region):
-        clusters_list = await AWSFacadeUtils.get_all_pages('emr', region, self.session, 'list_clusters', 'Clusters')
+        cluster_list = await AWSFacadeUtils.get_all_pages('emr', region, self.session, 'list_clusters', 'Clusters')
+        cluster_ids = [cluster['Id'] for cluster in cluster_list]
         client = AWSFacadeUtils.get_client('emr', self.session, region)
-        cluster_ids = [cluster['Id'] for cluster in clusters_list]
-        
-        return await run_tasks_concurrently({
-            run_concurrently(lambda: client.describe_cluster(ClusterId=id)['Cluster']) for id in cluster_ids
-        })
+
+        return await map_concurrently(
+            lambda cluster_id: run_concurrently(
+                lambda: client.describe_cluster(ClusterId=cluster_id)['Cluster']),
+            cluster_ids)
