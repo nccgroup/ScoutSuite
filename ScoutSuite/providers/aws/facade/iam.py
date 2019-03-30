ade1c8e0ef19f57bc8386eae33ed0b46313a5601
misg <michael.sghaier@gmail.com>
2019-03-30 15:17:12 -0400

    Improve concurrent support for IAM facade.

--- ScoutSuite/providers/aws/facade/iam.py
+++ ScoutSuite/providers/aws/facade/iam.py
@@ -1,9 +1,10 @@
+import asyncio
+import functools
+
 from ScoutSuite.providers.aws.facade.utils import AWSFacadeUtils
 from ScoutSuite.providers.aws.facade.basefacade import AWSBaseFacade
-from ScoutSuite.core.console import print_error, print_exception
-from ScoutSuite.providers.utils import run_concurrently, get_non_provider_id
-from ScoutSuite.providers.aws.utils import is_throttled
-from ScoutSuite.providers.utils import run_concurrently, run_tasks_concurrently
+from ScoutSuite.core.console import print_error
+from ScoutSuite.providers.utils import get_non_provider_id, run_concurrently
 
 
 class IAMFacade(AWSBaseFacade):
@@ -35,28 +36,27 @@ class IAMFacade(AWSBaseFacade):
 
     async def get_groups(self):
         groups = await AWSFacadeUtils.get_all_pages('iam', None, self.session, 'list_groups', 'Groups')
-        for group in groups:
-            group['Users'] = await self._fetch_group_users(group['GroupName'])
-            policies = await self._get_inline_policies(
-                'group', group['GroupId'], group['GroupName'])
-            if len(policies):
-                group['inline_policies'] = policies
-            group['inline_policies_count'] = len(policies)
+        await AWSFacadeUtils.get_and_set_concurrently(
+            [self._get_and_set_group_users,
+             functools.partial(self._get_and_set_inline_policies, iam_resource_type='group')], groups)
         return groups
 
     async def get_policies(self):
-        policies = await AWSFacadeUtils.get_all_pages('iam', None, self.session, 'list_policies', 'Policies', OnlyAttached=True)
-        return await run_tasks_concurrently({
-            self.get_detailed_policy(policy) for policy in policies
-        })
+        policies = await AWSFacadeUtils.get_all_pages(
+            'iam', None, self.session, 'list_policies', 'Policies', OnlyAttached=True)
+        await AWSFacadeUtils.get_and_set_concurrently([self._get_and_set_policy_details], policies)
+        return policies
 
-    async def get_detailed_policy(self, policy):
+    async def _get_and_set_policy_details(self, policy):
         client = AWSFacadeUtils.get_client('iam', self.session)
-        policy_version = await run_concurrently(lambda: client.get_policy_version(PolicyArn=policy['Arn'], VersionId=policy['DefaultVersionId']))
+        policy_version = await run_concurrently(
+            lambda: client.get_policy_version(PolicyArn=policy['Arn'], VersionId=policy['DefaultVersionId']))
         policy['PolicyDocument'] = policy_version['PolicyVersion']['Document']
 
         policy['attached_to'] = {}
-        attached_entities = await AWSFacadeUtils.get_multiple_entities_from_all_pages('iam', None, self.session, 'list_entities_for_policy',  ['PolicyGroups', 'PolicyRoles', 'PolicyUsers'], PolicyArn=policy['Arn'])
+        attached_entities = await AWSFacadeUtils.get_multiple_entities_from_all_pages(
+            'iam', None, self.session, 'list_entities_for_policy',  ['PolicyGroups', 'PolicyRoles', 'PolicyUsers'],
+            PolicyArn=policy['Arn'])
 
         for entity_type in attached_entities:
             resource_type = entity_type.replace('Policy', '').lower()
@@ -72,127 +72,105 @@ class IAMFacade(AWSBaseFacade):
                 policy['attached_to'][resource_type].append(
                     {'name': resource_name, 'id': resource_id})
 
-        return policy
-
     async def get_users(self):
         users = await AWSFacadeUtils.get_all_pages('iam', None, self.session, 'list_users', 'Users')
-        return await run_tasks_concurrently({
-            self.get_detailed_user(user) for user in users
-        })
+        await AWSFacadeUtils.get_and_set_concurrently(
+            [functools.partial(self._get_and_set_inline_policies, iam_resource_type='user'),
+             self._get_and_set_user_groups,
+             self._get_and_set_user_login_profile,
+             self._get_and_set_user_acces_keys,
+             self._get_and_set_user_mfa_devices],
+            users)
+        return users
 
-    async def get_detailed_user(self, user):
+    async def _get_and_set_user_login_profile(self, user: {}):
         client = AWSFacadeUtils.get_client('iam', self.session)
-        user_name = user['UserName']
-        user_id = user['UserId']
-
-        policies = await self._get_inline_policies('user', user_id, user_name)
-        if len(policies):
-            user['inline_policies'] = policies
-        user['inline_policies_count'] = len(policies)
-        user['groups'] = []
-        groups = await AWSFacadeUtils.get_all_pages('iam', None, self.session, 'list_groups_for_user', 'Groups', UserName=user_name)
-        for group in groups:
-            user['groups'].append(group['GroupName'])
         try:
             user['LoginProfile'] = await run_concurrently(
-                lambda: client.get_login_profile(UserName=user_name)['LoginProfile'])
+                lambda: client.get_login_profile(UserName=user['UserName'])['LoginProfile'])
         except Exception:
             pass
-        user['AccessKeys'] = await self._get_user_acces_keys(user_name)
-        user['MFADevices'] = await self._get_user_mfa_devices(user_name)
 
-        return user
+    async def _get_and_set_user_groups(self, user: {}):
+        groups = await AWSFacadeUtils.get_all_pages(
+            'iam', None, self.session, 'list_groups_for_user', 'Groups',UserName=user['UserName'])
+        user['groups'] = [group['GroupName'] for group in groups]
 
     async def get_roles(self):
         roles = await AWSFacadeUtils.get_all_pages('iam', None, self.session, 'list_roles', 'Roles')
-
-        # Handle throttling errors
         for role in roles:
             role['instances_count'] = 'N/A'
-
-            # Get role policies
-            policies = await self._get_inline_policies(
-                'role', role['RoleId'], role['RoleName'])
-            if len(policies):
-                role['inline_policies'] = policies
-            role['inline_policies_count'] = len(policies)
-
-            # Get instance profiles
-            profiles = await AWSFacadeUtils.get_all_pages('iam', None, self.session, 'list_instance_profiles_for_role', 'InstanceProfiles', RoleName=role['RoleName'])
-            role.setdefault('instance_profiles', {})
-            for profile in profiles:
-                profile_id = profile['InstanceProfileId']
-                role['instance_profiles'].setdefault(profile_id, {})
-                role['instance_profiles'][profile_id].setdefault(
-                    'arn', profile['Arn'])
-                role['instance_profiles'][profile_id].setdefault(
-                    'name', profile['InstanceProfileName'])
-
             # Get trust relationship
             role['assume_role_policy'] = {}
             role['assume_role_policy']['PolicyDocument'] = role.pop(
                 'AssumeRolePolicyDocument')
+        await AWSFacadeUtils.get_and_set_concurrently(
+            [functools.partial(self._get_and_set_inline_policies, iam_resource_type='role'),
+             self._get_and_set_role_profiles], roles)
 
         return roles
+
+    async def _get_and_set_role_profiles(self, role: {}):
+        profiles = await AWSFacadeUtils.get_all_pages(
+            'iam', None, self.session, 'list_instance_profiles_for_role', 'InstanceProfiles',
+            RoleName=role['RoleName'])
+        role.setdefault('instance_profiles', {})
+        for profile in profiles:
+            profile_id = profile['InstanceProfileId']
+            role['instance_profiles'].setdefault(profile_id, {})
+            role['instance_profiles'][profile_id].setdefault(
+                'arn', profile['Arn'])
+            role['instance_profiles'][profile_id].setdefault(
+                'name', profile['InstanceProfileName'])
 
     async def get_password_policy(self):
         client = AWSFacadeUtils.get_client('iam', self.session)
         return (await run_concurrently(client.get_account_password_policy))['PasswordPolicy']
 
-    async def _get_user_acces_keys(self, user_name):
+    async def _get_and_set_user_acces_keys(self, user: {}):
         client = AWSFacadeUtils.get_client('iam', self.session)
-        response = await run_concurrently(lambda: client.list_access_keys(UserName=user_name))
-        return response['AccessKeyMetadata']
+        user['AccessKeys'] = await run_concurrently(
+            lambda: client.list_access_keys(UserName=user['UserName'])['AccessKeyMetadata'])
 
-    async def _get_user_mfa_devices(self, user_name):
+    async def _get_and_set_user_mfa_devices(self, user: {}):
         client = AWSFacadeUtils.get_client('iam', self.session)
-        response = await run_concurrently(lambda: client.list_mfa_devices(UserName=user_name))
-        return response['MFADevices']
+        user['MFADevices'] = await run_concurrently(
+            lambda: client.list_mfa_devices(UserName=user['UserName'])['MFADevices'])
 
-    async def _fetch_group_users(self, group_name):
+    async def _get_and_set_group_users(self, group: {}):
         client = AWSFacadeUtils.get_client('iam', self.session)
-        fetched_users = client.get_group(GroupName=group_name)['Users']
+        users = await run_concurrently(lambda: client.get_group(GroupName=group['GroupName'])['Users'])
+        group['Users'] = [user['UserId'] for user in users]
 
-        users = []
-        for user in fetched_users:
-            users.append(user['UserId'])
-        return users
-
-    async def _get_inline_policies(self, iam_resource_type, resource_id, resource_name):
+    async def _get_and_set_inline_policies(self, resource, iam_resource_type):
         client = AWSFacadeUtils.get_client('iam', self.session)
-        get_policy_method = getattr(
-            client, 'get_' + iam_resource_type + '_policy')
-        fetched_policies = {}
-        list_policy_method = getattr(
-            client, 'list_' + iam_resource_type + '_policies')
+        list_policy_method = getattr(client, 'list_' + iam_resource_type + '_policies')
+        resource_name = resource[iam_resource_type.title() + 'Name']
         args = {iam_resource_type.title() + 'Name': resource_name}
-        
-        try:
-            policy_names = await run_concurrently(lambda: list_policy_method(**args)['PolicyNames'])
-        except Exception as e:
-            if is_throttled(e):
-                raise e
-            else:
-                print_exception(e)
-                return fetched_policies
-        
-        try:
-            for policy_name in policy_names:
-                args['PolicyName'] = policy_name
-                policy_document = get_policy_method(**args)['PolicyDocument']
-                policy_id = get_non_provider_id(policy_name)
-                fetched_policies[policy_id] = {}
-                fetched_policies[policy_id]['PolicyDocument'] = self._normalize_statements(
-                    policy_document)
-                fetched_policies[policy_id]['name'] = policy_name
-                fetched_policies[policy_id] = fetched_policies[policy_id]
-        except Exception as e:
-            if is_throttled(e):
-                raise e
-            else:
-                print_exception(e)
 
-        return fetched_policies
+        resource['inline_policies'] = {}
+        policy_names = await run_concurrently(lambda: list_policy_method(**args)['PolicyNames'])
+        if len(policy_names) == 0:
+            resource['inline_policies_count'] = 0
+            return
+
+        get_policy_method = getattr(client, 'get_' + iam_resource_type + '_policy')
+        tasks = {
+            asyncio.ensure_future(
+                run_concurrently(lambda: get_policy_method(**dict(args, PolicyName=policy_name)))
+            ) for policy_name in policy_names
+        }
+        for task in asyncio.as_completed(tasks):
+            policy = await task
+            policy_name = policy['PolicyName']
+            policy_id = get_non_provider_id(policy_name)
+            policy_document = policy['PolicyDocument']
+
+            resource['inline_policies'][policy_id] = {}
+            resource['inline_policies'][policy_id]['PolicyDocument'] = self._normalize_statements(
+                policy_document)
+            resource['inline_policies'][policy_id]['name'] = policy_name
+        resource['inline_policies_count'] = len(resource['inline_policies'])
 
     def _normalize_statements(self, policy_document):
         for statement in policy_document['Statement']:
