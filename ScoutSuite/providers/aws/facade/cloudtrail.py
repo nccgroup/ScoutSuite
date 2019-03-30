06523b5b35e98efea14065a7159dc72b3065e194
misg <michael.sghaier@gmail.com>
2019-03-30 15:15:05 -0400

    Improve concurrent support for CloudTrail facade.

--- ScoutSuite/providers/aws/facade/cloudtrail.py
+++ ScoutSuite/providers/aws/facade/cloudtrail.py
@@ -7,15 +7,19 @@ class CloudTrailFacade(AWSBaseFacade):
     async def get_trails(self, region):
         client = AWSFacadeUtils.get_client('cloudtrail', self.session, region)
         trails = await run_concurrently(
-            lambda: client.describe_trails()['trailList']
-        )
-
-        for trail in trails:
-            trail.update(await run_concurrently(
-                lambda: client.get_trail_status(Name=trail['TrailARN'])
-            ))
-            trail['EventSelectors'] = await run_concurrently(
-                lambda: client.get_event_selectors(TrailName=trail['TrailARN'])['EventSelectors']
-            )
+            lambda: client.describe_trails()['trailList'])
+        await AWSFacadeUtils.get_and_set_concurrently(
+            [self._get_and_set_status, self._get_and_set_selectors], trails, region=region)
 
         return trails
+
+    async def _get_and_set_status(self, trail: {}, region: str):
+        client = AWSFacadeUtils.get_client('cloudtrail', self.session, region)
+        trail_status = await run_concurrently(
+            lambda: client.get_trail_status(Name=trail['TrailARN']))
+        trail.update(trail_status)
+
+    async def _get_and_set_selectors(self, trail: {}, region: str):
+        client = AWSFacadeUtils.get_client('cloudtrail', self.session, region)
+        trail['EventSelectors'] = await run_concurrently(
+            lambda: client.get_event_selectors(TrailName=trail['TrailARN'])['EventSelectors'])
