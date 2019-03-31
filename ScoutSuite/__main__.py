57aff5a2284d2751be8812a00cdc24fbdf00f922
Xavier Garceau-Aranda <xavier.garceau-aranda@owasp.org>
2019-03-31 13:11:54 +0200

    Make exceptions string not list

--- ScoutSuite/__main__.py
+++ ScoutSuite/__main__.py
@@ -6,7 +6,7 @@ import webbrowser
 
 from ScoutSuite import DEFAULT_RESULT_FILE
 from ScoutSuite.core.cli_parser import ScoutSuiteArgumentParser
-from ScoutSuite.core.console import set_config_debug_level, print_info, print_debug, print_error, print_exception
+from ScoutSuite.core.console import set_config_debug_level, print_info, print_exception
 from ScoutSuite.core.exceptions import RuleExceptions
 from ScoutSuite.core.processingengine import ProcessingEngine
 from ScoutSuite.core.ruleset import Ruleset
