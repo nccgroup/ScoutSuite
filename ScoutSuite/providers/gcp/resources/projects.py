c8f4b2fb561246eecc3ed50d205eddc5ef1852a7
Rémi Pelletier <remi_pelletier_6@hotmail.com>
2019-04-02 14:02:46 -0400

    Parallelized GCP Projects fetch_all

--- ScoutSuite/providers/gcp/resources/projects.py
+++ ScoutSuite/providers/gcp/resources/projects.py
@@ -1,3 +1,4 @@
+import asyncio
 from ScoutSuite.providers.gcp.facade.gcp import GCPFacade
 from ScoutSuite.providers.gcp.resources.resources import GCPCompositeResources
 
@@ -7,11 +8,13 @@ class Projects(GCPCompositeResources):
 
     async def fetch_all(self, **kwargs):
         raw_projects = await self.gcp_facade.get_projects()
-        self['projects'] = {}
-        for raw_project in raw_projects:
-            project_id = raw_project['projectId']
-            self['projects'][project_id] = {}
-            await self._fetch_children(self['projects'][project_id], gcp_facade = self.gcp_facade, project_id = project_id)
+        self['projects'] = { raw_project['projectId'] : {} for raw_project in raw_projects }
+        tasks = {
+            asyncio.ensure_future(
+                self._fetch_children(self['projects'][raw_project['projectId']], gcp_facade = self.gcp_facade, project_id = raw_project['projectId'])
+            ) for raw_project in raw_projects
+        }
+        await asyncio.wait(tasks)
         self._set_counts()
 
     def _set_counts(self):
