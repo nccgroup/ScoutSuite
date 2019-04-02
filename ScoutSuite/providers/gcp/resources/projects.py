58cb21da8024cf674942762b36442b30dcd38da7
Rémi Pelletier <remi_pelletier_6@hotmail.com>
2019-04-02 14:24:58 -0400

    Added doc strings

--- ScoutSuite/providers/gcp/resources/projects.py
+++ ScoutSuite/providers/gcp/resources/projects.py
@@ -3,10 +3,20 @@ from ScoutSuite.providers.gcp.facade.gcp import GCPFacade
 from ScoutSuite.providers.gcp.resources.resources import GCPCompositeResources
 
 class Projects(GCPCompositeResources):
+
+    """This class represents a collection of GCP Resources that are grouped by project. 
+    Classes extending Projects should implement the method _fetch_children() with a project ID as paramater.
+    The children resources will be stored with the following structure {<projects>: {<project_id>: {<child_name>: {<child_id>: <child_instance>}}}}.
+    """
+
     def __init__(self, gcp_facade: GCPFacade):
         self.gcp_facade = gcp_facade
 
     async def fetch_all(self, **kwargs):
+        """This method fetches all the GCP projects that can be accessed with the given run configuration.
+        It then fetches all the children defined in _children and groups them by project.
+        """
+
         raw_projects = await self.gcp_facade.get_projects()
         self['projects'] = { raw_project['projectId'] : {} for raw_project in raw_projects }
         tasks = {
