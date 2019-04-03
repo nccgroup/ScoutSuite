a2829710b5ead0af6742030a53f1e7256965e7b4
Vincent Fortin <vincent-2.fortin@polymtl.ca>
2019-04-03 10:34:44 -0400

    Removed old func

--- ScoutSuite/output/data/inc-scoutsuite/sqlite.js
+++ ScoutSuite/output/data/inc-scoutsuite/sqlite.js
@@ -41,80 +41,16 @@ function requestDb (query, pageSize, pageIndex) {
  * Returns all the data from the server, excepted for resources
  * @returns {object}
  */
-function getScoutsuiteResultsSqlite3 () {
-  // The layers are named here in this fashion, these names don't always make sense depending in which
-  // nested dict you are though since our data structure is not consistent :
-  // paths(0) --> groups(1) --> services(2) --> counters(3) --> resources(4) --> items(5)
-  let paths = requestDb('').keys
-  runResults = {}
-  for (let i in paths) { // Layer 0
-    let list = {}
-    let groups = requestDb(paths[i]) 
-    if (groups.keys) {
-      groups = groups.keys
-    } else {
-      runResults[paths[i]] = groups
-      continue
-    }
-    for (let group in groups) { // Layer 1    
-      list[groups[group]] = requestDb(createQuery(paths[i], groups[group]))
-      let services = list[groups[group]].keys
-      if (services) {        
-        for (let service in services) { // Layer 2
-          let counters = requestDb(createQuery(paths[i], groups[group], services[service]))
-          if (!counters.keys) {
-            list[groups[group]][services[service]] = counters
-          } else {
-            counters = counters.keys
-            list[groups[group]][services[service]] = { [null]: null }
-          }
-          // The only elements for which we do not want to fetch everything are the resources which
-          // are not filters or findings since they will scale with the environment's size
-          if (paths[i] === 'services' && [services[service]] != 'filters' && [services[service]] != 'findings') {
-            continue
-          }
-          for (let counter in counters) { // Layer 3
-            list[groups[group]][services[service]][counters[counter]] =
-              requestDb(createQuery(paths[i], groups[group], services[service], counters[counter]))
-            if (!list[groups[group]][services[service]][counters[counter]]) { continue }
-            let resources = list[groups[group]][services[service]][counters[counter]].keys
-            for (let resource in resources) { // Layer 4
-              list[groups[group]][services[service]][counters[counter]][resources[resource]] = requestDb(
-                createQuery(paths[i], groups[group], services[service], counters[counter], resources[resource]))
-              let items = list[groups[group]][services[service]][counters[counter]][resources[resource]].keys
-              for (let item in items) { // Layer 5          
-                list[groups[group]][services[service]][counters[counter]][resources[resource]][items[item]] =
-                requestDb(createQuery(paths[i], groups[group], services[service], counters[counter], resources[resource],
-                  items[item]))
-                delete list[groups[group]][services[service]][counters[counter]].type
-                delete list[groups[group]][services[service]][counters[counter]].keys
-              }
-            }
-          }
-          delete list[groups[group]][services[service]].null
-        }
-        delete list[groups[group]].type
-        delete list[groups[group]].keys
-      }
-    }
-    runResults[paths[i]] = list
-  }
-  return runResults
-}
-
-/**
- * Returns all the data from the server, excepted for resources
- * @returns {object}
- */
 function getScoutsuiteResultsSqlite () {
   let runResults = requestDb()
-  for (let service in runResults['services']) {
+  /*for (let service in runResults['services']) {
     for (let resource in runResults['services'][service]) {
       if (resource.match(reCount)) {
-        runResults['services'][service][resource] = requestDb(createQuery(['services'], [service], [resource]))
+        runResults['services'][service][resource.replace(reCount, '')] = 
+          requestDb(createQuery(['services'], [service], [resource.replace(reCount, '')]))
       }
     }
-  }
+  }*/
   return runResults
 }
 
