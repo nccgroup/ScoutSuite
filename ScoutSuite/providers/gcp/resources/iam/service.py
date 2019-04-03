a9b7d5cee552d4a5fb0486568fca07d51a772f99
Rémi Pelletier <remi_pelletier_6@hotmail.com>
2019-04-03 18:30:46 -0400

    Removed unnecessary constructor

--- ScoutSuite/providers/gcp/resources/iam/service.py
+++ ScoutSuite/providers/gcp/resources/iam/service.py
@@ -6,6 +6,3 @@ class IAM(Projects):
     _children = [ 
         (ServiceAccounts, 'service_accounts') 
     ]
-
-    def __init__(self, gcp_facade: GCPFacade):
-        super(IAM, self).__init__(gcp_facade)
