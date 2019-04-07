048d0b6366bdf795e26adc19a43a142758c2ae57
Michaël Sghaïer <michael.sghaier@gmail.com>
2019-04-07 11:41:39 -0400

    Update ScoutSuite/providers/gcp/facade/cloudstorage.py
    Co-Authored-By: Remi05 <remi_pelletier_6@hotmail.com>

--- ScoutSuite/providers/gcp/facade/cloudstorage.py
+++ ScoutSuite/providers/gcp/facade/cloudstorage.py
@@ -4,4 +4,4 @@ from ScoutSuite.providers.utils import run_concurrently
 class CloudStorageFacade:
     async def get_buckets(self, project_id: str):
         client = storage.Client(project=project_id)
-        return await run_concurrently(lambda: [bucket for bucket in client.list_buckets()])
+        return await run_concurrently(lambda: list(client.list_buckets()))
