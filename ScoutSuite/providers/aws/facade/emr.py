fc8f017a61496bb3e3099cc090dc42a089b65d8c
misg <michael.sghaier@gmail.com>
2019-04-09 18:33:34 -0400

    Propagate exceptions in functions passed to `map_concurrently`.

--- ScoutSuite/providers/aws/facade/emr.py
+++ ScoutSuite/providers/aws/facade/emr.py
@@ -8,13 +8,13 @@ class EMRFacade(AWSBaseFacade):
     async def get_clusters(self, region):
         cluster_list = await AWSFacadeUtils.get_all_pages('emr', region, self.session, 'list_clusters', 'Clusters')
         cluster_ids = [cluster['Id'] for cluster in cluster_list]
-        client = AWSFacadeUtils.get_client('emr', self.session, region)
 
+        return await map_concurrently(self._get_cluster, cluster_ids, region=region)
+
+    async def _get_cluster(self, cluster_id: str, region: str):
+        client = AWSFacadeUtils.get_client('emr', self.session, region)
         try:
-            return await map_concurrently(
-                lambda cluster_id: run_concurrently(
-                    lambda: client.describe_cluster(ClusterId=cluster_id)['Cluster']),
-                cluster_ids)
+            return await run_concurrently(lambda: client.describe_cluster(ClusterId=cluster_id)['Cluster'])
         except Exception as e:
             print_exception('Failed to describe EMR cluster: {}'.format(e))
-            return []
+            raise
