935ddb164db6a24d602cd78737c56d0b3dfa1f49
Rémi Pelletier <remi_pelletier_6@hotmail.com>
2019-04-12 03:29:53 -0400

    Added error handling for Stackdriver Logging

--- ScoutSuite/providers/gcp/facade/stackdriverlogging.py
+++ ScoutSuite/providers/gcp/facade/stackdriverlogging.py
@@ -1,7 +1,12 @@
+from ScoutSuite.core.console import print_exception
 from google.cloud import logging as stackdriverlogging
 from ScoutSuite.providers.utils import run_concurrently
 
 class StackdriverLoggingFacade:
-    async def get_sinks(self, project_id: str):
-        client = stackdriverlogging.Client(project=project_id)
-        return await run_concurrently(lambda: [sink for sink in client.list_sinks()])
+    try:
+        async def get_sinks(self, project_id: str):
+            client = stackdriverlogging.Client(project=project_id)
+            return await run_concurrently(lambda: [sink for sink in client.list_sinks()])
+    except Exception as e:
+            print_exception('Failed to retrieve sinks: {}'.format(e))
+            return []
