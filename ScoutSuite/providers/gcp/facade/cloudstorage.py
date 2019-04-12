02288385c9c6621b7622c5f6e58cde363e908241
Rémi Pelletier <remi_pelletier_6@hotmail.com>
2019-04-12 04:00:00 -0400

    Added error handling for Cloud storage

--- ScoutSuite/providers/gcp/facade/cloudstorage.py
+++ ScoutSuite/providers/gcp/facade/cloudstorage.py
@@ -1,19 +1,30 @@
 import asyncio
 from google.cloud import storage
+from ScoutSuite.core.console import print_exception
 from ScoutSuite.providers.utils import run_concurrently, get_and_set_concurrently
 
 class CloudStorageFacade:
     async def get_buckets(self, project_id: str):
-        client = storage.Client(project=project_id)
-        buckets = await run_concurrently(lambda: list(client.list_buckets()))
-        await get_and_set_concurrently([self._get_and_set_bucket_logging, 
-            self._get_and_set_bucket_iam_policy], buckets)
-        return buckets
+        try:
+            client = storage.Client(project=project_id)
+            buckets = await run_concurrently(lambda: list(client.list_buckets()))
+            await get_and_set_concurrently([self._get_and_set_bucket_logging, 
+                self._get_and_set_bucket_iam_policy], buckets)
+            return buckets
+        except Exception as e:
+            print_exception('Failed to retrieve storage buckets: {}'.format(e))
+            return []
 
     async def _get_and_set_bucket_logging(self, bucket):
-        bucket_logging = await run_concurrently(lambda: bucket.get_logging())
-        setattr(bucket, 'logging', bucket_logging)
+        try:
+            bucket_logging = await run_concurrently(lambda: bucket.get_logging())
+            setattr(bucket, 'logging', bucket_logging)
+        except Exception as e:
+            print_exception('Failed to retrieve bucket logging: {}'.format(e))
 
     async def _get_and_set_bucket_iam_policy(self, bucket):
-        bucket_iam_policy = await run_concurrently(lambda: bucket.get_iam_policy())
-        setattr(bucket, 'iam_policy', bucket_iam_policy)
+        try:
+            bucket_iam_policy = await run_concurrently(lambda: bucket.get_iam_policy())
+            setattr(bucket, 'iam_policy', bucket_iam_policy)
+        except Exception as e:
+            print_exception('Failed to retrieve bucket IAM policy: {}'.format(e))
