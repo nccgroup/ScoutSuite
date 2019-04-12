e4f3323d0e98441fef818e5e9fe6c31ebed3cae1
Rémi Pelletier <remi_pelletier_6@hotmail.com>
2019-04-12 03:17:25 -0400

    Switched to using get_and_set()

--- ScoutSuite/providers/gcp/facade/cloudstorage.py
+++ ScoutSuite/providers/gcp/facade/cloudstorage.py
@@ -1,18 +1,19 @@
 import asyncio
 from google.cloud import storage
-from ScoutSuite.providers.utils import run_concurrently
+from ScoutSuite.providers.utils import run_concurrently, get_and_set_concurrently
 
 class CloudStorageFacade:
     async def get_buckets(self, project_id: str):
         client = storage.Client(project=project_id)
         buckets = await run_concurrently(lambda: list(client.list_buckets()))
-        if len(buckets) > 0:
-            await asyncio.wait({asyncio.ensure_future(self._fetch_additional_bucket_info(bucket)) for bucket in buckets})
+        await get_and_set_concurrently([self._get_and_set_bucket_logging, 
+            self._get_and_set_bucket_iam_policy], buckets)
         return buckets
 
-    async def _fetch_additional_bucket_info(self, bucket):
-        # We're adding new properties to the existing Bucket instance because creating
-        # a new dictionary and copying the bucket's __dict__ caused problem since 
-        # there are properties that do not have a corresponding attribute. 
-        bucket.__dict__['logging'] = await run_concurrently(lambda: bucket.get_logging())
-        bucket.__dict__['iam_policy'] = await run_concurrently(lambda: bucket.get_iam_policy())
+    async def _get_and_set_bucket_logging(self, bucket):
+        bucket_logging = await run_concurrently(lambda: bucket.get_logging())
+        setattr(bucket, 'logging', bucket_logging)
+
+    async def _get_and_set_bucket_iam_policy(self, bucket):
+        bucket_iam_policy = await run_concurrently(lambda: bucket.get_iam_policy())
+        setattr(bucket, 'iam_policy', bucket_iam_policy)
