937c1feff5fb13b4696c9c91f2eed314ddf60854
Rémi Pelletier <remi_pelletier_6@hotmail.com>
2019-04-12 02:44:26 -0400

    Fixed error for empty Buckets list and renamed method

--- ScoutSuite/providers/gcp/facade/cloudstorage.py
+++ ScoutSuite/providers/gcp/facade/cloudstorage.py
@@ -6,10 +6,11 @@ class CloudStorageFacade:
     async def get_buckets(self, project_id: str):
         client = storage.Client(project=project_id)
         buckets = await run_concurrently(lambda: list(client.list_buckets()))
-        await asyncio.wait({asyncio.ensure_future(self._add_additional_info(bucket)) for bucket in buckets})
+        if len(buckets) > 0:
+            await asyncio.wait({asyncio.ensure_future(self._fetch_additional_bucket_info(bucket)) for bucket in buckets})
         return buckets
 
-    async def _add_additional_info(self, bucket):
+    async def _fetch_additional_bucket_info(self, bucket):
         # We're adding new properties to the existing Bucket instance because creating
         # a new dictionary and copying the bucket's __dict__ caused problem since 
         # there are properties that do not have a corresponding attribute. 
