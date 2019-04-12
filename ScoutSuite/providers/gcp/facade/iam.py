e0cd8586106667af55f1d8f4228a3e44d0470984
Rémi Pelletier <remi_pelletier_6@hotmail.com>
2019-04-12 03:32:05 -0400

    Added error handling for IAM

--- ScoutSuite/providers/gcp/facade/iam.py
+++ ScoutSuite/providers/gcp/facade/iam.py
@@ -1,3 +1,4 @@
+from ScoutSuite.core.console import print_exception
 from ScoutSuite.providers.gcp.facade.base import GCPBaseFacade
 from ScoutSuite.providers.gcp.facade.utils import GCPFacadeUtils
 from ScoutSuite.providers.utils import run_concurrently
@@ -7,24 +8,36 @@ class IAMFacade(GCPBaseFacade):
         super(IAMFacade, self).__init__('iam', 'v1')
 
     async def get_bindings(self, project_id: str, service_account_email: str):
-        resource = 'projects/{}/serviceAccounts/{}'.format(project_id, service_account_email)
-        iam_client = self._get_client()
-        response = await run_concurrently(
-                lambda: iam_client.projects().serviceAccounts().getIamPolicy(resource=resource).execute()
-        )
-        return response.get('bindings', [])
+        try:
+            resource = 'projects/{}/serviceAccounts/{}'.format(project_id, service_account_email)
+            iam_client = self._get_client()
+            response = await run_concurrently(
+                    lambda: iam_client.projects().serviceAccounts().getIamPolicy(resource=resource).execute()
+            )
+            return response.get('bindings', [])
+        except Exception as e:
+            print_exception('Failed to retrieve service account bindings: {}'.format(e))
+            return []
 
     async def get_keys(self, project_id: str, service_account_email: str):
-        name = 'projects/{}/serviceAccounts/{}'.format(project_id, service_account_email)
-        iam_client = self._get_client()
-        response = await run_concurrently(
-                lambda: iam_client.projects().serviceAccounts().keys().list(name=name).execute()
-        )
-        return response.get('keys', [])
+        try:
+            name = 'projects/{}/serviceAccounts/{}'.format(project_id, service_account_email)
+            iam_client = self._get_client()
+            response = await run_concurrently(
+                    lambda: iam_client.projects().serviceAccounts().keys().list(name=name).execute()
+            )
+            return response.get('keys', [])
+        except Exception as e:
+            print_exception('Failed to retrieve service account keys: {}'.format(e))
+            return []
 
     async def get_service_accounts(self, project_id: str):
-        name = 'projects/{}'.format(project_id)     
-        iam_client = self._get_client()
-        request = iam_client.projects().serviceAccounts().list(name=name)
-        service_accounts_group = iam_client.projects().serviceAccounts()
-        return await GCPFacadeUtils.get_all('accounts', request, service_accounts_group)
+        try:
+            name = 'projects/{}'.format(project_id)     
+            iam_client = self._get_client()
+            request = iam_client.projects().serviceAccounts().list(name=name)
+            service_accounts_group = iam_client.projects().serviceAccounts()
+            return await GCPFacadeUtils.get_all('accounts', request, service_accounts_group)
+        except Exception as e:
+            print_exception('Failed to retrieve service accounts: {}'.format(e))
+            return []
