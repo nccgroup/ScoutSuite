9a330f24952999d8a1df18432e7ec2a6c19338c3
Antoine Boisier-Michaud <aboisiermichaud@gmail.com>
2019-04-15 08:43:32 -0400

    Updated peering connections to work with cleanup

--- ScoutSuite/providers/aws/resources/vpc/peering_connections.py
+++ ScoutSuite/providers/aws/resources/vpc/peering_connections.py
@@ -1,9 +1,14 @@
+from ScoutSuite.providers.aws.facade.base import AWSFacade
 from ScoutSuite.providers.aws.resources.base import AWSResources
 
 
 class PeeringConnections(AWSResources):
-    async def fetch_all(self, **kwargs):
-        raw_peering_connections = await self.facade.ec2.get_peering_connections(self.scope['region'])
+    def __init__(self, facade: AWSFacade, region: str):
+        self.facade = facade
+        self.region = region
+
+    async def fetch_all(self):
+        raw_peering_connections = await self.facade.ec2.get_peering_connections(self.region)
         for raw_peering_connection in raw_peering_connections:
             id, peering_connection = self._parse_peering_connections(raw_peering_connection)
             self[id] = peering_connection
