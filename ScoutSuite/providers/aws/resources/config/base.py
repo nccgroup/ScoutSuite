7b6c6a59aa48652b117d574a4780d12f4be1bed7
Xavier Garceau-Aranda <xavier.garceau-aranda@nccgroup.com>
2019-04-25 18:24:07 +0200

    Remove outdated code

--- ScoutSuite/providers/aws/resources/config/base.py
+++ ScoutSuite/providers/aws/resources/config/base.py
@@ -16,8 +16,3 @@ class Config(Regions):
     async def fetch_all(self, regions=None, partition_name='aws'):
         await super(Config, self).fetch_all(regions, partition_name)
 
-        for region in self['regions']:
-            config_enabled = any(recorder['config_enabled']
-                                 for recorder in self['regions'][region]['recorders'].values())
-            self['regions'][region]['config_enabled'] = config_enabled
-            self['regions'][region]['regions_count'] = 1
