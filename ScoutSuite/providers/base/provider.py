d48101344716a96d98fc8f753e0f510de7ea2277
Xavier Garceau-Aranda <xavier.garceau-aranda@nccgroup.com>
2019-04-26 14:28:50 +0200

    Fix bug inserted  in https://github.com/nccgroup/ScoutSuite/pull/379

--- ScoutSuite/providers/base/provider.py
+++ ScoutSuite/providers/base/provider.py
@@ -22,7 +22,8 @@ class BaseProvider(object):
     all cloud providers
     """
 
-    def __init__(self, report_dir=None, timestamp=None, services=None, skipped_services=None,
+    def __init__(self, report_dir=None, timestamp=None,
+                 services=None, skipped_services=None,
                  result_format='json', **kwargs):
         """
 
@@ -40,9 +41,10 @@ class BaseProvider(object):
 
         self._load_metadata()
 
-        if not self.services:
+        if not hasattr(self, 'services'):
             self.services = self.services_config(self.credentials)
         supported_services = vars(self.services).keys()
+
         self.service_list = self._build_services_list(supported_services, services, skipped_services)
 
     def get_report_name(self):
