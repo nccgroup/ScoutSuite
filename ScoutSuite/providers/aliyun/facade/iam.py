11fc0609c5a84ff936197d67350952e953cefd31
Xavier Garceau-Aranda <xavier.garceau-aranda@owasp.org>
2019-06-02 20:09:47 +0200

    Reformat Aliyun requests

--- ScoutSuite/providers/aliyun/facade/iam.py
+++ ScoutSuite/providers/aliyun/facade/iam.py
@@ -1,11 +1,7 @@
-import json
-
 from ScoutSuite.providers.aliyun.authentication_strategy import AliyunCredentials
-from ScoutSuite.providers.utils import run_concurrently
+from ScoutSuite.providers.aliyun.facade.utils import get_response
 
 from aliyunsdkram.request.v20150501 import ListUsersRequest, ListAccessKeysRequest
-from aliyunsdkcore.acs_exception.exceptions import ClientException
-from aliyunsdkcore.acs_exception.exceptions import ServerException
 
 
 class IAMFacade:
@@ -13,31 +9,24 @@ class IAMFacade:
         self._client = credentials.client
 
     async def get_users(self):
+        """
+        Get all users
 
-        # TODO handle truncated response
-        try:
-            response = await run_concurrently(lambda:
-                                              self._client.do_action_with_exception(
-                                                  ListUsersRequest.ListUsersRequest()))
-            response_decoded = json.loads(response)
-            return response_decoded['Users']['User']
-        except ServerException as e:
-            print(e)  # TODO log
-        except ClientException as e:
-            print(e)  # TODO log
+        :return: a list of all users
+        """
+        response = await get_response(client=self._client,
+                                request=ListUsersRequest.ListUsersRequest())
+        return response['Users']['User']
 
     async def get_user_api_keys(self, username):
+        """
+        Get API keys for a user
 
-        # TODO handle truncated response
-        try:
-
-            request = ListAccessKeysRequest.ListAccessKeysRequest()
-            request.set_UserName(username)
-            response = await run_concurrently(lambda:
-                                              self._client.do_action_with_exception(request))
-            response_decoded = json.loads(response)
-            return response_decoded['AccessKeys']['AccessKey']
-        except ServerException as e:
-            print(e)  # TODO log
-        except ClientException as e:
-            print(e)  # TODO log
+        :param username: The username of the user
+        :return: the list of API keys for that user
+        """
+        request = ListAccessKeysRequest.ListAccessKeysRequest()
+        request.set_UserName(username)
+        response = await get_response(client=self._client,
+                                      request=request)
+        return response['AccessKeys']['AccessKey']
