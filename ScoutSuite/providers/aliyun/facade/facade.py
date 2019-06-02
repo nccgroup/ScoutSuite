358114036d6515965ce8d32e49143fa3da8e3265
Xavier Garceau-Aranda <xavier.garceau-aranda@owasp.org>
2019-06-02 20:10:03 +0200

    Add facade

--- ScoutSuite/providers/aliyun/facade/facade.py
+++ ScoutSuite/providers/aliyun/facade/facade.py
@@ -1,7 +1,9 @@
-from ScoutSuite.providers.aliyun.facade.iam import IAMFacade
 from ScoutSuite.providers.aliyun.authentication_strategy import AliyunCredentials
+from ScoutSuite.providers.aliyun.facade.iam import IAMFacade
+from ScoutSuite.providers.aliyun.facade.actiontrail import ActiontrailFacade
 
 
 class AliyunFacade():
     def __init__(self, credentials: AliyunCredentials):
         self.iam = IAMFacade(credentials)
+        self.actiontrail = ActiontrailFacade(credentials)
