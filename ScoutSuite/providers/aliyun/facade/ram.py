f2a7a29475411c5efa6d943002dc2f42c91e2961
Xavier Garceau-Aranda <xavier.garceau-aranda@owasp.org>
2019-06-08 14:30:28 +0200

    Add initial support for Aliyun RAM password policy

--- ScoutSuite/providers/aliyun/facade/ram.py
+++ ScoutSuite/providers/aliyun/facade/ram.py
@@ -2,7 +2,7 @@ from ScoutSuite.providers.aliyun.authentication_strategy import AliyunCredential
 from ScoutSuite.providers.aliyun.facade.utils import get_response
 
 from ScoutSuite.core.console import print_exception
-from aliyunsdkram.request.v20150501 import ListUsersRequest, ListAccessKeysRequest, GetUserMFAInfoRequest, GetUserRequest, GetAccessKeyLastUsedRequest
+from aliyunsdkram.request.v20150501 import ListUsersRequest, ListAccessKeysRequest, GetUserMFAInfoRequest, GetUserRequest, GetAccessKeyLastUsedRequest, GetPasswordPolicyRequest
 
 
 class RAMFacade:
@@ -85,3 +85,14 @@ class RAMFacade:
                 return False, None
         else:
             return True, response['MFADevice']['SerialNumber']
+
+    async def get_password_policy(self):
+        """
+        Get the account's password policy
+
+        :return: the password policy
+        """
+        request = GetPasswordPolicyRequest.GetPasswordPolicyRequest()
+        response = await get_response(client=self._client,
+                                      request=request)
+        return response['PasswordPolicy']
--- ScoutSuite/providers/aliyun/resources/ram/base.py
+++ ScoutSuite/providers/aliyun/resources/ram/base.py
@@ -1,13 +1,19 @@
 from ScoutSuite.providers.aliyun.resources.base import AliyunCompositeResources
 from ScoutSuite.providers.aliyun.resources.ram.users import Users
+from ScoutSuite.providers.aliyun.resources.ram.password_policy import PasswordPolicy
 
 
 class RAM(AliyunCompositeResources):
     _children = [
         (Users, 'users'),
+        (PasswordPolicy, 'password_policy')
     ]
 
     async def fetch_all(self):
         await self._fetch_children(resource_parent=self)
+
+        # We do not want the report to count the password policies as resources,
+        # they aren't really resources.
+        self['password_policy_count'] = 0
 
         # TODO for each user check last login & API key usage for "last activity"
