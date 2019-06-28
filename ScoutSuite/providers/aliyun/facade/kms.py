14019f070b5d38a02ba822640afac5a7be29effa
Xavier Garceau-Aranda <xavier.garceau-aranda@nccgroup.com>
2019-06-28 20:04:31 +0200

    Add basic KMS support

--- ScoutSuite/providers/aliyun/facade/kms.py
+++ ScoutSuite/providers/aliyun/facade/kms.py
@@ -1,4 +1,4 @@
-from aliyunsdkkms.request.v20160120 import ListKeysRequest
+from aliyunsdkkms.request.v20160120 import ListKeysRequest, DescribeKeyRequest
 
 from ScoutSuite.providers.aliyun.authentication_strategy import AliyunCredentials
 from ScoutSuite.providers.aliyun.facade.utils import get_response
@@ -7,6 +7,7 @@ from ScoutSuite.providers.aliyun.facade.utils import get_response
 class KMSFacade:
     def __init__(self, credentials: AliyunCredentials):
         self._client = credentials.client
+        self._client.set_region_id('eu-west-1')  # FIXME this shouldn't be done here
 
     async def get_keys(self):
         """
@@ -14,9 +15,19 @@ class KMSFacade:
 
         :return: a list of all keys
         """
-
-        self._client.set_region_id('eu-west-1')  # FIXME this shouldn't be done here
-
         response = await get_response(client=self._client,
                                       request=ListKeysRequest.ListKeysRequest())
         return response['Keys']['Key']
+
+    async def get_key_details(self, key_id):
+        """
+        Gets details for a key
+
+        :return: a dictionary of details
+        """
+
+        request = DescribeKeyRequest.DescribeKeyRequest()
+        request.set_KeyId(key_id)
+        response = await get_response(client=self._client,
+                                      request=request)
+        return response['KeyMetadata']
