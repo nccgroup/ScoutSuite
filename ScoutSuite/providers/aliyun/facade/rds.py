bde07468360bfbfba01fee30994e686ffe667946
Xavier Garceau-Aranda <xavier.garceau-aranda@nccgroup.com>
2019-06-28 20:04:38 +0200

    Tweaks

--- ScoutSuite/providers/aliyun/facade/rds.py
+++ ScoutSuite/providers/aliyun/facade/rds.py
@@ -7,6 +7,7 @@ from ScoutSuite.providers.aliyun.facade.utils import get_response
 class RDSFacade:
     def __init__(self, credentials: AliyunCredentials):
         self._client = credentials.client
+        self._client.set_region_id('eu-west-1')  # FIXME this shouldn't be done here
 
     async def get_instances(self):
         """
@@ -14,9 +15,6 @@ class RDSFacade:
 
         :return: a list of all instances
         """
-
-        self._client.set_region_id('eu-west-1')  # FIXME this shouldn't be done here
-
         response = await get_response(client=self._client,
                                       request=DescribeDBInstancesRequest.DescribeDBInstancesRequest())
         return response['Items']['DBInstance']
