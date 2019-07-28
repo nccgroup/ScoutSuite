a6cf19412303a7234c292477dc797d8e95147adc
Xavier Garceau-Aranda <xavier.garceau-aranda@nccgroup.com>
2019-07-28 14:05:32 +0200

    Fix imports and instanciations

--- ScoutSuite/providers/azure/facade/base.py
+++ ScoutSuite/providers/azure/facade/base.py
@@ -1,15 +1,25 @@
+from ScoutSuite.providers.azure.authentication_strategy import AzureCredentials
+from ScoutSuite.providers.azure.facade.graphrbac import GraphRBACFacade
 from ScoutSuite.providers.azure.facade.keyvault import KeyVaultFacade
 from ScoutSuite.providers.azure.facade.network import NetworkFacade
 from ScoutSuite.providers.azure.facade.securitycenter import SecurityCenterFacade
 from ScoutSuite.providers.azure.facade.sqldatabase import SQLDatabaseFacade
 from ScoutSuite.providers.azure.facade.storageaccounts import StorageAccountsFacade
-from ScoutSuite.providers.azure.facade.graphrbac import GraphRBACFacade
-from ScoutSuite.providers.azure.authentication_strategy import AzureCredentials
 
+# Try to import proprietary services
 try:
     from ScoutSuite.providers.azure.facade.appgateway_private import AppGatewayFacade
+except ImportError:
+    pass
+try:
     from ScoutSuite.providers.azure.facade.appservice_private import AppServiceFacade
+except ImportError:
+    pass
+try:
     from ScoutSuite.providers.azure.facade.loadbalancer_private import LoadBalancerFacade
+except ImportError:
+    pass
+try:
     from ScoutSuite.providers.azure.facade.rediscache_private import RedisCacheFacade
 except ImportError:
     pass
@@ -24,10 +34,20 @@ class AzureFacade():
         self.storageaccounts = StorageAccountsFacade(credentials.credentials, credentials.subscription_id)
         self.graphrbac = GraphRBACFacade(credentials.graphrbac_credentials, credentials.tenant_id)
 
+        # Instantiate facades for proprietary services
         try:
             self.appgateway = AppGatewayFacade(credentials.credentials, credentials.subscription_id)
+        except NameError:
+            pass
+        try:
             self.appservice = AppServiceFacade(credentials.credentials, credentials.subscription_id)
+        except NameError:
+            pass
+        try:
             self.loadbalancer = LoadBalancerFacade(credentials.credentials, credentials.subscription_id)
+        except NameError:
+            pass
+        try:
             self.rediscache = RedisCacheFacade(credentials.credentials, credentials.subscription_id)
         except NameError:
             pass
