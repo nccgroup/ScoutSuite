b98728d60bc3fc363a1ae13f1f7b3abeb34eb7d4
Xavier Garceau-Aranda <xavier.garceau-aranda@nccgroup.com>
2019-07-30 17:45:58 +0200

    Improve NI SG reference

--- ScoutSuite/providers/azure/resources/network/network_interfaces.py
+++ ScoutSuite/providers/azure/resources/network/network_interfaces.py
@@ -25,7 +25,7 @@ class NetworkInterfaces(AzureResources):
         network_interface_dict['resource_guid'] = raw_network_interface.resource_guid
         network_interface_dict['enable_ip_forwarding'] = raw_network_interface.enable_ip_forwarding
         network_interface_dict['type'] = raw_network_interface.type
-        network_interface_dict['network_security_group'] = raw_network_interface.network_security_group
+        network_interface_dict['network_security_group'] = get_non_provider_id(raw_network_interface.network_security_group.id)
 
         # TODO process and display the below
         network_interface_dict['hosted_workloads'] = raw_network_interface.hosted_workloads
