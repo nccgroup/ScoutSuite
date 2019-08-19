78693353cad514e234ec93e9bd1986b7cb057930
Xavier Garceau-Aranda <xavier.garceau-aranda@nccgroup.com>
2019-08-19 10:04:14 +0200

    Include kwargs

--- ScoutSuite/providers/aws/resources/ec2/base.py
+++ ScoutSuite/providers/aws/resources/ec2/base.py
@@ -16,7 +16,7 @@ class EC2(Regions):
     def __init__(self, facade):
         super(EC2, self).__init__('ec2', facade)
 
-    async def fetch_all(self, regions=None, excluded_regions=None, partition_name='aws'):
+    async def fetch_all(self, regions=None, excluded_regions=None, partition_name='aws', **kwargs):
         await super(EC2, self).fetch_all(regions, excluded_regions, partition_name)
 
         for region in self['regions']:
