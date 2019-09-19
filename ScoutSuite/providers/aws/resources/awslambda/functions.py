85b4d20ddcd1964aaa1b35023346ea762e21299a
Xavier Garceau-Aranda <xavier.garceau-aranda@nccgroup.com>
2019-09-19 12:07:56 +0100

    Improve lambda support

--- ScoutSuite/providers/aws/resources/awslambda/functions.py
+++ ScoutSuite/providers/aws/resources/awslambda/functions.py
@@ -14,5 +14,20 @@ class Functions(AWSResources):
             self[name] = resource
 
     def _parse_function(self, raw_function):
-        raw_function['name'] = raw_function.pop('FunctionName')
-        return raw_function['name'], raw_function
+        function_dict = {}
+        function_dict['name'] = raw_function.get('FunctionName')
+        function_dict['arn'] = raw_function.get('FunctionArn')
+        function_dict['runtime'] = raw_function.get('Runtime')
+        function_dict['role'] = raw_function.get('Role')
+        function_dict['handler'] = raw_function.get('Handler')
+        function_dict['code_size'] = raw_function.get('CodeSize')
+        function_dict['description'] = raw_function.get('Description')
+        function_dict['timeout'] = raw_function.get('Timeout')
+        function_dict['memory_size'] = raw_function.get('MemorySize')
+        function_dict['last_modified'] = raw_function.get('LastModified')
+        function_dict['code_sha256'] = raw_function.get('CodeSha256')
+        function_dict['version'] = raw_function.get('Version')
+        function_dict['tracing_config'] = raw_function.get('TracingConfig')
+        function_dict['revision_id'] = raw_function.get('RevisionId')
+        return function_dict['name'], function_dict
+
