40d7b00575b2cc3082a6e55864355667ec3910bb
rracterr <ec2-user@ip-10-166-140-206.ec2.internal>
2019-12-27 18:24:42 +0000

    reworked to include entire certificate body

--- ScoutSuite/providers/aws/resources/acm/certificates.py
+++ ScoutSuite/providers/aws/resources/acm/certificates.py
@@ -14,19 +14,8 @@ class Certificates(AWSResources):
             self[name] = resource
 
     def _parse_certificate(self, raw_certificate):
+        raw_certificate['name'] = raw_certificate['CertificateArn']
+        raw_certificate['arn'] = raw_certificate['CertificateArn']
 
-        certificate_dict = {}
-        certificate_dict['name'] = raw_certificate.get('CertificateArn')
-        certificate_dict['CertificateArn'] = raw_certificate.get('CertificateArn')
-        certificate_dict['DomainName'] = raw_certificate.get('DomainName')
-        certificate_dict['Subject'] = raw_certificate.get('Subject')
-        certificate_dict['Issuer'] = raw_certificate.get('Issuer')
-        certificate_dict['Status'] = raw_certificate.get('Status')
-        certificate_dict['RevokedReason'] = raw_certificate.get('RevokedReason')
-        certificate_dict['KeyAlgorithm'] = raw_certificate.get('KeyAlgorithm')
-        certificate_dict['FailureReason'] = raw_certificate.get('FailureReason')
-        certificate_dict['CertificateAuthorityArn'] = raw_certificate.get('CertificateAuthorityArn')
-        certificate_dict['CertificateTransparencyLoggingPreference'] = raw_certificate.get('Options').get('CertificateTransparencyLoggingPreference')
-
-        return certificate_dict['CertificateArn'], certificate_dict
+        return raw_certificate.get('CertificateArn'), raw_certificate
 
