1d3788480765379be25e1538cfa630ffb9718e55
rracterr <ec2-user@ip-10-166-140-206.ec2.internal>
2019-12-30 18:45:59 +0000

    updated the partial to display all cert details updated certificate to load raw cert

--- ScoutSuite/providers/aws/resources/acm/certificates.py
+++ ScoutSuite/providers/aws/resources/acm/certificates.py
@@ -14,24 +14,8 @@ class Certificates(AWSResources):
             self[name] = resource
 
     def _parse_certificate(self, raw_certificate):
-        certificate_dict = {}
+#        lowercase_certificate = dict((k.lower(), v) for k,v in raw_certificate.items())
 
-        certificate_dict['certificate_arn'] = raw_certificate.get('CertificateArn')
-        certificate_dict['domain_name'] = raw_certificate.get('DomainName')
-        certificate_dict['subject_alternative_names'] = raw_certificate.get('SubjectAlternativeNames')
-        certificate_dict['domain_validation_options'] = raw_certificate.get('DomainValidationOptions')
-        certificate_dict['subject'] = raw_certificate.get('Subject')
-        certificate_dict['issuer'] = raw_certificate.get('Issuer')
-        certificate_dict['created_at'] = raw_certificate.get('CreatedAt')
-        certificate_dict['status'] = raw_certificate.get('Status')
-        certificate_dict['key_algorithm'] = raw_certificate.get('KeyAlgorithm')
-        certificate_dict['signature_algorithm'] = raw_certificate.get('SignatureAlgorithm')
-        certificate_dict['in_use_by'] = raw_certificate.get('InUseBy')
-        certificate_dict['type'] = raw_certificate.get('Type')
-        certificate_dict['key_usages'] = raw_certificate.get('KeyUsages')
-        certificate_dict['extended_key_usages'] = raw_certificate.get('ExtendedKeyUsages')
-        certificate_dict['renewal_eligibility'] = raw_certificate.get('RenewalEligibility')
-        certificate_dict['options'] = raw_certificate.get('Options')
-        certificate_dict['name'] = raw_certificate.get('DomainName')
+        raw_certificate['name'] = raw_certificate.get('DomainName')
 
-        return certificate_dict['name'], certificate_dict
+        return raw_certificate['name'], raw_certificate
