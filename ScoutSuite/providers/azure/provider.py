147a8c83f7e594d5ba0206e96c0ccc15a0ec5535
Xavier Garceau-Aranda <xavier.garceau-aranda@nccgroup.com>
2020-01-28 13:28:43 +0100

    Fix partial paths

--- ScoutSuite/providers/azure/provider.py
+++ ScoutSuite/providers/azure/provider.py
@@ -81,18 +81,19 @@ class AzureProvider(BaseProvider):
                     role_id = assignment['role_definition_id'].split('/')[-1]
                     for group in self.services['aad']['groups']:
                         if group == assignment['principal_id']:
-                            self.services['aad']['groups'][group]['roles'].append(role_id)
+                            self.services['aad']['groups'][group]['roles'].append({'subscription_id': subscription,
+                                                                                 'role_id': role_id})
                             self.services['arm']['subscriptions'][subscription]['roles'][role_id]['assignments']['groups'].append(group)
                             self.services['arm']['subscriptions'][subscription]['roles'][role_id]['assignments_count'] += 1
                     for user in self.services['aad']['users']:
                         if user == assignment['principal_id']:
-                            self.services['aad']['users'][user]['roles'].append(role_id)
+                            self.services['aad']['users'][user]['roles'].append({'subscription_id': subscription,
+                                                                                 'role_id': role_id})
                             self.services['arm']['subscriptions'][subscription]['roles'][role_id]['assignments']['users'].append(user)
                             self.services['arm']['subscriptions'][subscription]['roles'][role_id]['assignments_count'] += 1
                     for service_principal in self.services['aad']['service_principals']:
                         if service_principal == assignment['principal_id']:
-                            self.services['aad']['service_principals'][service_principal]['roles'].append(role_id)
+                            self.services['aad']['service_principals'][service_principal]['roles'].append({'subscription_id': subscription,
+                                                                                                           'role_id': role_id})
                             self.services['arm']['subscriptions'][subscription]['roles'][role_id]['assignments']['service_principals'].append(service_principal)
                             self.services['arm']['subscriptions'][subscription]['roles'][role_id]['assignments_count'] += 1
-
-        pass
