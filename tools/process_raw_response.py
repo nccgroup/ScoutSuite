c09c9653c0b7e7fbc18967f0141aae1dc881fc99
Xavier Garceau-Aranda <xavier.garceau-aranda@nccgroup.com>
2020-01-30 10:21:07 +0100

    Improve parsing

--- tools/process_raw_response.py
+++ tools/process_raw_response.py
@@ -61,8 +61,9 @@ if __name__ == "__main__":
         object_value_dict = eval(cleaned_value)
     elif args.provider == 'azure':
         object_format = 'raw_{}.{}'
-        cleaned_value = args.value.replace('<class \'dict\'>: ', '')
-        cleaned_value = cleaned_value.replace('<azure', '\'<azure').replace('>', '>\'')
+        pattern = re.compile(r'<[\w\'.:_\s]*>')
+        cleaned_value = pattern.sub('None', args.value)
+        cleaned_value = cleaned_value.replace(' {}', ' \'{}\'')
         object_value_dict = literal_eval(cleaned_value)
     elif args.provider == 'aliyun':
         object_format = 'raw_{}.get(\'{}\')'
