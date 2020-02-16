7a447bfaaf114b9136163d59d4e9581abb5b6c32
Xavier Garceau-Aranda <xavier.garceau-aranda@nccgroup.com>
2020-02-16 19:24:07 +0100

    Basic suport for alerts

--- ScoutSuite/providers/azure/resources/securitycenter/base.py
+++ ScoutSuite/providers/azure/resources/securitycenter/base.py
@@ -2,6 +2,7 @@ from ScoutSuite.providers.azure.resources.subscriptions import Subscriptions
 
 from .auto_provisioning_settings import AutoProvisioningSettings
 from .pricings import Pricings
+from .alerts import Alerts
 from .security_contacts import SecurityContacts
 # from .information_protection_policies import InformationProtectionPolicies
 # from .settings import Settings
@@ -11,6 +12,7 @@ class SecurityCenter(Subscriptions):
     _children = [
         (AutoProvisioningSettings, 'auto_provisioning_settings'),
         (Pricings, 'pricings'),
+        # (Alerts, 'alerts'),  # FIXME this needs to be tested with alert results...
         (SecurityContacts, 'security_contacts'),
         # (InformationProtectionPolicies, 'information_protection_policies'),  # FIXME this isn't properly implemented
         # (Settings, 'settings')  # FIXME this isn't implemented
