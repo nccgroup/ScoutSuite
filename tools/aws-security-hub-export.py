7043eb126a8e3744c92364707b714ff9dd1598b9
Juan Jose <juanjofs@gmail.com>
2020-02-18 14:15:10 +0100

    Mocked attributes

--- tools/aws-security-hub-export.py
+++ tools/aws-security-hub-export.py
@@ -10,4 +10,21 @@ with open('scoutsuite-report/scoutsuite-results/scoutsuite_results_aws-635327450
 
     #TODO parse json_file into new JSON variable
     json_aws_account = {"AwsAccountId": json_file["account_id"]}
+    json_aws_account["CreatedAt"] = json_file["last_run"]["time"]
+    json_aws_account["Description"] = json_file["services"]["config"]["findings"]["config-recorder-not-configured"]["description"]
+    json_aws_account["GeneratorId"] = "PLACEHOLDER"
+    json_aws_account["Id"] = "PLACEHOLDER"
+    json_aws_account["ProductArn"] = "PLACEHOLDER"
+
+    #TODO insert correct Resources
+    json_aws_resources = {"test": "PLACEHOLDER"}
+
+    json_aws_account["Resources"] = json_aws_resources
+
+    json_aws_account["SchemaVersion"] = "PLACEHOLDER"
+    json_aws_account["Severity"] = "PLACEHOLDER"
+    json_aws_account["Title"] = "PLACEHOLDER"
+    json_aws_account["Types"] = "PLACEHOLDER"
+    json_aws_account["UpdatedAt"] = "PLACEHOLDER"
+
     print(json_aws_account)
