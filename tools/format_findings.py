97c6ea2e47d957c687817a6f70a6d757aa89181a
xga <xavier.garceau-aranda@owasp.org>
2020-04-06 13:14:26 +0200

    Add handling

--- tools/format_findings.py
+++ tools/format_findings.py
@@ -32,32 +32,35 @@ def format_folder(folder_path):
             except Exception as e:
                 print('exception {} for \"{}\"'.format(e, fn))
             else:
-                # change legacy field name - TODO remove once there are none left
-                if 'title' in data:
-                    data['description'] = data['title']
-                    data.pop('title', None)
-                # remove legacy HTML from rationale - TODO remove once there are none left
-                if 'rationale' in data.keys():
-                    data['rationale'] = data['rationale'].replace('<b>Description:</b><br><br>', '')
-                    # check for legacy content - TODO remove once there are none left
-                    if 'References' in data['rationale'] or 'CIS' in data['rationale']:
-                        print('Potentially legacy rationale for {}: {}'.format(fn, data['rationale']))
-                else:
-                    finding_with_no_rationale += 1
-                # capitalize titles
-                data['description'] = get_capitalized_title(data['description'])
-                # back to start
-                json_file.seek(0)
-                # sort keys
-                sort_order = ['description', 'rationale', 'remediation', 'compliance', 'references',
-                              'dashboard_name', 'display_path', 'path', 'conditions',
-                              'key', 'keys', 'arg_names', 'id_suffix']
                 try:
-                    ordered_data = OrderedDict(sorted(data.items(), key=lambda i: sort_order.index(i[0])))
+                    # change legacy field name - TODO remove once there are none left
+                    if 'title' in data:
+                        data['description'] = data['title']
+                        data.pop('title', None)
+                    # remove legacy HTML from rationale - TODO remove once there are none left
+                    if 'rationale' in data.keys():
+                        data['rationale'] = data['rationale'].replace('<b>Description:</b><br><br>', '')
+                        # check for legacy content - TODO remove once there are none left
+                        if 'References' in data['rationale'] or 'CIS' in data['rationale']:
+                            print('Potentially legacy rationale for {}: {}'.format(fn, data['rationale']))
+                    else:
+                        finding_with_no_rationale += 1
+                    # capitalize titles
+                    data['description'] = get_capitalized_title(data['description'])
+                    # back to start
+                    json_file.seek(0)
+                    # sort keys
+                    sort_order = ['description', 'rationale', 'remediation', 'compliance', 'references',
+                                  'dashboard_name', 'display_path', 'path', 'conditions',
+                                  'key', 'keys', 'arg_names', 'id_suffix', 'class_suffix']
+                    try:
+                        ordered_data = OrderedDict(sorted(data.items(), key=lambda i: sort_order.index(i[0])))
+                    except Exception as e:
+                        print('{}: {}'.format(fn, e))
+                    # save to file
+                    json.dump(ordered_data, json_file, sort_keys=False, indent=4)
                 except Exception as e:
-                    print('{}: {}'.format(fn, e))
-                # save to file
-                json.dump(ordered_data, json_file, sort_keys=False, indent=4)
+                    print('Failed to process {}: {}'.format(fn, e))
 
     print('Found {}/{} findings with no rationale'.format(finding_with_no_rationale, len(files)))
 
