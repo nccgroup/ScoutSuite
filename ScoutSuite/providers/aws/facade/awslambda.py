bc5fa85977bce1a39f33cfc3f5820eb3503bfcad
xga <xavier.garceau-aranda@owasp.org>
2020-04-28 19:32:18 +0200

    Improve error handling

--- ScoutSuite/providers/aws/facade/awslambda.py
+++ ScoutSuite/providers/aws/facade/awslambda.py
@@ -25,15 +25,20 @@ class LambdaFacade(AWSBaseFacade):
 
     async def get_role_with_managed_policies(self, role_name):
         client = AWSFacadeUtils.get_client('iam', self.session)
-        role = client.get_role(RoleName=role_name)['Role']
-        managed_policies = client.list_attached_role_policies(RoleName=role_name)['AttachedPolicies']
-        for policy in managed_policies:
-            policy_version = client.get_policy(PolicyArn=policy['PolicyArn'])
-            if 'Policy' in policy_version and 'DefaultVersionId' in policy_version['Policy']:
-                policy_version = policy_version['Policy']['DefaultVersionId']
-                document = client.get_policy_version(PolicyArn=policy['PolicyArn'], VersionId=policy_version)
-                if 'PolicyVersion' in document and 'Document' in document['PolicyVersion']:
-                    policy['Document'] = document['PolicyVersion']['Document']
-        role['policies'] = managed_policies
+        try:
+            role = client.get_role(RoleName=role_name)['Role']
+            managed_policies = client.list_attached_role_policies(RoleName=role_name)['AttachedPolicies']
+            for policy in managed_policies:
+                policy_version = client.get_policy(PolicyArn=policy['PolicyArn'])
+                if 'Policy' in policy_version and 'DefaultVersionId' in policy_version['Policy']:
+                    policy_version = policy_version['Policy']['DefaultVersionId']
+                    document = client.get_policy_version(PolicyArn=policy['PolicyArn'], VersionId=policy_version)
+                    if 'PolicyVersion' in document and 'Document' in document['PolicyVersion']:
+                        policy['Document'] = document['PolicyVersion']['Document']
+            role['policies'] = managed_policies
+            return role
+        except Exception:
+            # Policy not found for this function
+            return None
 
-        return role
+
