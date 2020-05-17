442371b5185d804f07af9e9f195d26df5c051a50
xga <xavier.garceau-aranda@owasp.org>
2020-05-17 18:35:50 +0200

    Add support for cloudwatch metric filters

--- ScoutSuite/providers/aws/resources/cloudwatch/base.py
+++ ScoutSuite/providers/aws/resources/cloudwatch/base.py
@@ -2,11 +2,13 @@ from ScoutSuite.providers.aws.facade.base import AWSFacade
 from ScoutSuite.providers.aws.resources.regions import Regions
 
 from .alarms import Alarms
+from .metric_filters import MetricFilters
 
 
 class CloudWatch(Regions):
     _children = [
-        (Alarms, 'alarms')
+        (Alarms, 'alarms'),
+        (MetricFilters, 'metric_filters')
     ]
 
     def __init__(self, facade: AWSFacade):
