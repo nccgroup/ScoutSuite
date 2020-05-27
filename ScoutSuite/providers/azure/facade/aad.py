4edaa65f67057958ed8f5b08330ac607cd296a47
xga <xavier.garceau-aranda@owasp.org>
2020-05-27 13:10:25 +0200

    Minor changes

--- ScoutSuite/providers/azure/facade/aad.py
+++ ScoutSuite/providers/azure/facade/aad.py
@@ -10,10 +10,6 @@ class AADFacade:
     def __init__(self, credentials):
         self.credentials = credentials
 
-    def get_client(self):
-        return GraphRbacManagementClient(self.credentials.get_credentials('aad_graph'),
-                                         tenant_id=self.credentials.get_tenant_id())
-
         self._microsoft_graph_session = requests.Session()
         self._microsoft_graph_session.headers.update(
             {'Authorization': 'Bearer {}'.format(credentials.microsoft_graph_credentials.token['access_token']),
@@ -22,6 +18,11 @@ class AADFacade:
              'Content-Type': 'application/json',
              'SdkVersion': 'scout-suite',
              'return-client-request-id': 'true'})
+
+    def get_client(self):
+        return GraphRbacManagementClient(self.credentials.get_credentials('aad_graph'),
+                                         tenant_id=self.credentials.get_tenant_id())
+
 
     async def _get_microsoft_graph_response(self, api_resource, api_version='v1.0'):
         endpoint = 'https://graph.microsoft.com/{}/{}'.format(api_version, api_resource)
@@ -33,6 +34,7 @@ class AADFacade:
             else:
                 print_exception('Failed to query Microsoft Graph endpoint \"{}\": status code {}'.
                                 format(api_resource, response.status_code))
+                return {}
         except Exception as e:
             print_exception('Failed to query Microsoft Graph endpoint \"{}\": {}'.format(api_resource, e))
             return {}
@@ -41,7 +43,6 @@ class AADFacade:
         try:
             test = await self._get_microsoft_graph_response('users')
             test_beta = await self._get_microsoft_graph_response('users', 'beta')
-            # r = await self._get_microsoft_graph_response('reports/getCredentialUserRegistrationCount', 'beta')
 
             # This filters down the users which are pulled from the directory, otherwise for large tenants this
             # gets out of hands.
@@ -49,7 +50,7 @@ class AADFacade:
             user_filter = " and ".join([
                 'userType eq \'Guest\''
             ])
-            users = run_concurrently(lambda: list(self.get_client().users.list(filter=user_filter)))
+            users = await run_concurrently(lambda: list(self.get_client().users.list(filter=user_filter)))
             return users
         except Exception as e:
             print_exception('Failed to retrieve users: {}'.format(e))
