f589b3ac3343fb95c1ca80fa573f475a3810b061
Nick Klauer <nklauer@zendesk.com>
2020-05-30 20:42:26 -0500

    fix: recursive descent snake_keys and tests

--- ScoutSuite/providers/aws/utils.py
+++ ScoutSuite/providers/aws/utils.py
@@ -99,6 +99,11 @@ def snake_keys(d):
         new_key = no_camel(k)
         if isinstance(d[k], dict):
             new_table[new_key] = snake_keys(d[k])
+        elif isinstance(d[k], list):
+            new_ary = []
+            for v in d[k]:
+                new_ary.append(snake_keys(v))
+            new_table[new_key] = new_ary
         else:
             new_table[new_key] = d[k]
     return new_table
