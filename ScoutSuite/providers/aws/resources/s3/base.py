6d5ef576e35efc53a4fce63f494e5068bce0257e
Alessandro.Gonzalez <Alessandro.Gonzalez@nccgroup.com>
2020-06-03 16:04:23 +0100

    Added public access block configuration to S3 buckets

--- ScoutSuite/providers/aws/resources/s3/base.py
+++ ScoutSuite/providers/aws/resources/s3/base.py
@@ -15,5 +15,25 @@ class S3(AWSCompositeResources):
     async def fetch_all(self, partition_name='aws', **kwargs):
         # Keep track of regions as S3 is both a global and regional service
         self.facade.s3.regions = kwargs.get('regions')
-
+        self['public_access_block_configuration'] = self.facade.s3.get_s3_public_access_block(self.facade.owner_id)
         await self._fetch_children(self)
+
+    async def finalize(self):
+        for bucket_id in self['buckets']:
+            if "public_access_block_configuration" in self['buckets'][bucket_id]:
+                # The resulting configuration will be the most restrictive
+                self['buckets'][bucket_id]["public_access_block_configuration"]["BlockPublicAcls"] = \
+                self['buckets'][bucket_id]["public_access_block_configuration"]["BlockPublicAcls"] or \
+                self['public_access_block_configuration']["BlockPublicAcls"]
+                self['buckets'][bucket_id]["public_access_block_configuration"]["IgnorePublicAcls"] = \
+                self['buckets'][bucket_id]["public_access_block_configuration"]["IgnorePublicAcls"] or \
+                self['public_access_block_configuration']["IgnorePublicAcls"]
+                self['buckets'][bucket_id]["public_access_block_configuration"]["BlockPublicPolicy"] = \
+                self['buckets'][bucket_id]["public_access_block_configuration"]["BlockPublicPolicy"] or \
+                self['public_access_block_configuration']["BlockPublicPolicy"]
+                self['buckets'][bucket_id]["public_access_block_configuration"]["RestrictPublicBuckets"] = \
+                self['buckets'][bucket_id]["public_access_block_configuration"]["RestrictPublicBuckets"] or \
+                self['public_access_block_configuration']["RestrictPublicBuckets"]
+            else:
+                # No bucket-level configuration, use account level configuration
+                self['buckets'][bucket_id]["public_access_block_configuration"] = self['public_access_block_configuration']
