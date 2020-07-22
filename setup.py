c1f8bdb19dfe00a478a621450e1d861d97e88c62
xga <xavier.garceau-aranda@owasp.org>
2020-07-22 16:11:14 +0200

    Update supported python versions

--- setup.py
+++ setup.py
@@ -56,9 +56,9 @@ setup(
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8'
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
     ],
 )
