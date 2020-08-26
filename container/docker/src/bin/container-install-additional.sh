d566a64f7278e1b42d1d84cc5f702006937eea6e
Thomas Morledge <thomas.morledge@nccgroup.com>
2020-08-26 10:03:08 +0100

    Fixed Azure CLI not installing. Install vim and nano to make the docker image more usable

---
+++ container/docker/src/bin/container-install-additional.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+apt-get update
+apt-get install -y vim nano
+
+echo -e "Additional Software Installation Complete!\n\n"
--- container/docker/src/bin/container-install-azure.sh
+++ container/docker/src/bin/container-install-azure.sh
@@ -20,25 +20,25 @@ echo -e "\n\nAzure CLI Installation Starting...\n\n"
 
 # manual process
 
-# add msft gpg key to apt
-curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /etc/apt/trusted.gpg.d/microsoft.asc.gpg
+# # add msft gpg key to apt
+# curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /etc/apt/trusted.gpg.d/microsoft.asc.gpg
+#
+# # set the right repo name
+# CLI_REPO=$(lsb_release -cs)
+#
+# # MSFT has no repo for focal yet, force the system to use eoan instead
+# if [[ ${CLI_REPO} -eq "focal" ]]; then
+#   CLI_REPO="eoan"
+# fi
+#
+# # add the msft repo to apt
+# echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ ${CLI_REPO} main" \
+#     > /etc/apt/sources.list.d/azure-cli.list
+#
+# # install the software
+# apt-get update && apt-get install -y azure-cli
 
-# set the right repo name
-CLI_REPO=$(lsb_release -cs)
-
-# MSFT has no repo for focal yet, force the system to use eoan instead
-if [[ ${CLI_REPO} -eq "focal" ]]; then
-  CLI_REPO="eoan"
-fi
-
-# add the msft repo to apt
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ ${CLI_REPO} main" \
-    > /etc/apt/sources.list.d/azure-cli.list
-
-# install the software
-apt-get update && apt-get install -y azure-cli
-
-# curl -sL https://aka.ms/InstallAzureCLIDeb | bash
+curl -sL https://aka.ms/InstallAzureCLIDeb | bash
 
 echo -e "\n"
 az --version
