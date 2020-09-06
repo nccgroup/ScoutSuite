fb80aec6dc0a9fe612b5a8ad437e88278cc9d0fc
xga <xavier.garceau-aranda@owasp.org>
2020-09-06 11:36:01 +0200

    Move folder to top directory

--- container/docker/src/bin/container-install-additional.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-apt-get update
-apt-get install -y vim nano less
-
-echo -e "Additional Software Installation Complete!\n\n"
--- container/docker/src/bin/container-set-motd.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cat <<'EOF' >> /root/.bashrc
-cd ${HOME}
-echo -e "Welcome to ScoutSuite!\nTo run ScoutSuite, just type \`scout -h\` to see the help documentation.\nHave fun!\n\n"
-EOF
