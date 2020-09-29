577c23305e72951760deb773498ceaaa17b682f4
xga <xavier.garceau-aranda@owasp.org>
2020-09-29 12:01:21 +0200

    Handle project not configured as a workspace

--- ScoutSuite/providers/gcp/facade/stackdrivermonitoring.py
+++ ScoutSuite/providers/gcp/facade/stackdrivermonitoring.py
@@ -7,6 +7,7 @@ from ScoutSuite.utils import get_user_agent
 
 
 class StackdriverMonitoringFacade:
+    # TODO find a way to skip the project if it's not configured as a stackdriver workspace
 
     def get_uptime_client(self):
         client_info = ClientInfo(user_agent=get_user_agent())
@@ -24,7 +25,8 @@ class StackdriverMonitoringFacade:
             name = client.project_path(project_id)
             return await run_concurrently(lambda: [r for r in client.list_uptime_check_configs(name)])
         except Exception as e:
-            print_exception(f'Failed to retrieve uptime checks: {e}')
+            if 'is not a workspace' not in e.message and '404' not in str(e):
+                print_exception(f'Failed to retrieve uptime checks: {e}')
             return []
 
     async def get_alert_policies(self, project_id: str):
@@ -33,5 +35,6 @@ class StackdriverMonitoringFacade:
             name = client.project_path(project_id)
             return await run_concurrently(lambda: [r for r in client.list_alert_policies(name)])
         except Exception as e:
-            print_exception(f'Failed to retrieve alert policies: {e}')
+            if 'is not a workspace' not in e.message and '404' not in str(e):
+                print_exception(f'Failed to retrieve alert policies: {e}')
             return []
