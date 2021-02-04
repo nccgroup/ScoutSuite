c33094abbee18466d9efc5f9f3e05d2cdcc1aad0
Samuel Meilleur <samuel.meilleur@polymtl.ca>
2021-02-03 19:53:46 -0500

    Created ServiceCard component.

---
+++ ScoutSuite/frontend/src/components/DetailedValue/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {
+  label: PropTypes.string.isRequired,
+  value: PropTypes.any.isRequired,
+  renderValue: PropTypes.func,
+}
+
+const defaultProps = {
+  renderValue: value => value,
+}
+
+const DetailedValue = props => {
+  const {
+    label,
+    value,
+    renderValue,
+  } = props;
+
+  return (
+    <div className="detailed-value"> 
+      <span className="label">{label}</span>
+      <span className="value">{renderValue(value)}</span>
+    </div>
+  )
+}
+
+DetailedValue.propTypes = propTypes;
+DetailedValue.defaultProps = defaultProps;
+
+export default DetailedValue;
