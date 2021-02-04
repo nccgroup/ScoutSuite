cf425c8f75ec957c9fd08707a2d0dd7cddf52191
Samuel Meilleur <samuel.meilleur@polymtl.ca>
2021-02-04 02:30:01 -0500

    Created Dashboard page.

--- ScoutSuite/frontend/src/App.js
+++ ScoutSuite/frontend/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import Dashboard from './pages/Dashboard';
+
+// TODO: Change for API hook
+const services = [
+  {
+    name: 'EC2',
+    warnings: 6,
+    issues: 6,
+    resources: 1450,
+    rules: 607,
+    checks: 439,
+  },
+  {
+    name: 'IAM',
+    warnings: 0,
+    issues: 0,
+    resources: 0,
+    rules: 0,
+    checks: 0,
+  },
+  {
+    name: 'S3',
+    warnings: 17,
+    issues: 15,
+    resources: 3514,
+    rules: 1097,
+    checks: 786,
+  },
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Dashboard services={services} />
   );
 }
 
