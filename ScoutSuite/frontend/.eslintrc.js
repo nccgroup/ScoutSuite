ab3a8d0fe59ef8620e2ed25497c4cd7a22c4ef80
Loïc Bellemare-Alford <loicbellemare@hotmail.com>
2021-02-09 10:46:18 -0500

    Fix running eslint

--- ScoutSuite/frontend/.eslintrc.js
+++ ScoutSuite/frontend/.eslintrc.js
@@ -1,38 +1,29 @@
 module.exports = {
     'env': {
         'browser': true,
-        'es2021': true
+        'es2021': true,
+        'node': true
     },
-    'extends': [
-        'eslint:recommended',
-        'plugin:react/recommended'
-    ],
+    'extends': ['eslint:recommended', 'plugin:react/recommended'],
     'parserOptions': {
         'ecmaFeatures': {
-            'jsx': true
+            'jsx': true,
+            'modules': true
         },
         'ecmaVersion': 12,
         'sourceType': 'module'
     },
-    'plugins': [
-        'react'
-    ],
+    'plugins': ['react'],
     'rules': {
-        'indent': [
-            'error',
-            4
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'never'
-        ]
+        'indent': ['error', 4],
+        'linebreak-style': ['error', 'unix'],
+        'quotes': ['error', 'single'],
+        'semi': ['error', 'never']
+    },
+    'settings': {
+        'react': {
+            'pragma': 'React',
+            'version': 'detect'
+        }
     }
 }
