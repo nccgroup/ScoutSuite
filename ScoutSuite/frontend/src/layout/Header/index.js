e84559085831630b72c936f8fcc5890b12411cf7
Loïc Bellemare-Alford <loicbellemare@hotmail.com>
2021-02-11 00:12:37 -0500

    Added dark mode toggle

--- ScoutSuite/frontend/src/layout/Header/index.js
+++ ScoutSuite/frontend/src/layout/Header/index.js
@@ -1,16 +1,29 @@
 import React from 'react';
+import Switch from '@material-ui/core/Switch';
+import { Link } from '@reach/router';
 
 import Logo from './ScoutSuiteLogo/logo.png'; 
 
 import './style.scss';
-import { Link } from '@reach/router';
 
 const Header = () => {
+  const [darkMode, setDarkMode] = React.useState(false);
+
+  const handleChange = (e) => {
+    document.body.classList.toggle('dark-mode');
+    setDarkMode(e.target.checked);
+  };
+
   return (
     <div className="nav-header">
       <div className="content">
         <Link to="/"><img src={Logo} /></Link>
-        <button>TEMP</button>
+        <Switch
+          checked={darkMode}
+          onChange={handleChange}
+          label="Dark Mode"
+          labelPlacement="start"
+        />
       </div>
     </div>
   );
