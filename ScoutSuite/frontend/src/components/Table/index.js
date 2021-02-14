96f720c0e18433aac8e01e6f86a84805cd1ac7ae
Loïc Bellemare-Alford <loicbellemare@hotmail.com>
2021-02-13 22:33:53 -0500

    Reformating

--- ScoutSuite/frontend/src/components/Table/index.js
+++ ScoutSuite/frontend/src/components/Table/index.js
@@ -5,7 +5,6 @@ import TableRender from './Renderer/index';
 
 import './style.scss';
 
-
 const propTypes = {
   columns: PropTypes.arrayOf(PropTypes.object).isRequired,
   data: PropTypes.arrayOf(PropTypes.object).isRequired,
@@ -14,8 +13,14 @@ const propTypes = {
   sortBy: PropTypes.object,
 };
 
-const Table = props => {
-  const { columns, data, initialState = {}, formatters = {}, sortBy = {} } = props;
+const Table = (props) => {
+  const {
+    columns,
+    data,
+    initialState = {},
+    formatters = {},
+    sortBy = {},
+  } = props;
 
   const cols = columns.map((item) => {
     let col = {
@@ -23,7 +28,11 @@ const Table = props => {
       accessor: item.key,
     };
     if (formatters[item.key]) col.Cell = formatters[item.key];
-    if (sortBy[item.key]) col.sortType = typeof sortBy[item.key] === 'function' ? sortBy[item.key] : sortBy[item.key];
+    if (sortBy[item.key])
+      col.sortType =
+        typeof sortBy[item.key] === 'function'
+          ? sortBy[item.key]
+          : sortBy[item.key];
     return col;
   });
 
