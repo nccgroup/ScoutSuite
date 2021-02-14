f8788aacd69dd976a13f1c302376085d44f61c8a
Loïc Bellemare-Alford <loicbellemare@hotmail.com>
2021-02-13 22:20:51 -0500

    Added items table

--- ScoutSuite/frontend/src/App.js
+++ ScoutSuite/frontend/src/App.js
@@ -5,6 +5,7 @@ import Findings from './pages/Findings';
 import Resources from './pages/Resources';
 import Dashboard from './pages/Dashboard';
 import ExternalAttack from './pages/ExternalAttack/index';
+import FlaggedItems from './pages/FlaggedItems/index';
 
 function App() {
   return (
@@ -12,8 +13,8 @@ function App() {
       <Dashboard path="/" />
       <Findings path="/services/:service/findings" /> 
       <ExternalAttack path="/services/:service/external-attacks" /> 
-      <Resources path="/services/:service/findings/:finding/items" />
-      <Resources path="/services/:service/findings/:finding/items/:item" />
+      <FlaggedItems path="/services/:service/findings/:finding/items" />
+      <FlaggedItems path="/services/:service/findings/:finding/items/:item" />
       <Resources path="/services/:service/:resources" /> 
     </Router>
   );
---
+++ ScoutSuite/frontend/src/pages/FlaggedItems/index.js
@@ -0,0 +1,79 @@
+import { useParams } from '@reach/router';
+import React from 'react';
+
+// import PropTypes from 'prop-types';
+
+import { useAPI } from '../../api/useAPI';
+import { sortBySeverity } from '../../utils/Severity/sort';
+import Layout from '../../layout';
+import Table from '../../components/Table';
+
+import './style.scss';
+import Name from './formatters/Name/index';
+
+const propTypes = {};
+
+const FlaggedItems = () => {
+  const params = useParams();
+  const { data: items, loading } = useAPI(`services.${params.service}.findings.${params.finding}.items`);
+
+  if (loading) return (
+    <Layout>
+      
+    </Layout>
+  );
+
+  const columns = [
+    { name: 'ID', key: 'id' },
+    { name: 'Name', key: 'name' },
+  ];
+
+  for (let [key,value] of Object.entries(items[0].extra)) {
+    columns.push({ name: value.name, key });
+  }
+
+  const data = items.map((item) => {
+    let newItem = item;
+
+    for (let [key, extra] of Object.entries(item.extra)) {
+      newItem[key] = extra.value;
+    }
+
+    return newItem;
+  });
+
+  const initialState = {};
+
+  const formatters = {
+    name: Name
+  };
+
+  const sortBy = {
+    severity: sortBySeverity
+  };
+
+  return (
+    <Layout>
+      <div className="findings">
+
+        <div className="table-card">
+          <Table
+            columns={columns}
+            data={data}
+            initialState={initialState}
+            formatters={formatters}
+            sortBy={sortBy} />
+        </div>
+
+        <div className="table-card">
+          {!params.item ? 'No selected item.' : params.item}
+        </div>
+
+      </div>
+    </Layout>
+  );
+};
+
+FlaggedItems.propTypes = propTypes;
+
+export default FlaggedItems;
