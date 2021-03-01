c4d05820d39fc9565a0e99a9bc353176e4ddf0c0
Sophie <sophie.dorval@polymtl.ca>
2021-03-01 16:30:53 -0500

    add rules 4.2.5, 4.2.6, 4.2.7 and 4.2.8 from cis azure

--- ScoutSuite/providers/azure/facade/sqldatabase.py
+++ ScoutSuite/providers/azure/facade/sqldatabase.py
@@ -116,3 +116,14 @@ class SQLDatabaseFacade:
         except Exception as e:
             print_exception(f'Failed to retrieve database transparent data encryptions: {e}')
             return []
+
+    async def get_server_vulnerability_assessments(self, resource_group_name, server_name,
+                                                   subscription_id: str):
+        try:
+            client = self.get_client(subscription_id)
+            return await run_concurrently(
+                lambda: client.server_vulnerability_assessments.get(resource_group_name, server_name, 'default')
+            )
+        except Exception as e:
+            print_exception(f'Failed to retrieve database transparent data encryptions: {e}')
+            return []
