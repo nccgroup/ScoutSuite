d41ae0f9fec9b363fb7795308db235eccb9b215e
Sophie Dorval <42855086+SophieDorval@users.noreply.github.com>
2021-03-03 10:36:57 -0500

    Enhancement/azure sql 6.3 (#1148)
    * refactor sql facade code
    
    * added rule 6.3 from azure cis
    
    Co-authored-by: Sophie <sophie.dorval@polymtl.ca>
    Co-authored-by: xnkevinnguyen <xuannamkevin@gmail.com>

--- ScoutSuite/providers/azure/facade/sqldatabase.py
+++ ScoutSuite/providers/azure/facade/sqldatabase.py
@@ -112,3 +112,13 @@ class SQLDatabaseFacade:
         except Exception as e:
             print_exception(f'Failed to retrieve database transparent data encryptions: {e}')
             return []
+
+    async def get_firewall_rules(self, resource_group_name, server_name, subscription_id: str):
+        try:
+            client = self.get_client(subscription_id)
+            return await run_concurrently(
+                lambda: list(client.firewall_rules.list_by_server(resource_group_name, server_name))
+            )
+        except Exception as e:
+            print_exception(f'Failed to retrieve firewalls rules: {e}')
+            return []
