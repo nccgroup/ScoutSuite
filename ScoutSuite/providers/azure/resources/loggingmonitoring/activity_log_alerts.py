e9500f38a8260bb8c1a6a965ac3cfa99782e2555
Sophie <sophie.dorval@polymtl.ca>
2021-03-03 14:41:35 -0500

    added rule of section 5.2 of azure cis

--- ScoutSuite/providers/azure/resources/loggingmonitoring/activity_log_alerts.py
+++ ScoutSuite/providers/azure/resources/loggingmonitoring/activity_log_alerts.py
@@ -9,16 +9,42 @@ class ActivityLogAlerts(AzureResources):
         self.subscription_id = subscription_id
 
     async def fetch_all(self):
-        for log_alert in await self.facade.loggingmonitoring.get_activity_log_alerts(self.subscription_id):
-            id, log_alerts = self._parse_log_alerts(log_alert)
-            self[id] = log_alerts
+        log_alerts = await self.facade.loggingmonitoring.get_activity_log_alerts(self.subscription_id)
+        self[self.subscription_id] = self._parse_log_alerts(log_alerts)
 
-    def _parse_log_alerts(self, log_alert):
-        log_alert_dict = {}
+    def _parse_log_alerts(self, log_alerts):
+        log_alerts_dict = {}
+        log_alerts_dict['create_policy_assignment_exist'] = self.ensure_alert_exist(log_alerts,
+                                                                                    'microsoft.authorization'
+                                                                                    '/policyassignments/write')
+        log_alerts_dict['create_update_NSG_exist'] = self.ensure_alert_exist(log_alerts,
+                                                                             'microsoft.network/networksecuritygroups'
+                                                                             '/write')
+        log_alerts_dict['delete_NSG_exist'] = self.ensure_alert_exist(log_alerts,
+                                                                      'microsoft.network/networksecuritygroups/delete')
+        log_alerts_dict['create_update_NSG_rule_exist'] = self.ensure_alert_exist(log_alerts,
+                                                                                   'microsoft.network'
+                                                                                   '/networksecuritygroups '
+                                                                                   '/securityrules/write')
+        log_alerts_dict['delete_NSG_rule_exist'] = self.ensure_alert_exist(log_alerts,
+                                                                           'microsoft.network/networksecuritygroups'
+                                                                           '/securityrules/delete')
+        log_alerts_dict['create_update_security_solution_exist'] = self.ensure_alert_exist(log_alerts,
+                                                                                           'microsoft.security'
+                                                                                           '/securitysolutions/write')
+        log_alerts_dict['delete_security_solution_exist'] = self.ensure_alert_exist(log_alerts,
+                                                                           'microsoft.security/securitysolutions/delete')
+        log_alerts_dict['create_delete_firewall_rule_exist'] = self.ensure_alert_exist(log_alerts,
+                                                                                       'microsoft.sql/servers'
+                                                                                       '/firewallrules/write')
 
-        log_alert_dict['id'] = log_alert.id
-        log_alert_dict['name'] = log_alert.name
+        return log_alerts_dict
 
-        return log_alert_dict['id'], log_alert_dict
-
-
+    def ensure_alert_exist(self, log_alerts, equals_value: str):
+        for log_alert in log_alerts:
+            if log_alert.location == 'Global' and log_alert.enabled:
+                if '/subscriptions/' + self.subscription_id in log_alert.scopes:
+                    for condition in log_alert.condition.all_of:
+                        if condition.field == 'operationName' and condition.equals == equals_value:
+                            return True
+        return False
