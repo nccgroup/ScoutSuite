6289c929d3d82774b49a140d210c337fe68d769f
Loïc Bellemare-Alford <loicbellemare@hotmail.com>
2021-03-08 17:18:49 -0500

    New menu design (#1167)

--- ScoutSuite/frontend/src/pages/Dashboard/index.js
+++ ScoutSuite/frontend/src/pages/Dashboard/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 
 import { TabsMenu, TabPane } from '../../components/Tabs/';
-import Layout from '../../layout/index';
 import Summary from './TabsContent/Summary';
 import ExecutionDetails from './TabsContent/ExecutionDetails';
 import ResourcesDetails from './TabsContent/ResourcesDetails';
 import { useAPI } from '../../api/useAPI';
 import { TAB_NAMES } from '../../utils/Dashboard';
+import Breadcrumb from '../../components/Breadcrumb/index';
 
 import './style.scss';
-
-
 
 const Dashboard = () => {
   const { data: services, loading } = useAPI('raw/last_run/summary');
@@ -20,7 +18,8 @@ const Dashboard = () => {
   const serivcesList = Object.entries(services).map(([name, item]) => ({name, ...item}));
 
   return (
-    <Layout>
+    <>
+      <Breadcrumb />
       <div className="dashboard">
         <TabsMenu>
           <TabPane title={TAB_NAMES.SUMMARY}>
@@ -34,7 +33,7 @@ const Dashboard = () => {
           </TabPane>
         </TabsMenu>
       </div>
-    </Layout>
+    </>
   );
 };
 
