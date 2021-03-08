7ac1ce9bf94ec2f6351dec601fdffbf8f1211a6a
Samuel Meilleur <samuel.meilleur@polymtl.ca>
2021-03-08 01:05:06 -0500

    Added warning for default group with rules.

--- ScoutSuite/frontend/src/partials/aws/ec2.regions.id.vpcs.id.security_groups.id/RulesList/index.js
+++ ScoutSuite/frontend/src/partials/aws/ec2.regions.id.vpcs.id.security_groups.id/RulesList/index.js
@@ -1,5 +1,7 @@
 import React, { useContext }  from 'react';
+import ReportProblemOutlinedIcon from '@material-ui/icons/ReportProblemOutlined';
 import get from 'lodash/get';
+
 
 import { PartialContext, PartialPathContext } from '../../../../components/Partial/context';
 import { PartialValue } from '../../../../components/Partial';
@@ -12,76 +14,91 @@ const RulesList = () => {
   const basePath = useContext(PartialPathContext);
   const value = get(ctx.item, basePath);
 
+  const isDefault = get(ctx.item, 'name') === 'default';
+
   return (
-    <ul className="rules-list">
-      {Object.entries(value.protocols).map(([name, { ports }], i) => (
-        <div key={i}>
-          <li>{name}</li>
-          <ul> 
-            <li>
-              Ports:
-              <ul>
-                {Object.entries(ports).map(([port_name, port], i) => (
-                  <div key={i}>
-                    <li>
-                      <PartialValue 
-                        value={port_name} 
-                        errorPath={`protocols.${name}.ports.${port_name}`}
-                      />
-                    </li>
-                    <ul>
-                      {port.cidrs && (
-                        <li>
-                          IP addresses:
-                          <ul>
-                            {port.cidrs.map((address, i) => (
-                              <li key={i}>
-                                <PartialValue
-                                  value={address.CIDR}
-                                  errorPath={`protocols.${name}.ports.${port_name}.cidrs.${i}.CIDR`}
-                                />
-                              </li>
-                            ))}
-                          </ul>
-                        </li>
-                      )}
-                      {port.Ipv6Ranges && (
-                        <li>
-                          IPv6 addresses:
-                          <ul>
-                            {port.Ipv6Ranges.map((address, i) => (
-                              <li key={i}>
-                                {/* TODO: actual value for IPv6*/}
-                                {address}
-                              </li>
-                            ))}
-                          </ul>
-                        </li>
-                      )}
-                      {port.security_groups && (
-                        <li>
-                          EC2 security groups:
-                          <ul>
-                            {port.security_groups.map((group, i) => (
-                              <li key={i}>
-                                <PartialValue 
-                                  value={`${group.GroupName}\xa0\xa0(${group.GroupId})`}
-                                  errorPath={`protocols.${name}.ports.${port_name}.security_groups.${i}`}
-                                />
-                              </li>
-                            ))}
-                          </ul>
-                        </li>
-                      )}
-                    </ul>
-                  </div>
-                ))}
-              </ul>
-            </li>
-          </ul>
-        </div>
-      ))}
-    </ul>
+    <>
+      <ul className="rules-list">
+        {Object.entries(value.protocols).map(([name, { ports }], i) => (
+          <div key={i}>
+            <li>{name}</li>
+            <ul>
+              <li>
+                Ports:
+                <ul>
+                  {Object.entries(ports).map(([port_name, port], i) => (
+                    <div key={i}>
+                      <li>
+                        <PartialValue
+                          value={port_name}
+                          errorPath={`protocols.${name}.ports.${port_name}`}
+                        />
+                      </li>
+                      <ul>
+                        {port.cidrs && (
+                          <li>
+                            IP addresses:
+                            <ul>
+                              {port.cidrs.map((address, i) => (
+                                <li key={i}>
+                                  <PartialValue
+                                    value={address.CIDR}
+                                    errorPath={`protocols.${name}.ports.${port_name}.cidrs.${i}.CIDR`}
+                                  />
+                                </li>
+                              ))}
+                            </ul>
+                          </li>
+                        )}
+                        {port.Ipv6Ranges && (
+                          <li>
+                            IPv6 addresses:
+                            <ul>
+                              {port.Ipv6Ranges.map((address, i) => (
+                                <li key={i}>
+                                  {/* TODO: actual value for IPv6*/}
+                                  {address}
+                                </li>
+                              ))}
+                            </ul>
+                          </li>
+                        )}
+                        {port.security_groups && (
+                          <li>
+                            EC2 security groups:
+                            <ul>
+                              {port.security_groups.map((group, i) => (
+                                <li key={i}>
+                                  <PartialValue
+                                    value={`${group.GroupName}\xa0\xa0(${group.GroupId})`}
+                                    errorPath={`protocols.${name}.ports.${port_name}.security_groups.${i}`}
+                                  />
+                                </li>
+                              ))}
+                            </ul>
+                          </li>
+                        )}
+                      </ul>
+                    </div>
+                  ))}
+                </ul>
+              </li>
+            </ul>
+          </div>
+        ))}
+      </ul>
+      {isDefault && (
+        <PartialValue 
+          value="Default security groups should have no rules."
+          errorPath="default_with_rules"
+          renderValue={value => (
+            <span className="default-with-rules">
+              <ReportProblemOutlinedIcon fontSize="inherit"/> {value}
+            </span>
+          )}
+        />
+      )}
+    </>
   );
 };
 
