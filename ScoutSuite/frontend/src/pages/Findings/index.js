f3e04e389291322a96e34aa1b3ea19f946d91101
Loïc Bellemare-Alford <loicbellemare@hotmail.com>
2021-03-19 12:08:29 -0400

    Added server side pagination (#1194)
    * Added server side pagination
    
    * Small cleanup
    
    * Small cleanup

--- ScoutSuite/frontend/src/pages/Findings/index.js
+++ ScoutSuite/frontend/src/pages/Findings/index.js
@@ -1,4 +1,4 @@
-import { useParams } from '@reach/router';
+import { useParams } from 'react-router-dom';
 import React from 'react';
 import CheckCircleOutlineOutlinedIcon from '@material-ui/icons/CheckCircleOutlineOutlined';
 
