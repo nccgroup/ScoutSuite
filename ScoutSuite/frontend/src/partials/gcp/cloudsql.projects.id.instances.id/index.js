5a96b3be706063c991b5cc8c919f0076b97cfa8e
Loïc Bellemare-Alford <loicbellemare@hotmail.com>
2021-03-22 12:23:53 -0400

    Added CloudSQL partial

--- ScoutSuite/frontend/src/partials/gcp/cloudsql.projects.id.instances.id/index.js
+++ ScoutSuite/frontend/src/partials/gcp/cloudsql.projects.id.instances.id/index.js
@@ -1,15 +1,34 @@
-
 import React from 'react';
 import PropTypes from 'prop-types';
+import get from 'lodash/get';
 
 import { Partial, PartialValue } from '../../../components/Partial';
-import { 
+import {
   partialDataShape,
-  convertBoolToEnable, 
-  formatDate, 
-  valueOrNone
+  convertBoolToEnable,
+  formatDate,
+  valueOrNone,
 } from '../../../utils/Partials';
+import { TabPane, TabsMenu } from '../../../components/Tabs';
 
+const renderAuthorizedNetworks = items => {
+  if (!items || items.length === 0) return <span>None</span>;
+
+  return (
+    <ul>
+      {items.map((value, i) => {
+        return (
+          <li key={i}>
+            <PartialValue
+              errorPath={`authorized_networks.${i}.open_to_the_world`}
+              value={value}
+            />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
 const propTypes = {
   data: PropTypes.shape(partialDataShape).isRequired,
@@ -17,6 +36,7 @@ const propTypes = {
 
 const SQLInstances = props => {
   const { data } = props;
+  const item = get(data, ['item'], {});
 
   if (!data) return null;
 
@@ -30,37 +50,49 @@ const SQLInstances = props => {
         <PartialValue
           label="Automatic Backups"
           valuePath="automatic_backup_enabled"
-          renderValue={convertBoolToEnable} />
+          renderValue={convertBoolToEnable}
+        />
 
         <PartialValue
           label="Last Backup"
           valuePath="last_backup_timestamp"
-          renderValue={formatDate} />
+          renderValue={formatDate}
+        />
 
         <PartialValue
           label="Logs"
           valuePath="log_enabled"
-          renderValue={convertBoolToEnable} />
+          renderValue={convertBoolToEnable}
+        />
 
         <PartialValue
           label="SSL Required"
           valuePath="ssl_required"
-          renderValue={convertBoolToEnable} />
+          renderValue={convertBoolToEnable}
+        />
 
         <PartialValue
           label="Public IP Address"
           valuePath="public_ip"
-          renderValue={valueOrNone} />
+          renderValue={valueOrNone}
+        />
 
         <PartialValue
           label="Private IP Address"
           valuePath="private_ip"
-          renderValue={valueOrNone} />
+          renderValue={valueOrNone}
+        />
 
-
+        <PartialValue
+          label="Authorized Networks"
+          valuePath="authorized_networks"
+          renderValue={valueOrNone}
+        />
       </div>
 
-      
+      <TabsMenu>
+        <TabPane title="Authorized Networks">{renderAuthorizedNetworks(item.authorized_networks)}</TabPane>
+      </TabsMenu>
     </Partial>
   );
 };
