37d67e1d2d306d57a72ab5c57625035b0534dcbd
Sophie <sophie.dorval@polymtl.ca>
2021-03-22 15:41:40 -0400

    added rule 6.1.2 from gcp

--- ScoutSuite/providers/gcp/resources/cloudsql/base.py
+++ ScoutSuite/providers/gcp/resources/cloudsql/base.py
@@ -1,8 +1,10 @@
 from ScoutSuite.providers.gcp.resources.projects import Projects
 from ScoutSuite.providers.gcp.resources.cloudsql.database_instances import DatabaseInstances
+from ScoutSuite.providers.gcp.resources.cloudsql.mysql_instances import MySQLDatabaseInstances
 
 
 class CloudSQL(Projects):
     _children = [ 
-        (DatabaseInstances, 'instances')
+        (DatabaseInstances, 'instances'),
+        (MySQLDatabaseInstances, 'mysql_instances')
      ]
