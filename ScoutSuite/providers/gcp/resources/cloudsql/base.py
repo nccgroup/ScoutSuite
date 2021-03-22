a9f9904d53e8c9a9e25fd1fd44c0dc7e3ddbd048
Sophie <sophie.dorval@polymtl.ca>
2021-03-22 16:49:39 -0400

    added rule 6.2 from gcp

--- ScoutSuite/providers/gcp/resources/cloudsql/base.py
+++ ScoutSuite/providers/gcp/resources/cloudsql/base.py
@@ -1,8 +1,10 @@
 from ScoutSuite.providers.gcp.resources.projects import Projects
 from ScoutSuite.providers.gcp.resources.cloudsql.database_instances import DatabaseInstances
+from ScoutSuite.providers.gcp.resources.cloudsql.postgresql_instances import PostgreSQLDatabaseInstances
 
 
 class CloudSQL(Projects):
     _children = [ 
-        (DatabaseInstances, 'instances')
+        (DatabaseInstances, 'instances'),
+        (PostgreSQLDatabaseInstances, 'postgresql_instances')
      ]
---
+++ ScoutSuite/providers/gcp/resources/cloudsql/postgresql_instances.py
@@ -0,0 +1,106 @@
+from ScoutSuite.core.console import print_exception
+from ScoutSuite.providers.base.resources.base import Resources
+from ScoutSuite.providers.gcp.facade.base import GCPFacade
+from ScoutSuite.providers.utils import get_non_provider_id
+
+
+class PostgreSQLDatabaseInstances(Resources):
+
+    def __init__(self, facade: GCPFacade, project_id: str):
+        super().__init__(facade)
+        self.project_id = project_id
+
+    async def fetch_all(self):
+        raw_instances = await self.facade.cloudsql.get_database_instances(self.project_id)
+        for raw_instance in raw_instances:
+            if 'POSTGRES' in raw_instance['databaseVersion']:
+                instance_id, instance = self._parse_instance(raw_instance)
+                self[instance_id] = instance
+
+    def _parse_instance(self, raw_instance):
+        instance_dict = {}
+
+        instance_dict['id'] = get_non_provider_id(raw_instance['name'])
+        instance_dict['name'] = raw_instance['name']
+        instance_dict['project_id'] = raw_instance['project']
+        instance_dict['automatic_backup_enabled'] = raw_instance['settings']['backupConfiguration']['enabled']
+        if raw_instance['settings'].get('databaseFlags', None):
+            instance_dict['log_checkpoints_on'] = self._postgres_flags_on(raw_instance, 'log_checkpoints')
+            instance_dict['log_connections_on'] = self._postgres_flags_on(raw_instance, 'log_connections')
+            instance_dict['log_disconnections_on'] = self._postgres_flags_on(raw_instance, 'log_disconnections')
+            instance_dict['log_lock_waits_on'] = self._postgres_flags_on(raw_instance, 'log_lock_waits')
+            instance_dict['log_min_messages'] = self._postgres_log_min_error_statement_flags(raw_instance)
+            instance_dict['log_temp_files_0'] = self._postgres_log_temp_files_flags_0(raw_instance)
+            instance_dict['log_min_duration_statement_-1'] = self._postgres_log_min_duration_statement_flags_1(raw_instance)
+        else:
+            instance_dict['log_checkpoints_on'] = False
+            instance_dict['log_connections_on'] = False
+            instance_dict['log_disconnections_on'] = False
+            instance_dict['log_lock_waits_on'] = False
+            instance_dict['log_min_messages'] = False
+            instance_dict['log_temp_files_0'] = False
+            instance_dict['log_min_duration_statement_-1'] = False
+
+        instance_dict['database_version'] = raw_instance['databaseVersion']
+        instance_dict['log_enabled'] = self._is_log_enabled(raw_instance)
+        instance_dict['ssl_required'] = self._is_ssl_required(raw_instance)
+        instance_dict['authorized_networks'] = raw_instance['settings']['ipConfiguration']['authorizedNetworks']
+
+        # check if is or has a failover replica
+        instance_dict['has_failover_replica'] = raw_instance.get('failoverReplica', []) != []
+        instance_dict['is_failover_replica'] = raw_instance.get('masterInstanceName', '') != ''
+
+        # network interfaces
+        instance_dict['public_ip'] = None
+        instance_dict['private_ip'] = None
+        for address in raw_instance.get('ipAddresses', []):
+            if address['type'] == 'PRIMARY':
+                instance_dict['public_ip'] = address['ipAddress']
+            elif address['type'] == 'PRIVATE':
+                instance_dict['private_ip'] = address['ipAddress']
+            else:
+                print_exception('Unknown Cloud SQL instance IP address type: {}'.format(address['type']))
+
+        return instance_dict['id'], instance_dict
+
+    def _is_log_enabled(self, raw_instance):
+        return raw_instance['settings']['backupConfiguration'].get('binaryLogEnabled')
+
+    def _is_ssl_required(self, raw_instance):
+        return raw_instance['settings']['ipConfiguration'].get('requireSsl', False)
+
+    def _set_last_backup_timestamps(self, instances):
+        for instance_id, _ in instances:
+            self[instance_id]['last_backup_timestamp'] = self._get_last_backup_timestamp(
+                self[instance_id]['backups'])
+
+    def _get_last_backup_timestamp(self, backups):
+        if not backups:
+            return None
+        last_backup_id = max(backups.keys(), key=(
+            lambda k: backups[k]['creation_timestamp']))
+        return backups[last_backup_id]['creation_timestamp']
+
+    def _postgres_flags_on(self, raw_instance, flag_name: str):
+        for flag in raw_instance['settings']['databaseFlags']:
+            if flag['name'] == flag_name and flag['value'] == 'off':
+                return False
+        return True
+
+    def _postgres_log_min_error_statement_flags(self, raw_instance):
+        for flag in raw_instance['settings']['databaseFlags']:
+            if flag['name'] == 'log_min_error_statement' and flag['value'] is not None:
+                return True
+        return False
+
+    def _postgres_log_temp_files_flags_0(self, raw_instance):
+        for flag in raw_instance['settings']['databaseFlags']:
+            if flag['name'] == 'log_temp_files' and flag['value'] != 0:
+                return False
+        return True
+
+    def _postgres_log_min_duration_statement_flags_1(self, raw_instance):
+        for flag in raw_instance['settings']['databaseFlags']:
+            if flag['name'] == 'log_min_duration_statement' and flag['value'] != -1:
+                return False
+        return True
