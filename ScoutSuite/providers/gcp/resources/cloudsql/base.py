0ce3b5623ef019ad45898cc9f4b4da4edd37b7f7
Sophie <sophie.dorval@polymtl.ca>
2021-04-05 11:23:24 -0400

    modified mysql to use only database instances

--- ScoutSuite/providers/gcp/resources/cloudsql/base.py
+++ ScoutSuite/providers/gcp/resources/cloudsql/base.py
@@ -1,10 +1,8 @@
 from ScoutSuite.providers.gcp.resources.projects import Projects
 from ScoutSuite.providers.gcp.resources.cloudsql.database_instances import DatabaseInstances
-from ScoutSuite.providers.gcp.resources.cloudsql.mysql_instances import MySQLDatabaseInstances
 
 
 class CloudSQL(Projects):
     _children = [ 
-        (DatabaseInstances, 'instances'),
-        (MySQLDatabaseInstances, 'mysql_instances')
+        (DatabaseInstances, 'instances')
      ]
