5a9d75f65113ca218c06fd14d45696002f383929
Samuel Meilleur <samuel.meilleur@polymtl.ca>
2021-04-14 17:15:00 -0400

    Fixed issue after merge conflict

--- ScoutSuite/frontend/src/App.js
+++ ScoutSuite/frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { SnackbarProvider } from 'notistack';
 
+import AppLoader from './components/AppLoader';
 import { ExceptionsContextProvider } from './components/Exceptions/context';
 import Findings from './pages/Findings';
 import Resources from './pages/Resources';
@@ -14,47 +15,49 @@ import Layout from './layout';
 
 function App() {
   return (
-    <SnackbarProvider 
-      maxSnack={1}
-      preventDuplicate
-    >
-      <ExceptionsContextProvider>
-        <Router>
-          <Layout>
-            <Switch>
-              <Route
-                path={[
-                  '/services/:service/findings/:finding/items/:item',
-                  '/services/:service/findings/:finding',
-                ]}
-              >
-                <FindingItems />
-              </Route>
-              <Route path="/services/:service/resources/:resource/:id?">
-                <Resources />
-              </Route>
-              <Route path="/services/:service/findings">
-                <Findings />
-              </Route>
-              <Route
-                path={[
-                  '/services/:service/external_attack_surface',
-                  '/categories/:category/external_attack_surface',
-                ]}
-              >
-                <ExternalAttack />
-              </Route>
-              <Route path="/services/:service/password_policy">
-                <PasswordPolicy />
-              </Route>
-              <Route path="/" exact>
-                <Dashboard />
-              </Route>
-            </Switch>
-          </Layout>
-        </Router>
-      </ExceptionsContextProvider>
-    </SnackbarProvider>
+    <AppLoader>
+      <SnackbarProvider 
+        maxSnack={1}
+        preventDuplicate
+      >
+        <ExceptionsContextProvider>
+          <Router>
+            <Layout>
+              <Switch>
+                <Route
+                  path={[
+                    '/services/:service/findings/:finding/items/:item',
+                    '/services/:service/findings/:finding',
+                  ]}
+                >
+                  <FindingItems />
+                </Route>
+                <Route path="/services/:service/resources/:resource/:id?">
+                  <Resources />
+                </Route>
+                <Route path="/services/:service/findings">
+                  <Findings />
+                </Route>
+                <Route
+                  path={[
+                    '/services/:service/external_attack_surface',
+                    '/categories/:category/external_attack_surface',
+                  ]}
+                >
+                  <ExternalAttack />
+                </Route>
+                <Route path="/services/:service/password_policy">
+                  <PasswordPolicy />
+                </Route>
+                <Route path="/" exact>
+                  <Dashboard />
+                </Route>
+              </Switch>
+            </Layout>
+          </Router>
+        </ExceptionsContextProvider>
+      </SnackbarProvider>
+    </AppLoader>
   );
 }
 
