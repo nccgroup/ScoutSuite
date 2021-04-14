c260f22ddd7049d10f7c7100f519fc9888f9028d
Noboru Yoshida <noboruyoshida1997@gmail.com>
2021-04-14 15:16:38 -0400

    Refactor unnused code

--- ScoutSuite/output/html.py
+++ ScoutSuite/output/html.py
@@ -27,27 +27,15 @@ class HTMLReport:
 
         # exceptions = {} if exceptions is None else exceptions
         self.exceptions = exceptions if exceptions else {}
-        self.scout_report_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
         self.exceptions_encoder = JavaScriptEncoder(self.report_name, report_dir, timestamp)    
         self.encoder = JavaScriptEncoder(self.report_name, report_dir, timestamp)
 
-    def prepare_html_report_dir(self):
+    def prepare_results_dir(self):
         if not os.path.isdir(self.report_dir):
             os.makedirs(self.report_dir)
         run_results_dir = os.path.join(self.report_dir, DEFAULT_REPORT_RESULTS_DIRECTORY)
         if not os.path.isdir(run_results_dir):
             os.makedirs(run_results_dir)
-        # Copy static 3rd-party files
-        archive = os.path.join(self.scout_report_data_path, 'includes.zip')
-        zip_ref = zipfile.ZipFile(archive)
-        zip_ref.extractall(self.report_dir)
-        zip_ref.close()
-        # Copy static files
-        inc_scout_dir = os.path.join(self.report_dir, DEFAULT_INCLUDES_DIRECTORY)
-        src_inc_scout_dir = os.path.join(self.scout_report_data_path, DEFAULT_INCLUDES_DIRECTORY)
-        if os.path.isdir(inc_scout_dir):
-            shutil.rmtree(inc_scout_dir)
-        shutil.copytree(src_inc_scout_dir, inc_scout_dir)
 
 
 class ScoutReport(HTMLReport):
@@ -62,7 +50,7 @@ class ScoutReport(HTMLReport):
         super().__init__(report_name, report_dir, timestamp, exceptions)
 
     def save(self, config, exceptions, force_write=False, debug=False):
-        self.prepare_html_report_dir()
+        self.prepare_results_dir()
         self.encoder.save_to_file(config, 'RESULTS', force_write, debug)
         self.exceptions_encoder.save_to_file(exceptions, 'EXCEPTIONS', force_write, debug)
         if ERRORS_LIST:
