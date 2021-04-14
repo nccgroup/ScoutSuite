220bf051122c02cca26fbb184345f5c2d9568901
Sophie <sophie.dorval@polymtl.ca>
2021-04-14 17:46:12 -0400

    add remove prefix function

--- ScoutSuite/providers/aws/resources/route53/hosted_zones.py
+++ ScoutSuite/providers/aws/resources/route53/hosted_zones.py
@@ -19,7 +19,7 @@ class HostedZones(AWSResources):
 
     async def _parse_hosted_zone(self, raw_hosted_zone):
         hosted_zone_dict = {}
-        hosted_zone_dict['id'] = raw_hosted_zone.get('Id').removeprefix('/hostedzone/')
+        hosted_zone_dict['id'] = self.remove_prefix(raw_hosted_zone.get('Id'), '/hostedzone/')
         hosted_zone_dict['name'] = raw_hosted_zone.get('Name')
         hosted_zone_dict['caller_reference'] = raw_hosted_zone.get('CallerReference')
         hosted_zone_dict['config'] = raw_hosted_zone.get('Config')
@@ -27,3 +27,8 @@ class HostedZones(AWSResources):
         hosted_zone_dict['resource_record_set_count'] = raw_hosted_zone.get('ResourceRecordSetCount')
         hosted_zone_dict['arn'] = format_arn(self.partition,  self.service, self.region, '', raw_hosted_zone.get('Id'), self.resource_type)
         return hosted_zone_dict['id'], hosted_zone_dict
+
+    def remove_prefix(self, text, prefix):
+        if text.startswith(prefix):
+            return text[len(prefix):]
+        return text
