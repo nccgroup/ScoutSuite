6c9abbb68b56bc4d32d2f61ac63f5a22f4b310b5
Loïc Bellemare-Alford <loicbellemare@hotmail.com>
2021-04-16 14:57:04 -0400

    Added Scout Suite details

--- ScoutSuite/frontend/src/layout/index.js
+++ ScoutSuite/frontend/src/layout/index.js
@@ -3,38 +3,48 @@ import PropTypes from 'prop-types';
 import BarChartIcon from '@material-ui/icons/BarChart';
 import DevicesOtherIcon from '@material-ui/icons/DevicesOther';
 import { useLocation } from 'react-router-dom';
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+import Dialog from '@material-ui/core/Dialog';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 
 import { useAPI } from '../api/useAPI';
-import { 
-  getDashboardName,
-  getDashboardLink,
-} from '../utils/Dashboard';
+import { getDashboardName, getDashboardLink } from '../utils/Dashboard';
 import { getServicesEndpoint } from '../api/paths';
 import Header from './Header';
 import { MenuBar, SubMenu, MenuGroup, MenuElement } from './Menu';
+import DownloadException from '../components/Exceptions/DownloadButton';
 
 import './style.scss';
-import DownloadException from '../components/Exceptions/DownloadButton';
+
 
 const propTypes = {
   children: PropTypes.node.isRequired,
 };
 
-const Layout = (props) => {
+const Layout = props => {
   const location = useLocation();
   const [opened, setOpened] = useState(null);
   const [selected, setSelected] = useState(null);
   const { data: categories, loading } = useAPI(getServicesEndpoint());
   const { children } = props;
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   useEffect(() => {
     if (categories) {
       const service = location.pathname.match(/^\/services\/(.*?)\//);
       const navOpen = categories.find(({ services }) =>
-        services.map((s) => s.id).includes(service ? service[1] : null),
+        services.map(s => s.id).includes(service ? service[1] : null),
       );
       const pathParts = location.pathname.split('/');
-      const selected = pathParts.slice(0,5).join('/');
+      const selected = pathParts.slice(0, 5).join('/');
       setSelected(selected);
       setOpened(navOpen ? navOpen.name : null);
     }
@@ -43,56 +53,77 @@ const Layout = (props) => {
   if (loading) return null;
 
   return (
-    <div className="main-layout">
-      <Header />
-      <MenuBar>
-        {categories.map((category) => {
-          return (
-            <SubMenu
-              title={category.name}
-              opened={opened}
-              setOpened={setOpened}
-              key={category.id}
-            >
-              {category.services.map((service) => (
-                <MenuGroup
-                  title={service.name} key={service.id}
-                  size="large">
-                  {service.dashboards.map((dashboard) => (
-                    <MenuElement
-                      link={getDashboardLink(dashboard, service.id)}
-                      key={dashboard}
-                      selected={selected}
-                    >
-                      <BarChartIcon fontSize="inherit" />{' '}
-                      <span>{getDashboardName(dashboard)}</span>
-                    </MenuElement>
-                  ))}
+    <>
+      <div className="main-layout">
+        <Header />
+        <MenuBar>
+          {categories.map(category => {
+            return (
+              <SubMenu
+                title={category.name}
+                opened={opened}
+                setOpened={setOpened}
+                key={category.id}
+              >
+                {category.services.map(service => (
+                  <MenuGroup
+                    title={service.name} key={service.id}
+                    size="large">
+                    {service.dashboards.map(dashboard => (
+                      <MenuElement
+                        link={getDashboardLink(dashboard, service.id)}
+                        key={dashboard}
+                        selected={selected}
+                      >
+                        <BarChartIcon fontSize="inherit" />{' '}
+                        <span>{getDashboardName(dashboard)}</span>
+                      </MenuElement>
+                    ))}
 
-                  {service.resources.map((res) => (
-                    <MenuElement
-                      link={`services/${service.id}/resources/${res.id}`}
-                      disabled={!res.count}
-                      selected={selected}
-                      key={res.id}
-                    >
-                      <DevicesOtherIcon fontSize="inherit" />{' '}
-                      <span>
-                        {res.name} ({res.count || 0})
-                      </span>
-                    </MenuElement>
-                  ))}
-                </MenuGroup>
-              ))}
-            </SubMenu>
-          );
-        })}
-        <DownloadException />
-      </MenuBar>
-      <div className="main">
-        {children}
+                    {service.resources.map(res => (
+                      <MenuElement
+                        link={`services/${service.id}/resources/${res.id}`}
+                        disabled={!res.count}
+                        selected={selected}
+                        key={res.id}
+                      >
+                        <DevicesOtherIcon fontSize="inherit" />{' '}
+                        <span>
+                          {res.name} ({res.count || 0})
+                        </span>
+                      </MenuElement>
+                    ))}
+                  </MenuGroup>
+                ))}
+              </SubMenu>
+            );
+          })}
+          <DownloadException />
+          <div className="about-scout" onClick={handleClickOpen}>
+            <InfoOutlinedIcon /> About Scout Suite
+          </div>
+        </MenuBar>
+        <div className="main">{children}</div>
       </div>
-    </div>
+
+      <Dialog
+        onClose={handleClose}
+        aria-labelledby="customized-dialog-title"
+        open={open}
+      >
+        <div className="modal-title" onClose={handleClose}>
+          <h2>About Scout Suite</h2>
+          <IconButton aria-label="close" onClick={handleClose}>
+            <CloseIcon />
+          </IconButton>
+        </div>
+        <div className="modal-content">
+          <p>Scout Suite is an open-source tool released by <a href="https://www.nccgroup.trust/">NCC Group</a>.</p>
+          <p>Use the top navigation bar to review the configuration of the supported cloud provider services.</p>
+          <p>For more information about Scout Suite, please check out the {'project\'s'} page on <a href="https://github.com/nccgroup/ScoutSuite">GitHub</a>.</p>
+        </div>
+      </Dialog>
+    </>
   );
 };
 
