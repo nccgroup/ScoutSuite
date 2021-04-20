f424e58989d97d754593e81fb515bd057dd31860
Loïc Bellemare-Alford <loicbellemare@hotmail.com>
2021-04-19 22:44:47 -0400

    Fixed breadcrumb

--- ScoutSuite/frontend/src/components/Breadcrumb/index.js
+++ ScoutSuite/frontend/src/components/Breadcrumb/index.js
@@ -1,10 +1,11 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useLocation } from 'react-router-dom';
 import React from 'react';
 
 import Provider from './Provider';
 import Service from './Service';
 import Findings from './Findings';
 import Resources from './Resources';
+import Permissions from './Permissions';
 import ErrorBoundary from '../ErrorBoundary';
 
 import './style.scss';
@@ -12,6 +13,7 @@ import './style.scss';
 
 const Breadcrumb = () => {
   const params = useParams();
+  const { pathname } = useLocation();
 
   return (
     <ErrorBoundary>
@@ -21,6 +23,7 @@ const Breadcrumb = () => {
         {params.service && <Service service={params.service} />}
         {params.finding && <Findings service={params.service} finding={params.finding} />}
         {params.resource && <Resources service={params.service} resource={params.resource} />}
+        {pathname.includes('/permissions/') && <Permissions service={params.service} id={params.id} />}
       </div>
     </ErrorBoundary>
   );
