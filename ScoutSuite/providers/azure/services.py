0d47bbaff5d4610a9b9c7be0daf8d42b01837c69
Maxime Nadeau <maxime.nadeau@outlook.com>
2021-12-10 11:05:49 -0500

    Allowed Azure checks on AAD to run when no subscriptions are found

--- ScoutSuite/providers/azure/services.py
+++ ScoutSuite/providers/azure/services.py
@@ -41,28 +41,30 @@ class AzureServicesConfig(BaseServicesConfig):
                              programmatic_execution)
 
         self.aad = AAD(facade)
-        self.rbac = RBAC(facade)
-        self.securitycenter = SecurityCenter(facade)
-        self.sqldatabase = Servers(facade)
-        self.storageaccounts = StorageAccounts(facade)
-        self.keyvault = KeyVaults(facade)
-        self.network = Networks(facade)
-        self.virtualmachines = VirtualMachines(facade)
-        self.appservice = AppServices(facade)
 
-        # Instantiate proprietary services
-        try:
-            self.appgateway = ApplicationGateways(facade)
-        except NameError as _:
-            pass
-        try:
-            self.loadbalancer = LoadBalancers(facade)
-        except NameError as _:
-            pass
-        try:
-            self.rediscache = RedisCaches(facade)
-        except NameError as _:
-            pass
+        if len(subscription_ids) > 0:
+            self.rbac = RBAC(facade)
+            self.securitycenter = SecurityCenter(facade)
+            self.sqldatabase = Servers(facade)
+            self.storageaccounts = StorageAccounts(facade)
+            self.keyvault = KeyVaults(facade)
+            self.network = Networks(facade)
+            self.virtualmachines = VirtualMachines(facade)
+            self.appservice = AppServices(facade)
+
+            # Instantiate proprietary services
+            try:
+                self.appgateway = ApplicationGateways(facade)
+            except NameError as _:
+                pass
+            try:
+                self.loadbalancer = LoadBalancers(facade)
+            except NameError as _:
+                pass
+            try:
+                self.rediscache = RedisCaches(facade)
+            except NameError as _:
+                pass
 
     def _is_provider(self, provider_name):
         return provider_name == 'azure'
