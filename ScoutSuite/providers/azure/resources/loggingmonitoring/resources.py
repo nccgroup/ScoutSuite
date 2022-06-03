dd405f69043ccf710fd1d5913149d19818ce5f46
fernando-gallego <102300106+fernando-gallego@users.noreply.github.com>
2022-06-03 11:48:17 +0200

    Disable KeyVault diagnostic settings checks
    Disable checks related to KeyVault diagnostic settings since some of the functions involved do not work as expected. get_diagnostic_settings invokes the "list" operation from DiagnosticSettingsOperations for the MonitorManagementClient class, which expects a "resource_uri" parameter consisting on an Azure resource URI. This is not currently being passed onto that function correctly and generates errors.

--- ScoutSuite/providers/azure/resources/loggingmonitoring/resources.py
+++ ScoutSuite/providers/azure/resources/loggingmonitoring/resources.py
@@ -5,26 +5,30 @@ from .diagnostic_resource_key_vault import DiagnosticResourceKeyVault
 
 
 class Resources(AzureCompositeResources):
+    """
+    TODO this is commented out since DiagnositcResourceKeyVault.get_diagnostic_settings needs to be fixed
     _children = [
         (DiagnosticResourceKeyVault, 'diagnostic_key_vault'),
-    ]
-
+    ]"""
+    
     def __init__(self, facade: AzureFacade, subscription_id: str):
         super().__init__(facade)
         self.subscription_id = subscription_id
-
     async def fetch_all(self):
         for raw_resource in await self.facade.resourcemanagement.get_specific_type_resources_with_filter(
                 self.subscription_id, 'Microsoft.KeyVault/vaults'):
             id, resource = self._parse_resource(raw_resource)
             self[id] = resource
-
+         
+        """
+        TODO this is commented out since DiagnositcResourceKeyVault.get_diagnostic_settings needs to be fixed
         await self._fetch_children_of_all_resources(
             resources=self,
             scopes={resource_id: {'resource_id': resource['id'],
                                   'subscription_id': self.subscription_id}
                     for (resource_id, resource) in self.items()}
         )
+        """
 
     def _parse_resource(self, raw_resource):
         resource = {}
