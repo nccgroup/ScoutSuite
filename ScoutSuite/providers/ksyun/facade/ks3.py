baaa244521ca67605e9a0b02ff5e5037264bebb5
alchu4n <54417083@qq.com>
2023-04-01 11:32:06 +0800

    完成ks3 API

--- ScoutSuite/providers/ksyun/facade/ks3.py
+++ ScoutSuite/providers/ksyun/facade/ks3.py
@@ -1,5 +1,6 @@
 import json
 
+import requests
 from ksyun.common.common_client import CommonClient
 from ksyun.common import credential
 from ksyun.common.exception.ksyun_sdk_exception import KsyunSDKException
@@ -13,10 +14,15 @@ class KS3Facade:
     def __init__(self, credentials: KsyunCredentials):
         self._credentials = credentials
 
-    async def get_buckets(self, region):
+    async def get_buckets(self):
+        url = "https://ks3.console.ksyun.com/i/ks3/ks3-www/api/?projectIds=0,106536"
+        headers = {
+            "Accept": "application/json",
+            "Cookie": "kscdigest=77372c6d8a01ef3b46dca11e3ef3f1dc-1344489989;"
+        }
         try:
-
-            response = ''
+            r = requests.get(url, headers=headers)
+            response = json.loads(r.text).get("bucketListV2")
             if response:
                 return response
             else:
--- ScoutSuite/providers/ksyun/resources/ks3/buckets.py
+++ ScoutSuite/providers/ksyun/resources/ks3/buckets.py
@@ -1,7 +1,13 @@
+
+from ScoutSuite.providers.ksyun.facade.base import KsyunFacade
 from ScoutSuite.providers.ksyun.resources.base import KsyunResources
 
 
 class Buckets(KsyunResources):
+    # def __init__(self, facade: KsyunFacade, region: str):
+    #     super().__init__(facade)
+    #     self.region = region
+
     async def fetch_all(self):
         for raw_bucket in await self.facade.ks3.get_buckets():
             id, bucket = self._parse_bucket(raw_bucket)
@@ -9,8 +15,8 @@ class Buckets(KsyunResources):
 
     def _parse_bucket(self, raw_bucket):
         bucket_dict = {}
-        bucket_dict['name'] = bucket_dict['id'] = raw_bucket.name
-        bucket_dict['location'] = raw_bucket.location
-        bucket_dict['storage_class'] = raw_bucket.storage_class
-        bucket_dict['creation_date'] = raw_bucket.creation_date
+        bucket_dict['name'] = bucket_dict['id'] = raw_bucket.get('name')
+        bucket_dict['location'] = raw_bucket.get('region')
+        bucket_dict['storage_class'] = raw_bucket.get('type')
+        bucket_dict['creation_date'] = raw_bucket.get('creationDate')
         return bucket_dict['id'], bucket_dict
