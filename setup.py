40d699cea7d81cb67bfbb199e17a1a90b81f1aa8
fernando-gallego <102300106+fernando-gallego@users.noreply.github.com>
2023-06-13 13:58:38 +0200

    Update setup.py
    Update supported Python versions.

--- setup.py
+++ setup.py
@@ -56,8 +56,7 @@ setup(
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11'
     ]
 )
