3d3fa716e18b16eaa15ca149650508141f0bc4dd
fernando-gallego <102300106+fernando-gallego@users.noreply.github.com>
2023-06-14 13:28:49 +0200

    Update setup.py
    Adding Python v3.9

--- setup.py
+++ setup.py
@@ -56,6 +56,7 @@ setup(
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11'
     ]
