63b1b4036d16e29e708bd8ac705c1ca572a48e0b
Asif Wani <asifna3@gmail.com>
2024-02-09 20:32:50 +0530

    added-pagination-support

--- ScoutSuite/providers/do/facade/droplet.py
+++ ScoutSuite/providers/do/facade/droplet.py
@@ -1,27 +1,35 @@
 from ScoutSuite.core.console import print_exception
 from ScoutSuite.providers.do.authentication_strategy import DoCredentials
 from ScoutSuite.providers.utils import run_concurrently
+from ScoutSuite.providers.do.facade.utils import DOFacadeUtils
 
 
 class DropletFacade:
     def __init__(self, credentials: DoCredentials):
         self._credentials = credentials
         self._client = credentials.client
+        self.current_page = 1
+        self.per_page = 50
 
     async def get_droplets(self):
         try:
-            droplets = await run_concurrently(
-                lambda: self._client.droplets.list()["droplets"]
+            droplets = await DOFacadeUtils.get_all_from_pagination(
+                self._client.droplets.list, self.current_page, self.per_page, "droplets"
             )
-            return droplets
+            return droplets["droplets"]
         except Exception as e:
             print_exception(f"Failed to get droplets: {e}")
             return []
 
     async def get_droplet_fwconfig(self, id):
         try:
-            droplet_fwconfig = await run_concurrently(
-                lambda: self._client.droplets.list_firewalls(id)
+            filters = {"droplet_id": id}
+            droplet_fwconfig = await DOFacadeUtils.get_all_from_pagination(
+                self._client.droplets.list_firewalls,
+                self.current_page,
+                self.per_page,
+                "firewalls",
+                filters,
             )
             return droplet_fwconfig
         except Exception as e:
--- ScoutSuite/providers/do/facade/networking.py
+++ ScoutSuite/providers/do/facade/networking.py
@@ -1,5 +1,6 @@
 from ScoutSuite.core.console import print_exception
 from ScoutSuite.providers.do.authentication_strategy import DoCredentials
+from ScoutSuite.providers.do.facade.utils import DOFacadeUtils
 from ScoutSuite.providers.utils import run_concurrently
 
 
@@ -7,33 +8,41 @@ class Networkingfacade:
     def __init__(self, credentials: DoCredentials):
         self._credentials = credentials
         self._client = credentials.client
+        self.current_page = 1
+        self.per_page = 50
 
     async def get_firewalls(self):
         try:
-            firewalls = await run_concurrently(
-                lambda: self._client.firewalls.list()["firewalls"]
+            firewalls = await DOFacadeUtils.get_all_from_pagination(
+                self._client.firewalls.list,
+                self.current_page,
+                self.per_page,
+                "firewalls",
             )
-            return firewalls
+            return firewalls["firewalls"]
         except Exception as e:
             print_exception(f"Failed to get firewalls: {e}")
             return []
 
     async def get_domains(self):
         try:
-            domains = await run_concurrently(
-                lambda: self._client.domains.list()["domains"]
+            domains = await DOFacadeUtils.get_all_from_pagination(
+                self._client.domains.list, self.current_page, self.per_page, "domains"
             )
-            return domains
+            return domains["domains"]
         except Exception as e:
             print_exception(f"Failed to get domains: {e}")
             return []
 
     async def get_load_balancers(self):
         try:
-            load_balancers = await run_concurrently(
-                lambda: self._client.load_balancers.list()["load_balancers"]
+            load_balancers = await DOFacadeUtils.get_all_from_pagination(
+                self._client.load_balancers.list,
+                self.current_page,
+                self.per_page,
+                "load_balancers",
             )
-            return load_balancers
+            return load_balancers["load_balancers"]
         except Exception as e:
             print_exception(f"Failed to get load balancers: {e}")
             return []
