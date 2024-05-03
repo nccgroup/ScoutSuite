cc9f7d710fbf9038690891b92ef4e619c5dd66f3
x4v13r64 <xavier.garceau-aranda@owasp.org>
2024-05-03 15:31:36 +0200

    Improve error handling

--- ScoutSuite/providers/aws/resources/emr/clusters.py
+++ ScoutSuite/providers/aws/resources/emr/clusters.py
@@ -10,8 +10,9 @@ class EMRClusters(AWSResources):
     async def fetch_all(self):
         raw_clusters = await self.facade.emr.get_clusters(self.region)
         for raw_cluster in raw_clusters:
-            name, resource = self._parse_cluster(raw_cluster)
-            self[name] = resource
+            if raw_cluster:
+                name, resource = self._parse_cluster(raw_cluster)
+                self[name] = resource
 
     def _parse_cluster(self, raw_cluster):
         raw_cluster['id'] = raw_cluster.pop('Id')
