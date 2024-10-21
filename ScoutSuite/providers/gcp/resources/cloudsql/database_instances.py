170153dab0a4860d7428e979c8c0eadbdb6aefa6
Liyun <7397775-liyun-li@users.noreply.gitlab.com>
2024-10-20 21:35:28 -0400

    sql server ok

--- ScoutSuite/providers/gcp/resources/cloudsql/database_instances.py
+++ ScoutSuite/providers/gcp/resources/cloudsql/database_instances.py
@@ -48,13 +48,14 @@ class DatabaseInstances(GCPCompositeResources):
             instance_dict['log_lock_waits_on'] = self._postgres_flags_on(raw_instance, 'log_lock_waits')
             instance_dict['log_min_messages'] = self._postgres_log_min_error_statement_flags(raw_instance)
             instance_dict['log_temp_files_0'] = self._postgres_log_temp_files_flags_0(raw_instance)
-            instance_dict['log_min_duration_statement_-1'] = self._postgres_log_min_duration_statement_flags_1(
-                raw_instance)
+            instance_dict['log_min_duration_statement_-1'] = self._postgres_log_min_duration_statement_flags_1(raw_instance)
 
-            instance_dict['cross_db_ownership_chaining_off'] = self._sqlservers_cross_db_ownership_chaining_flag_off(
-                raw_instance, 'cross db ownership chaining')
-            instance_dict['contained_database_authentication_off'] = self._sqlservers_cross_db_ownership_chaining_flag_off(
-                raw_instance, 'contained database authentication')
+            instance_dict['cross_db_ownership_chaining_off'] = self._sqlservers_flag_off(raw_instance, 'cross db ownership chaining')
+            instance_dict['contained_database_authentication_off'] = self._sqlservers_flag_off(raw_instance, 'contained database authentication')
+            instance_dict['external_scripts_enabled'] = self._sqlservers_flag_on(raw_instance, 'external scripts enabled')
+            instance_dict['user_options_specified'] = self._sqlservers_flag_specified(raw_instance, 'user options')
+            instance_dict['3625_off'] = self._sqlservers_flag_off(raw_instance, '3625')
+            instance_dict['remote_access_on'] = self._sqlservers_flag_on(raw_instance, 'remote access')
 
         else:
             instance_dict['local_infile_off'] = True
@@ -156,10 +157,29 @@ class DatabaseInstances(GCPCompositeResources):
         else:
             return None
 
-    def _sqlservers_cross_db_ownership_chaining_flag_off(self, raw_instance, flag_name: str):
+    def _sqlservers_flag_off(self, raw_instance, flag_name: str):
         if 'SQLSERVER' in raw_instance['databaseVersion']:
             for flag in raw_instance['settings'].get('databaseFlags', []):
                 if flag['name'] == flag_name and flag['value'] == 'off':
+                    return True
+            return False
+        else:
+            return None
+
+    # sorry i have no time to consolidate
+    def _sqlservers_flag_on(self, raw_instance, flag_name: str):
+        if 'SQLSERVER' in raw_instance['databaseVersion']:
+            for flag in raw_instance['settings'].get('databaseFlags', []):
+                if flag['name'] == flag_name and flag['value'] == 'on':
+                    return True
+            return False
+        else:
+            return None
+
+    def _sqlservers_flag_specified(self, raw_instance, flag_name: str):
+        if 'SQLSERVER' in raw_instance['databaseVersion']:
+            for flag in raw_instance['settings'].get('databaseFlags', []):
+                if flag['name'] == flag_name and flag['value']:
                     return True
             return False
         else:
