b91ac61ccfd81ef88858c24248018c9fa11135f2
Ronan Chota <ronan.chota@saic.com>
2025-12-05 18:45:28 -0500

    fix get namespace for current cdk

--- ScoutSuite/providers/oci/facade/objectstorage.py
+++ ScoutSuite/providers/oci/facade/objectstorage.py
@@ -13,10 +13,8 @@ class ObjectStorageFacade:
 
     async def get_namespace(self):
         try:
-            response = await run_concurrently(
-                lambda: list_call_get_all_results(self._client.get_namespace))
-            # for some reason it returns a list of chars instead of a string
-            return ''.join(response.data)
+            response = self._client.get_namespace()
+            return response.data
         except Exception as e:
             print_exception(f'Failed to get Object Storage namespace: {e}')
             return None
