eadedbd205b7102bcc25063da55734a4afad2110
philipdarlington <philip@wubbleyou.co.uk>
2026-02-19 15:26:22 +0000

    Changes scanning

--- ScoutSuite/providers/aws/resources/ecr/images.py
+++ ScoutSuite/providers/aws/resources/ecr/images.py
@@ -7,24 +7,16 @@ class Images(AWSResources):
     def __init__(self, facade: AWSFacade, region: str):
         super().__init__(facade)
         self.region = region
-        self.repository_name = None
 
     async def fetch_all(self):
-        if not self.repository_name:
-            self.repository_name = await self._get_repository_name()
-        if self.repository_name:
-            raw_repos = await self.facade.ecr.get_images(self.region, self.repository_name)
-            for raw_repo in raw_repos:
-                name, resource = self._parse_images(raw_repo)
-                self[name] = resource
-
-    async def _get_repository_name(self):
         raw_repos = await self.facade.ecr.get_repositories(self.region)
-        for raw in raw_repos:
-            repository_name = raw.get('repositoryName')
+        for raw_repo in raw_repos:
+            repository_name = raw_repo.get('repositoryName')
             if repository_name:
-                return repository_name
-        return None
+                images = await self.facade.ecr.get_images(self.region, repository_name)
+                for image in images:
+                    name, resource = self._parse_images(image)
+                    self[name] = resource
 
     def _parse_images(self, raw_repo):
         image = {}
@@ -46,4 +38,4 @@ class Images(AWSResources):
         image_scan_summary_completed = raw_repo.get('imageScanFindingsSummary', {}).get('imageScanCompletedAt')
         image['imageScanFindingsSummaryCompleted'] = image_scan_summary_completed.strftime('%Y-%m-%d %H:%M:%S') if image_scan_summary_completed else ''
         
-        return get_non_provider_id(image['name']), image
+        return get_non_provider_id(image['imageDigest']), image
